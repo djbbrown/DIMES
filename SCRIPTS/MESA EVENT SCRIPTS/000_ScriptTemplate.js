bee7d71583328e4d44084404aa37f8c4a790aed7
Kevin Ford <kford@accela.com>
2016-02-02 14:09:45 -0800

    Sample Commit with JS file.

---
+++ SCRIPTS/MESA EVENT SCRIPTS/000_ScriptTemplate.js
@@ -0,0 +1,11 @@
+/*===================================================================
+// Script Number: 
+// Script Name: 
+// Script Developer: 
+// Script Agency: (Mesa/Accela/Woolpert)
+// Script Description: 
+// Script Run Event: 
+/*==================================================================*/
+
+// Under this line create the function that will need to run at script runtime.
+// the function will be called in the event ("AppSubmitAfter") major event.
