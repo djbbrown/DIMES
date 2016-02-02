3c1284b0ec871b63b7334fda1eb5f573e7c2c10b
Kevin Ford <kford@accela.com>
2016-02-02 14:31:59 -0800

    Updating with a function in it.

--- SCRIPTS/EVENT/ASA;Permits!Residential!Mobile Home!NA.js
+++ SCRIPTS/EVENT/ASA;Permits!Residential!Mobile Home!NA.js
@@ -8,3 +8,14 @@ if (AInfo["Type of Work"] == "New Mobile Home")
                 showMessage = true;
                 commet("Please check for Historic District");
 }
+
+
+/*===================================================================
+// Script Number: 003
+// Script Name: LICSubstantiveReviewDueDateSet
+// Script Developer: 
+// Script Agency: (Mesa/Accela/Woolpert)
+// Script Description: 
+// Script Run Event: 
+/*==================================================================*/
+003_LICSubstantiveReviewDueDateSet();
