482eed1deb042548aa8cdd3fafef0eec0a710630
Kevin Ford <kford@accela.com>
2016-02-08 15:55:09 -0800

    Added descriptions.

--- SCRIPTS/MESA EVENT SCRIPTS/LIC_CreateLicenseRecord.js
+++ SCRIPTS/MESA EVENT SCRIPTS/LIC_CreateLicenseRecord.js
@@ -3,7 +3,12 @@
 // Script Name: Create License Record
 // Script Developer: Kevin Ford
 // Script Agency: Accela
-// Script Description: 
+// Script Description: Create the license record when the "Issue License"
+			task on the application record is set to a status of "Issued".
+			Convert to contact of type "Applicant" to "Licensee". Set the
+			expiration status to Active and the expiration date according
+			to the expiration code. Copy info from application to license
+			according to standard choice EMSE:ASI Copy Exceptions.  
 // Script Run Event: Workflow Task Update After
 // Script Parents:
 //            WTUA;Licenses!General!~!Application.js
--- SCRIPTS/MESA EVENT SCRIPTS/PLN_CasePurge.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PLN_CasePurge.js
@@ -3,11 +3,14 @@
 // Script Name: Case Purge
 // Script Developer: Kevin Ford
 // Script Agency: Accela
-// Script Description: 
+// Script Description: Every month, run a batch script that will check
+					records with the ASI "Meeting Date" having a value
+					of more than 2 years in the past. For those records,
+					update the record status to "File Purged" and delete
+					all attached documents.
 // Script Run Event: 
 // Script Parents:
-//            ASA;Planning!Group Home-Daycare!Application!NA  (example)
-//            ASA;Licensing!General!ParkAndSwap!NA  (example)
+//				BATCH SCRIPT
 /*==================================================================*/
 
 // Under this line create the function that will need to run at script runtime.
