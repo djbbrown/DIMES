11b37d86441ba4afc9e6c9015ff2e431c2acd4cf
rgillaccela <rgill@accela.com>
2016-02-08 22:16:52 -0800

    Initial ENF_CitationIssuedWFUpdate.js

---
+++ SCRIPTS/MESA EVENT SCRIPTS/ENF_CitationIssuedWFUpdate.js
@@ -0,0 +1,26 @@
+/*===================================================================
+// Script Number: 022
+// Script Name:022_ENFCitationIssuedWFUpdate.js
+// Script Developer: Raminder Gill
+// Script Agency: Accela
+// Script Description: Update workflow task and schedule additional Inspection based on Insp result
+// Script Run Event: IRSA
+// Script Parents:
+//	IRSA;Enforcement!Case!Code Sign Issue!~ 
+//            
+/*==================================================================*/
+
+if ((inspType == "Initial Inspection" || inspType == "Follow-Up Inspection") && inspResult == "Citation Issued") {
+	if( isTaskActive("Initial Inspection") ) {
+		closeTask("Initial Inspection","Citation Issued","Closed by Script","Closed by Script");
+		setTask("Citation Inspections","Y","N");
+		updateTask("Citation Inspections","Citation Issued","Updated by Script","Updated by Script");
+	}
+	if( isTaskActive("Follow-Up Inspection") ) {
+		closeTask("Follow-Up Inspection","Citation Issued","Closed by Script","Closed by Script");
+		setTask("Citation Inspections","Y","N");
+		updateTask("Citation Inspections","Citation Issued","Updated by Script","Updated by Script");
+	}
+	
+}
+
