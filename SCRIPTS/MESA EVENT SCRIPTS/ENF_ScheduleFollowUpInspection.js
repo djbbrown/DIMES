0897046bfdfd60d6e4065b3d5e5bf60c40adc47b
rgillaccela <rgill@accela.com>
2016-02-10 11:28:20 -0800

    removed workflow logic

--- SCRIPTS/MESA EVENT SCRIPTS/ENF_ScheduleFollowUpInspection.js
+++ SCRIPTS/MESA EVENT SCRIPTS/ENF_ScheduleFollowUpInspection.js
@@ -3,32 +3,23 @@
 // Script Name:ENF_ScheduleFollowUpInspection
 // Script Developer: Raminder Gill
 // Script Agency: Accela
-// Script Description: Update workflow task and schedule additional Inspection based on Insp result
+// Script Description: Schedule additional Inspection based on Insp result
+//  Workflow Update part is being done in ENF_InViolationInspectionUpdateWFStatus
 // Script Run Event: IRSA
 // Script Parents:
 //	IRSA;Enforcement!Case!~!~ 
 //	IRSA;Enforcement!Environmental!~!~ 
-//	-- this doesn't exist anymore IRSA;Enforcement!Animal Control!~!~ 
+//	 
 //      
 //            
 /*==================================================================*/
 
 if (inspType == "Initial Inspection" && inspResult == "In Violation") {
-	if( isTaskActive("Initial Inspection") ) {
-		branchTask("Initial Inspection","In Violation","Closed by Script","Closed by Script");
-	}
-	if( isTaskActive("Follow-Up Inspection") ) {
-		updateTask("Follow-Up Inspection","In Violation","Updated by Script","Updated by Script");
-	}
-	//assuming the citation Inspections is to be updated.
-	if( isTaskActive("Citation Inspections") ) {
-		updateTask("Citation Inspections","In Violation","Updated by Script","Updated by Script");
-	}
-	//Using the default of 14 days for Inspection interval - don't know how to use ASI field value here
+		
 	var inspInterval = AInfo["Inspection Interval"];
 	var daysOut = 14;
 	if (inspInterval == "10 Days")
-		daysOut = 7;
+		daysOut = 10;
 	if (inspInterval == "7 Days")
 		daysOut = 7;
 	if (inspInterval == "3 Days")
