b8adf27b6a81b6bc1ef30ab87760f8302fcd2654
Kevin Ford <kford@accela.com>
2016-02-19 15:31:03 -0800

    Adding code getPeopleWithADdresses

---
+++ INCLUDES_CUSTOM/getPeopleWithAddresses.js
@@ -0,0 +1,36 @@
+// The following function can be used when Contact Addresses are turned on.
+function getPeopleWithAddresses(capId)
+{
+	capPeopleArr = null;
+	var s_result = aa.people.getCapContactByCapID(capId);
+	if(s_result.getSuccess())
+	{
+		capPeopleArr = s_result.getOutput();
+		if(capPeopleArr != null || capPeopleArr.length > 0)
+		{
+			for (loopk in capPeopleArr)	
+			{
+				var capContactScriptModel = capPeopleArr[loopk];
+				var capContactModel = capContactScriptModel.getCapContactModel();
+				var peopleModel = capContactScriptModel.getPeople();
+				var contactAddressrs = aa.address.getContactAddressListByCapContact(capContactModel);
+				if (contactAddressrs.getSuccess())
+				{
+					var contactAddressModelArr = convertContactAddressModelArr(contactAddressrs.getOutput());
+					peopleModel.setContactAddressList(contactAddressModelArr);    
+				}
+			}
+		}
+		
+		else
+		{
+			capPeopleArr = null;
+		}
+	}
+	else
+	{
+		logDebug("ERROR: Failed to People: " + s_result.getErrorMessage());
+		capPeopleArr = null;	
+	}
+	return capPeopleArr;
+}
