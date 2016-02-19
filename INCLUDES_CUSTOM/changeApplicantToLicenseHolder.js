044a1a9febd3db804b8c42a4d00c8b554caa020e
Kevin Ford <kford@accela.com>
2016-02-19 15:50:42 -0800

    Update Function.
    Added a debug message to the script to change contact type to license
    holder.

--- INCLUDES_CUSTOM/changeApplicantToLicenseHolder.js
+++ INCLUDES_CUSTOM/changeApplicantToLicenseHolder.js
@@ -3,6 +3,7 @@ function changeApplicantToLicenseHolder(fromCType, toCType, licCapId) {
 	var conToChange = null; 
 	consResult = aa.people.getCapContactByCapID(licCapId);
 	if (consResult.getSuccess()) {
+		logDebug("Starting "+fromCType+" to "+toCType);
 		cons = consResult.getOutput();
 		for (thisCon in cons) {
 			if (cons[thisCon].getCapContactModel().getPeople().getContactType() == fromCType) { 
