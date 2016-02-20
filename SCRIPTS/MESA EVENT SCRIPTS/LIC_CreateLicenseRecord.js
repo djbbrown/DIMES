d397815db1127362a32e125579837cf7050e7927
Kevin Ford <kford@accela.com>
2016-02-19 16:38:40 -0800

    Update code for Create License Records

--- SCRIPTS/MESA EVENT SCRIPTS/LIC_CreateLicenseRecord.js
+++ SCRIPTS/MESA EVENT SCRIPTS/LIC_CreateLicenseRecord.js
@@ -18,7 +18,7 @@
 if (wfTask.equals("Issue License") && wfStatus.equals("Issued"))
 {
 	aa.print("Creating License Record");
-	// •	Create a child record of type License/*/*/License (where the record type and subtype are the same as the parent application record)
+	// Create a child record of type License/*/*/License (where the record type and subtype are the same as the parent application record)
 	// Need to get the type and sub-type broken out, the following format can be used appTypeArray[1]
 	license = createChild(appTypeArray[0],appTypeArray[1],appTypeArray[2],"License","License");
 	// Convert the Contact of type "Applicant" (This should be "License Applicant")
@@ -53,7 +53,7 @@ if (wfTask.equals("Issue License") && wfStatus.equals("Issued"))
 	lic.setExpiration(dateAdd(null,365)); // This will add 365 days to the expiration.
 
 	// Copy info from application to "License" according to standard choice EMSE:ASI Copy Exceptions.
-	// o	EMSE:ASI Copy Exceptions – contains the record type (in the “Standard Choices Value” field)
+	// -	EMSE:ASI Copy Exceptions – contains the record type (in the “Standard Choices Value” field)
 	//		along with a “|” delimited list ASI fields to exclude when copying the ASI (in the “Value Desc” field).
 	
 	// Get the standard choice
