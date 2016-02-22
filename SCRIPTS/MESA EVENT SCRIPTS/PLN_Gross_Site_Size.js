eb72489166393608df873ff1c5f276c11573d811
maryroseshultz <maryrose.schultz@mesaaz.gov>
2016-02-22 08:24:05 -0700

    Created Annual Facilities Expiration date.

--- SCRIPTS/MESA EVENT SCRIPTS/PLN_Gross_Site_Size.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PLN_Gross_Site_Size.js
@@ -21,13 +21,57 @@ var servProvCode=expression.getValue("$$servProvCode$$").value;
 var variable0=expression.getValue("ASI::SITE DATA::Gross Site Size (acres)");
 var variable1=expression.getValue("PARTPL::PARCEL INFORMATION::SIZE(ACRES)");
 var variable2=expression.getValue("ASI::SITE DATA::Gross Site Size (sqft)");
-var totalRowCount = expression.getTotalRowCount();
+var TotalAcres = number("TotalAcres") + ("PARTPL::PARCEL INFORMATION::SIZE(ACRES)") ;
 
-		if(variable0.value!=null && variable0.value*1==toPrecision(variable1.getValue() * 1 *43560) && variable2.value!=null && variable2.value*1==toPrecision(variable1.getValue() * 1 /43560)){
 
-			variable1.readOnly=true;
-		expression.setReturn(variable1);
-	}else{ 
-		variable1.readOnly=false;
-		expression.setReturn(variable1);
-	}
+
+    
+
+        if variable1.value!=null && variable1.value*1==toPrecision(variable1.getValue()
+             TotalAcres = TotalAcres();
+             continue
+
+//if (wfTask.equals("License Application") && wfStatus.equals("Received")) {
+//    // set ASI field
+//	editAppSpecific("Administrative Review Due", dateAdd(null, 10))
+//}
+
+//if (inspType == "Initial Inspection" && inspResult == "In Violation") {
+		
+//	var inspInterval = AInfo["Inspection Interval"];
+//	var daysOut = 14;
+//	if (inspInterval == "10 Days")
+//    daysOut = 10;
+//if (inspInterval == "7 Days")
+//    daysOut = 7;
+//if (inspInterval == "3 Days")
+//    daysOut = 5;
+	
+//scheduleInspection("Follow-Up Inspection", daysOut, "ADMIN", null, "ADMIN");
+//}
+
+
+//var parcel = "";
+//var v_b1-parcel_area = "";
+//var site = "MESA";
+//var v_b1_Per_ID1 = "REC15";
+//var v_b1_Per_ID2 = "00000";
+//var v_b1_Per_ID3 = "00067"; 
+
+//var B3ParcelResult = aa.parcel.getParceListForAdmin(parcel, b1_parcel_area);
+
+
+//if(B3Parcel.getSuccess())
+//    aa.print("There is parcel info.")
+//aa.print("B1-Parcel_area")
+//else
+//aa.print("There is no parcel info.");
+
+	//	if(variable0.value!=null && variable0.value*1==toPrecision(variable1.getValue() * 1 *43560) && variable2.value!=null && variable2.value*1==toPrecision(variable1.getValue() * 1 /43560)){
+
+	//		variable1.readOnly=true;
+	//	expression.setReturn(variable1);
+	//}else{ 
+	//	variable1.readOnly=false;
+	//	expression.setReturn(variable1);
+	//}
