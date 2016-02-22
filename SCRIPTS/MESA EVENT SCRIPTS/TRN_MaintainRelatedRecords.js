7e6278f0ce89a40daafecd2c390926a201ab0571
rgillaccela <rgill@accela.com>
2016-02-22 15:50:51 -0800

    Script to maintain Related Records

---
+++ SCRIPTS/MESA EVENT SCRIPTS/TRN_MaintainRelatedRecords.js
@@ -0,0 +1,28 @@
+/*===================================================================
+// Script Number: 054
+// Script Name:TRN_MaintainRelatedRecords.js
+// Script Developer: Raminder Gill
+// Script Agency: Accela
+// Script Description: Maintain related records - ROW and UTL Permit numbers
+// Script Run Event: ASA, ASIUA
+// Script Parents:
+//	IRSA;Transportation!Temporary Traffic Control!~!~ 
+//             
+/*==================================================================*/
+
+var rowPermit = getAppSpecific("ROW Permit No");
+var UtlPermit = getAppSpecific("UTL Permit No");
+
+//check if these values exist as permits
+if (rowPermit != null && rowPermit != "") {
+   if (!isParent(rowPermit)) {
+   		addParent(rowPermit)
+   }
+}
+
+if (UtlPermit != null && UtlPermit != "") {
+   if (!isParent(UtlPermit)) {
+   		addParent(UtlPermit)
+   }
+}
+
