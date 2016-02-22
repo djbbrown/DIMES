51c77df1986d0d6a176e0bfe06f1224c1dc6147e
maryroseshultz <maryrose.schultz@mesaaz.gov>
2016-02-22 08:27:37 -0700

    Trying to commit.

---
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_Annual_Facilities_Expiration.js
@@ -0,0 +1,24 @@
+/*===================================================================
+// Script Number: 119
+// Script Name: PMT_Annual_Facilities_Expiration.js
+// Script Developer: MaryRose Schultz
+// Script Agency: Mesa
+// Script Description: Update ASI "Permit Expiration Date"  based on WF task/status
+// Script Run Event: WTUA
+// Script Parents:
+//    WTUA;Permits!Commercial!Annual Facilities!NA
+//           
+//            
+/*==================================================================*/
+
+if (wfTask.equals("Application Subbmital/Review") && wfStatus.equals("Ready to Issue")) 
+{
+    // set ASI field
+    var d = new Date();
+    var curr_date = 31;
+    var curr_month = 12;
+    var curr_year = d.getFullYear();
+    
+    editAppSpecific("Permit Expiration Date", date(curr_month,"/", curr_date,"/",curr_year);
+}
+
