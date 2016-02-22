cc761663340e1fac7ca64f1aea9f76caf04e484b
maryroseshultz <maryrose.schultz@mesaaz.gov>
2016-02-22 13:04:51 -0700

    this event keeps disappearing #2.

--- SCRIPTS/EVENT/WTUA;Permits!Commercial!Annual Facilities!NA.js
+++ SCRIPTS/EVENT/WTUA;Permits!Commercial!Annual Facilities!NA.js
@@ -2,4 +2,4 @@
 // Script Number: 119
 // Script Name: WTUA;Permits!Commercial!Annual Facilities!NA.js
 /*==================================================================*/
-include("PMT_Annual_Facilities_Expiration");
+include("PMT_Annual_Facilities_Expiration"); 
