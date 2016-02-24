d971eaaddc1ac2e6792be257d675a09e1e30caea
rgillaccela <rgill@accela.com>
2016-02-24 09:08:33 -0800

    Pass CapID for addparent() functioninstead of ASI

--- SCRIPTS/MESA EVENT SCRIPTS/TRN_MaintainRelatedRecords.js
+++ SCRIPTS/MESA EVENT SCRIPTS/TRN_MaintainRelatedRecords.js
@@ -12,17 +12,24 @@
 var rowPermit = getAppSpecific("ROW Permit No.");
 var UtlPermit = getAppSpecific("UTL Permit No.");
 
+var rowPermitCapId = aa.cap.getCapID(rowPermit).getOutput();
+var UtlPermitCapId = aa.cap.getCapID(UtlPermit).getOutput();
+
 //check if these values exist as permits
 if (rowPermit != null && rowPermit != "") {
    if (!isParent(rowPermit)) {
-   		addParent(rowPermit)
+   		addParent(rowPermitCapId)
    }
 }
 
 if (UtlPermit != null && UtlPermit != "") {
    if (!isParent(UtlPermit)) {
-   		addParent(UtlPermit)
+   		addParent(UtlPermitCapId)
    }
 }
+
+
+
+
 
 
