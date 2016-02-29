1455b0a5efbb086744523c08ed348be664bf27b5
rgillaccela <rgill@accela.com>
2016-02-29 18:28:25 -0500

    Send Clearance Email

---
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_GasClearanceEmail.js
@@ -0,0 +1,37 @@
+/*===================================================================
+// Script Number: 102
+// Script Name: PMT_GasClearanceEmail.js 
+// Script Developer: Raminder Gill
+// Script Agency: Accela
+// Script Description: When Gas Pipe Final inspection is resulted with “Approved - Utility Clearance Required”
+// …send email to Company named in Utility Service Info ASIT, Clearance To. 
+// Script Run Event: IRSA
+// Script Parents:
+//  IRSA:Permits/Online/NA/NA
+//	IRSA:Permits/Residential/NA/NA
+//	IRSA:Permits/Residential/Mobile Home/NA/NA
+//	IRSA:Permits/Commercial/NA/NA
+/*==================================================================*/
+
+
+
+var vEParams = aa.util.newHashtable(); 
+addParameter(vEParams,"%%RECORD ID%%",capIDString);
+
+tmpTable = loasASITable("UTILITY SERVICE INFORMATION");
+countCityOfMesa = countASITRows(tmpTAble, "Clearance To", "City of Mesa");
+countSouthwestGas = countASITRows(tmpTAble, "Clearance To", "Southwest Gas");
+
+if (countCityOfMEsa > 0 ) {
+
+	addParameter(vEParams,"%%CLEARANCE TO%%",);
+	emailAddress = "rgill@accela.com";
+	//emailAddress = "customerinfobillingops@mesaaz.gov";
+	sendNotification("", emailAddress, "", "GAS CLEARANCE", vEParams, null, capId)
+}
+	if (countSouthwest > 0) {
+		addParameter(vEParams,"%%CLEARANCE TO%%",);
+		emailAddress = "rgill@accela.com";
+		//emailAddress = "gasinspectiontag@swgas.com";
+		sendNotification("", emailAddress, "", "GAS CLEARANCE", vEParams, null, capId)
+		}
