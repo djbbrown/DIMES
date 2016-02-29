01cbfa421d0e36db9b4a26772bc8083886e5952a
rgillaccela <rgill@accela.com>
2016-02-29 12:14:36 -0500

    testing

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_UtilityServiceFees.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_UtilityServiceFees.js
@@ -15,9 +15,16 @@
 var tmpTable = loadASITable("UTILITY SERVICE INFORMATION");  
 
 var tmpTable = loadASITable("UTILITY SERVICE INFORMATION");  
-if (tmpTable) {      
+if (tmpTable) { 
+//define variables to use
 var countGasServiceMeter = countASITRows(tmpTable, "Service Type", "Gas Service and Meter" );
 var countGasMeter = countASITRows(tmpTable, "Service Type", "Gas Meter" );
+var countGasServMeterCommercial = countASITRows(tmpTable, "Service Type", "Gas Service/Meter – Commercial" );
+var countGasServMeterResLarge = countASITRows(tmpTable, "Service Type", "Gas Service/Meter – Large Residential");
+var countGasRelocationRetrofit = countASITRows(tmpTable, "Service Type", "Gas  Relocation/Retrofit");
+
+
+logDebug(countGasServiceMeter);
 // Gas Service and Meter - USF010
 if (countGasServiceMeter == 0 && feeExists("USF010")) removeFee("USF010", "FINAL");
 if (countGasServiceMeter > 0) updateFee("USF010","PMT_UTL_SERV", "FINAL",  1, "N");
@@ -26,6 +33,12 @@ if (countGasMeter == 0 && feeExists("USF020")) removeFee("USF020", "FINAL");
 if (countGasMeter > 0) {
 	var sumQtyMeters = sumASITColumn(tmpTable, "Qty of Meters", "INCLUDE", "Service Type", "Gas Meter");
 	updateFee("USF020","PMT_UTL_SERV", "FINAL",  sumQtyMeters, "N");
+	}
+//Gas Service/Meter – Commercial, Gas Service/Meter – Large Residential,Gas  Relocation/Retrofit  - USF030
+if (countGasMeter == 0 && feeExists("USF020")) removeFee("USF020", "FINAL");
+if (countGasServMeterCommercial > 0 || countGasServMeterResLarge || countGasRelocationRetrofit ) {
+	//var sumQtyMeters = sumASITColumn(tmpTable, "Qty of Meters", "INCLUDE", "Service Type", "Gas Meter");
+	updateFee("USF030","PMT_UTL_SERV", "FINAL",  1, "N");
 	}
 
  }
