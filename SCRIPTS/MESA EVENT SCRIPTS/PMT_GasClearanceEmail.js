6696d4283586bfcbcc74f19e06c378711d713f1f
rgillaccela <rgill@accela.com>
2016-02-29 21:33:40 -0500

    Send Email Notification

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_GasClearanceEmail.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_GasClearanceEmail.js
@@ -14,21 +14,21 @@
 /*==================================================================*/
 
 if (inspType == "Gas Pipe Final" && inspResult == "Approved - Utl Clearance Req") {
-
 var vEParams = aa.util.newHashtable(); 
-addParameter(vEParams,"%%RECORD ID%%",capIDString);
 tmpTable = loadASITable("UTILITY SERVICE INFORMATION");
 countCityOfMesa = countASITRows(tmpTAble, "Clearance To", "City of Mesa");
 countSouthwestGas = countASITRows(tmpTAble, "Clearance To", "Southwest Gas");
 
 if (countCityOfMesa > 0 ) {
-	addParameter(vEParams,"%%CLEARANCE TO%%",capIDString);
+	addParameter(vEParams,"%%RECORD ID%%",capIDString);
+	addParameter(vEParams,"%%CLEARANCE TO%%","City of Mesa");
 	emailAddress = "rgill@accela.com";
 	//emailAddress = "customerinfobillingops@mesaaz.gov";
 	sendNotification("", emailAddress, "", "GAS CLEARANCE", vEParams, null, capId);
 }
-	if (countSouthwest > 0) {
-		addParameter(vEParams,"%%CLEARANCE TO%%",capIDString);
+	if (countSouthwestGas > 0) {
+		addParameter(vEParams,"%%RECORD ID%%",capIDString);
+		addParameter(vEParams,"%%CLEARANCE TO%%","Southwest Gas");
 		emailAddress = "rgill@accela.com";
 		//emailAddress = "gasinspectiontag@swgas.com";
 		sendNotification("", emailAddress, "", "GAS CLEARANCE", vEParams, null, capId);
