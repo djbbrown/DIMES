0e408cdd31f90a5d9d6ec89c4050e983d289e8cb
rgillaccela <rgill@accela.com>
2016-03-01 15:12:11 -0500

    Adding USF050

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_UtilityServiceFees.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_UtilityServiceFees.js
@@ -34,14 +34,8 @@ if (countGasMeter > 0) {
 	var sumQtyMeters = sumASITColumn(tmpTable, "Qty of Meters", "INCLUDE", "Service Type", "Gas Meter");
 	updateFee("USF020","PMT_UTL_SERV", "FINAL",  sumQtyMeters, "N");
 	}
-//Water Meter: Adapter - USF040
-//if (countGasMeterAdapter == 0 && feeExists("USF040")) removeFee("USF040", "FINAL");
-//if (countGasMeterAdapter > 0) {	
-//            		updateFee("USF040","PMT_UTL_SERV", "FINAL",  1, "N");	
- //       }
-//
+//Water Meter: Adapter - USF040  - Service Size - Water Meter Adapter A24
 
-//if (countGasMeterAdapter == 0 && feeExists("USF040")) removeFee("USF040", "FINAL");
 if (countGasMeterAdapter > 0) {
 	var tempSum=0;
 	for (var rowIndex in tmpTable) {
@@ -58,8 +52,26 @@ if (countGasMeterAdapter > 0) {
 	}
 }
 	
-//
+//USF050 Service Type - Water Meter: Domestic or Water Meter: Landscaping   
+// Service Size - Water - 3/4" or Water 1.0"
+
+if (countGasMeterAdapter > 0) {
+	var tempSum=0;
+	for (var rowIndex in tmpTable) {
+        thisRow = tmpTable[rowIndex];
+        if ((thisRow["Service Type"].fieldValue == "Water Meter: Domestic" || thisRow["Service Type"].fieldValue == "Water Meter: Landscaping") && (thisRow["Service Size"].fieldValue == 'Water - 3/4"' or thisRow["Service Size"].fieldValue == 'Water 1.0"'))  {    
+        	if (!isNaN(parseFloat(thisRow["Qty of Meters"].fieldValue)))
+        		tempSum = tempSum + parseFloat(thisRow["Qty of Meters"].fieldValue);		
+        }
+	}
+	if (tempSum > 0)
+		updateFee("USF050","PMT_UTL_SERV", "FINAL",  tempSum, "N");
+	else {
+		if (feeExists("USF050")) removeFee("USF040", "FINAL");
+	}
+}
 	
+//
  }
 
 
