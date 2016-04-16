a2a16663263b99b1a8765b40e195992f5d4abc8a
Kevin Ford <kford@accela.com>
2016-04-15 17:00:20 -0700

    Update Gas Clearance Email to send emails to kford@accela for testing.

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_GasClearanceEmail.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_GasClearanceEmail.js
@@ -11,25 +11,29 @@
 //	IRSA:Permits/Residential/NA/NA
 //	IRSA:Permits/Residential/Mobile Home/NA/NA
 //	IRSA:Permits/Commercial/NA/NA
+//
+// This script calls the notification template "GAS CLEARANCE"
 /*==================================================================*/
 
 if (inspType == "Gas Pipe Final" && inspResult == "Approved - Utl Clearance Req") {
-var vEParams = aa.util.newHashtable(); 
-var tmpTable = loadASITable("UTILITY SERVICE INFORMATION");  
-countCityOfMesa = countASITRows(tmpTable, "Clearance To", "City of Mesa");
-countSouthwestGas = countASITRows(tmpTable, "Clearance To", "Southwest Gas");
+	var vEParams = aa.util.newHashtable(); 
+	var tmpTable = loadASITable("UTILITY SERVICE INFORMATION");  
+	countCityOfMesa = countASITRows(tmpTable, "Clearance To", "City of Mesa");
+	countSouthwestGas = countASITRows(tmpTable, "Clearance To", "Southwest Gas");
 
-if (countCityOfMesa > 0 ) {
-	addParameter(vEParams,"$$RECORD ID$$",capIDString);
-	addParameter(vEParams,"$$CLEARANCE TO$$","City of Mesa");
-	emailAddress = "rgill@accela.com";
-	//emailAddress = "customerinfobillingops@mesaaz.gov";
-	sendNotification("", emailAddress, "", "GAS CLEARANCE", vEParams, null, capId);
-}
+	if (countCityOfMesa > 0 ) {
+		addParameter(vEParams,"$$RECORD ID$$",capIDString);
+		addParameter(vEParams,"$$CLEARANCE TO$$","City of Mesa");
+		//emailAddress = "rgill@accela.com";
+		emailAddress = "kford@accela.com";
+		//emailAddress = "customerinfobillingops@mesaaz.gov";
+		sendNotification("", emailAddress, "", "GAS CLEARANCE", vEParams, null, capId);
+	}
 	if (countSouthwestGas > 0) {
 		addParameter(vEParams,"$$RECORD ID$$",capIDString);
 		addParameter(vEParams,"$$CLEARANCE TO$$","Southwest Gas");
-		emailAddress = "rgill@accela.com";
+		//emailAddress = "rgill@accela.com";
+		emailAddress = "kford@accela.com";
 		//emailAddress = "gasinspectiontag@swgas.com";
 		sendNotification("", emailAddress, "", "GAS CLEARANCE", vEParams, null, capId);
 		}
