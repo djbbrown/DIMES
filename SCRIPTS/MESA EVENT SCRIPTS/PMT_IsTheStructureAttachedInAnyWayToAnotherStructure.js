751ddf1da4d02a88a1deb3d400babb4549e0bb20
Bryan de Jesus <bryan.dejesus@woolpert.com>
2016-04-27 17:37:20 -0600

    debugging PMT_IsTheStructureAttachedInAnyWayToAnotherStructure.js

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_IsTheStructureAttachedInAnyWayToAnotherStructure.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_IsTheStructureAttachedInAnyWayToAnotherStructure.js
@@ -11,9 +11,9 @@
 // Script Parents:
 //            ASB;Permitting!Demolition!NA!NA
 /*==================================================================*/
-
-if (getAppSpecific("Is the structure attached in any way to another structure?", capId) === "Yes") {
-	showMessage = true; 
-	comment("The applicant must apply for a Remodel permit when demolishing a structure that is attached to another structure."); 
-	cancel = true;
-}
+logDebug(getAppSpecific("Is the structure attached in any way to another structure?"));
+//if (getAppSpecific("Is the structure attached in any way to another structure?", capId) === "Yes") {
+//	showMessage = true; 
+//	comment("The applicant must apply for a Remodel permit when demolishing a structure that is attached to another structure."); 
+//	cancel = true;
+//}
