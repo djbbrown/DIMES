93ea4dceeea30e242a3c87bd78554d4ad7cc9bf1
Bryan de Jesus <bryan.dejesus@woolpert.com>
2016-04-27 17:05:59 -0600

    Added event scripts for Script 107
    New scripts:
    ASB;Permits!Demolition!NA!NA.js
    PMT_IsTheStructureAttachedInAnyWayToAnotherStructure.js

---
+++ SCRIPTS/EVENT/ASB;Permits!Demolition!NA!NA.js
@@ -0,0 +1,5 @@
+/*===================================================================
+// Script Number: 001
+// Script Name: ASB;Permits!Demolition!NA!NA.js
+/*==================================================================*/
+include("PMT_IsTheStructureAttachedInAnyWayToAnotherStructure");
---
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_IsTheStructureAttachedInAnyWayToAnotherStructure.js
@@ -0,0 +1,19 @@
+/*===================================================================
+// Script Number: 107
+// Script Name: PMT_IsTheStructureAttachedInAnyWayToAnotherStructure.js
+// Script Developer: Bryan de Jesus
+// Script Agency: Woolpert
+// Script Description: If ASI field “Is the structure attached in any 
+// 		way to another structure?” is “Yes” then applicant should not 
+//		be allowed to submit application. Instead they would need to 
+//		apply for a Remodel permit.
+// Script Run Event: ASB
+// Script Parents:
+//            ASB;Permitting!Demolition!NA!NA
+/*==================================================================*/
+
+if (getAppSpecific("Is the structure attached in any way to another structure?") === "Yes") {
+	showMessage = true; 
+	comment("The applicant must apply for a Remodel permit when demolishing a structure that is attached to another structure."); 
+	cancel = true;
+}
