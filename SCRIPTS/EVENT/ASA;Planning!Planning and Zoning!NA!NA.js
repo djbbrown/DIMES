5fea6222989d1ad4b13ccc7b491ba93e9bd52540
Bryan de Jesus <bryan.dejesus@woolpert.com>
2016-04-29 09:35:25 -0600

    added event scripts for script #86

--- SCRIPTS/EVENT/ASA;Planning!Planning and Zoning!NA!NA.js
+++ SCRIPTS/EVENT/ASA;Planning!Planning and Zoning!NA!NA.js
@@ -1,2 +1,3 @@
 //
 include("PLN_ParcelNoAddress");
+include("PLN_AnnexationFees"); // added by Bryan de Jesus
---
+++ SCRIPTS/MESA EVENT SCRIPTS/PLN_AnnexationFees.js
@@ -0,0 +1,17 @@
+/*===================================================================
+// Script Number: 86
+// Script Name: PLN Annexation Fees
+// Script Developer: Bryan de Jesus
+// Script Agency: Woolpert
+// Script Description: On application submitttal Assess fee “Rezoning”  
+//		(FP030) at application submittal when Address and Parcel are 
+//		located outside of the “downtown” district (downtown layer 
+//		currently exists in GIS)
+//		If inside Downtown district, “Rezoning Downtown” fee should be assessed instead. 
+// Script Run Event: ASA
+// Script Parents:
+//            ASA;Planning!Annexation!NA!NA  
+//            ASA;Planning!Planning and Zoning!NA!NA 
+/*==================================================================*/
+
+logDebug("GIS Info for Zoning Description: " + getGISInfo("Planning/Zoning", "Zoning Districts", "Description"));
