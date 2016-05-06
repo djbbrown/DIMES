b51d45a50d6df6fa4f6b40217198da17e6747809
Bryan de Jesus <bryan.dejesus@woolpert.com>
2016-05-06 11:48:09 -0600

    debugging PMT_SignFees

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_SignFees.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_SignFees.js
@@ -3,16 +3,72 @@
 // Script Name: PMT_SignFees.js
 // Script Developer: Bryan de Jesus
 // Script Agency: Woolpert
-// Script Description: “Ready to Issue” status is applied to “Plans Coordination” wf task - When status of  “Ready to Issue” status is applied to “Plans Coordination” wf task then Calculate “Sign Permit” Fee” using base fee of $102.40 and adding  3% of total sign valuation (value entered into ASI field “Total Sign Valuation”) and also adding $0.30 per square foot (include total square feet in multiple ASIT fields “Sign Square Footage”)
+// Script Description: SGN050 Banner Fee If Type of Work = “Grand Opening Banners”
+// 	$110 per banner $110 * Quantity in ASIT where Type of Work = “Grand Opening Banners” Enter 1 TECH
+// 	SGN060 Subdivision Sign If Type of Work = “Subdivision Sign”
+//	$110 per sign $110 * Quantity in ASIT where Type of Work = “Subdivision Sign” Enter 1 TECH
+// 	SGN070 Weekend Directional Sign Fee If Type of Work = “Subdivision Weekend Sign”
+//
+//	$550 per sign $550 * Quantity in ASIT where Type of Work = “Subdivision Weekend Sign” Enter 1 TECH
+//	SGN080 Subdivision Directional Signs If Type of Work = “Subdivision Directional Sign”
+//
+//	$110 per sign $110 * Quantity in ASIT where Type of Work = “Subdivision Directional Sign” Enter 1 TECH
+//	SGN090 Downtown Directional A-Frames If Type of Work = “Downtown Directional A-Frames”
+//
+//	$25 each $25 * Quantity in ASIT where Type of Work = “Downtown Directional A-Frames” Enter 1 TECH
 // Script Run Event: ASA, ASIUA
 // Script Parents:
 //            ASA;Permits!Sign!NA!NA
 //			  ASIUA;Permits!Sign!NA!NA
 /*==================================================================*/
 showDebug = true;
+// constants
+var FEE_CODES = {
+		GRAND_OPENING_BANNER: "SGN050",
+		SUBDIVISION: "SGN060",
+		SUBDIVISION_WEEKEND: "SGN070",
+		SUBDIVISION_DIRECTIONAL: "SGN080",
+		DOWNTOWN_DIRECTIONAL_AFRAME: "SGN090"
+};
+var FEE_SCHEDULE = "PMT_SIGNS";
+var FEE_PERIOD = "FINAL";
+var QUANTITY_COLUMN_NAME = "Quantity";
+var SIGN_TYPE_COLUMN_NAME = "Type of Work";
+var SIGN_TYPES = {
+		GRAND_OPENING_BANNER = "Grand Opening Banners",
+		SUBDIVISION = "Subdivision Sign",
+		SUBDIVISION_WEEKEND = "Subdivision Weekend Sign",
+		SUBDIVISION_DIRECTIONAL = "Subdivision Directional Sign",
+		DOWNTOWN_DIRECTIONAL_AFRAME = "Downtown Directional A-Frames"
+};
+
+// load ASIT
 var t = loadASITable("SIGN INFO");
 if (!!t){
-	logDebug(t[0]["Quantity"] + " " + t[0]["Type of Work"]);
+	// loop through rows
+	for (index in t){
+		var row = t[index];
+		logDebug("Assessing fees for " + row[QUANTITY_COLUMN_NAME] + " " + row[SIGN_TYPE_COLUMN_NAME]);
+		
+		// apply fee
+		if (row[SIGN_TYPE_COLUMN_NAME] == SIGN_TYPES.GRAND_OPENING_BANNER){
+			if (feeExists(FEE_CODES.GRAND_OPENING_BANNER, "INVOICED")) voidRemoveFee(FEE_CODES.GRAND_OPENING_BANNER);
+			updateFee(FEE_CODES.GRAND_OPENING_BANNER, FEE_SCHEDULE, FEE_PERIOD, row[QUANTITY_COLUMN_NAME], "N");
+		} else if (row[SIGN_TYPE_COLUMN_NAME] == SIGN_TYPES.SUBDIVISION){
+			if (feeExists(FEE_CODES.SUBDIVISION, "INVOICED")) voidRemoveFee(FEE_CODES.SUBDIVISION);
+			updateFee(FEE_CODES.SUBDIVISION, FEE_SCHEDULE, FEE_PERIOD, row[QUANTITY_COLUMN_NAME], "N");
+		} else if (row[SIGN_TYPE_COLUMN_NAME] == SIGN_TYPES.SUBDIVISION_WEEKEND){
+			if (feeExists(FEE_CODES.SUBDIVISION_WEEKEND, "INVOICED")) voidRemoveFee(FEE_CODES.SUBDIVISION_WEEKEND);
+			updateFee(FEE_CODES.SUBDIVISION_WEEKEND, FEE_SCHEDULE, FEE_PERIOD, row[QUANTITY_COLUMN_NAME], "N");
+		} else if (row[SIGN_TYPE_COLUMN_NAME] == SIGN_TYPES.SUBDIVISION_DIRECTIONAL){
+			if (feeExists(FEE_CODES.SUBDIVISION_DIRECTIONAL, "INVOICED")) voidRemoveFee(FEE_CODES.SUBDIVISION_DIRECTIONAL);
+			updateFee(FEE_CODES.SUBDIVISION_DIRECTIONAL, FEE_SCHEDULE, FEE_PERIOD, row[QUANTITY_COLUMN_NAME], "N");
+		} else if (row[SIGN_TYPE_COLUMN_NAME] == SIGN_TYPES.DOWNTOWN_DIRECTIONAL_AFRAME){
+			if (feeExists(FEE_CODES.DOWNTOWN_DIRECTIONAL_AFRAME, "INVOICED")) voidRemoveFee(FEE_CODES.DOWNTOWN_DIRECTIONAL_AFRAME);
+			updateFee(FEE_CODES.DOWNTOWN_DIRECTIONAL_AFRAME, FEE_SCHEDULE, FEE_PERIOD, row[QUANTITY_COLUMN_NAME], "N");
+		}
+	}
+	
 } else {
 	logDebug("Did not find table 'SIGN INFO'");
 }
