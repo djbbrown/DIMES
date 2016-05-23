df1e1431e8264548893d70c63534e85f7d8ab001
Kevin Ford <kford@accela.com>
2016-05-23 08:17:02 -0700

    Adding LIC_Substantive Review Due Date and it is ready to test.

--- SCRIPTS/EVENT/WTUA;Licenses!General!~!Application.js
+++ SCRIPTS/EVENT/WTUA;Licenses!General!~!Application.js
@@ -6,4 +6,5 @@ include("LIC_AppealDeadlineDateSet");
 include("LIC_AdministrativeReviewDueDateSet");
 include("LIC_AssessLicenseFees");
 include("LIC_CreateLicenseRecord"); // Added by Kevin Ford
+include("LIC_SubstantiveReviewDueDateSet"); // Added by Kevin Ford
 include("LIC_AdjustSubstantiveReviewDueDate"); // Added by Raminder Gill=======
---
+++ SCRIPTS/MESA EVENT SCRIPTS/LIC_SubstantiveReviewDueDate.js
@@ -0,0 +1,33 @@
+/*===================================================================
+// Script Number: 3
+// Script Name: LIC Substantive Review Due Date - Set
+// Script Developer: Kevin Ford
+// Script Agency: Accela
+// Script Description:
+// 		LIC Set the 'Substantive Review Due' ASI field to the current date plus
+// 		60 calendar days when the "License Application" task is set to a status of "Complete" 
+// Script Run Event:
+//		Workflow Task Update After (WTUA)
+// Script Parents:
+//		WTUA;Licenses!General!~!Application
+/*==================================================================*/
+
+// Under this line create the function that will need to run at script runtime.
+// the function will be called in the event ("AppSubmitAfter") major event.
+// Need to make sure which record types have "Substantive Review Due Date".
+if(
+		// Most Licenses
+		(wfTask == "License Application" && wfStatus == "Complete")
+		// Peddler/OffTrackBetting ("Substantive Review Due" field exists)
+		|| (wfTask == "License Application" && wfStatus == "Completed")
+		
+		// The following must be checked.
+		// Bingo Hall - No Status (No "Substantive Review Due" field)
+		// Livestock - No Status (No "Substantive Review Due" field)
+		// Special Event - No Status (No "Substantive Review Due" field)
+)
+{
+	// Update "Substantive Review Due" to current date + 60
+	// get 
+	editAppSpecific("Substantive Review Due", dateAdd(null, 60));
+}
