6581a351953cf7fe7c0702e4e9b48f484c36332a
Lynda Wacht <lwacht@users.noreply.github.com>
2016-06-13 07:18:03 -0600

    id-199

--- SCRIPTS/EVENT/ASA;Enforcement!~!~!~.js
+++ SCRIPTS/EVENT/ASA;Enforcement!~!~!~.js
@@ -1,2 +1,11 @@
 // Script ASA;Enforcement!~!~!~.js
 include("ENF_populateCitationNumber");
+
+//ID-199: On ASA and ASIUA, check the rows in the VIOLATION INFORMATION ASIT and create a row in the CITATION CHECKLIST ASIT for each. The column Citation Number in the VIOLATION INFORMATION ASIT should be used to populate the same column in the CITATION CHECKLIST ASIT.
+try{
+	if(matches(appTypeArray[2],"Case", "Environmental")){
+		updateViolationInfoCustomList();
+	}
+}catch (err){
+	logDebug("A JavaScript Error occurred: ASA:Enforcement/*/*/*: ID-199: " + err.message);
+}
