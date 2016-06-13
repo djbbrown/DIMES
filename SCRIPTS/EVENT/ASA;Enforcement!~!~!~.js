963699077feeb44f77804b17cb327c141250ba17
Lynda Wacht <lwacht@users.noreply.github.com>
2016-06-13 08:46:09 -0600

    debug

--- SCRIPTS/EVENT/ASA;Enforcement!~!~!~.js
+++ SCRIPTS/EVENT/ASA;Enforcement!~!~!~.js
@@ -3,7 +3,7 @@ include("ENF_populateCitationNumber");
 
 //ID-199: On ASA and ASIUA, check the rows in the VIOLATION INFORMATION ASIT and create a row in the CITATION CHECKLIST ASIT for each. The column Citation Number in the VIOLATION INFORMATION ASIT should be used to populate the same column in the CITATION CHECKLIST ASIT.
 try{
-	if(matches(appTypeArray[2],"Case", "Environmental")){
+	if(matches(appTypeArray[1],"Case", "Environmental")){
 		updateViolationInfoCustomList();
 	}
 }catch (err){
