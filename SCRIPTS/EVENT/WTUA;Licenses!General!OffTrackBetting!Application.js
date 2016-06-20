5ea635a24899623f34b48c0642f580a8f922bb2b
rgillaccela <rgill@accela.com>
2016-06-20 16:11:57 -0400

    Corrected the script name in include statement

--- SCRIPTS/EVENT/WTUA;Licenses!General!OffTrackBetting!Application.js
+++ SCRIPTS/EVENT/WTUA;Licenses!General!OffTrackBetting!Application.js
@@ -1,6 +1,7 @@
 /*===================================================================
 // Script Name: WTUA;Licenses!General!OffTrackBetting!Application.js
+                     Licenses/General/OffTrackBetting/Application
 /*==================================================================*/
 
-include("LIC_AdministrativeReviewDueDateSet2"); // Added by Raminder Gill=======
+include("LIC_AdministrativeReviewDueDateSet_2"); // Added by Raminder Gill=======
 
