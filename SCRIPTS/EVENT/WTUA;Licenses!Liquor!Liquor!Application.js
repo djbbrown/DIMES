36f9d33e2ba983cebfe9b4ebf8f9ceac3e200d62
rgillaccela <rgill@accela.com>
2016-06-20 19:42:09 -0400

    Script 087

---
+++ SCRIPTS/EVENT/WTUA;Licenses!Liquor!Liquor!Application.js
@@ -0,0 +1,6 @@
+/*===================================================================
+// Script Name: WTUA;Licenses!Liquor!~!Application.js
+/*==================================================================*/
+
+include("LIC_AdministrativeReviewDueDateSet_2"); // Added by Raminder Gill=======
+
---
+++ SCRIPTS/EVENT/WTUA;Licenses!Liquor!Liquor!Renewal.js
@@ -0,0 +1,5 @@
+/*===================================================================
+// Script Name: WTUA;Licenses!Liquor!Liquor!Renewal.js
+/*==================================================================*/
+
+include("LIC_AssessLicenseFees"); // Added by Raminder Gill=======
