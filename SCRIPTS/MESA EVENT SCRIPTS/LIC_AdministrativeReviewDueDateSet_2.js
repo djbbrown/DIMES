7b879ec5fff19f7de4871aa64be06a2e5c43be2b
rgillaccela <rgill@accela.com>
2016-06-20 13:55:39 -0400

    Adding 14 days

--- SCRIPTS/MESA EVENT SCRIPTS/LIC_AdministrativeReviewDueDateSet_2.js
+++ SCRIPTS/MESA EVENT SCRIPTS/LIC_AdministrativeReviewDueDateSet_2.js
@@ -12,6 +12,5 @@
 
 if ((wfTask.equals("License Application") || wfTask.equals("Application Intake")) && wfStatus.equals("Received")) {
 	// set ASI field
-	editAppSpecific("Administrative Review Due", null)
-	editAppSpecific("Administrative Review Due", dateAdd(null, 14))
+	editAppSpecific("Administrative Review Due", dateAdd(null, 14));
 }
