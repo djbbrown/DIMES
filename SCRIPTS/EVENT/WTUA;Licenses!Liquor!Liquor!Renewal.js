487100ba7be5363f4c965e3dfa516ce356c834e4
rgillaccela <rgill@accela.com>
2016-06-20 20:01:40 -0400

    correct script name

--- SCRIPTS/EVENT/WTUA;Licenses!Liquor!Liquor!Renewal.js
+++ SCRIPTS/EVENT/WTUA;Licenses!Liquor!Liquor!Renewal.js
@@ -2,4 +2,4 @@
 // Script Name: WTUA;Licenses!Liquor!Liquor!Renewal.js
 /*==================================================================*/
 
-include("LIC_AssessLicenseFees"); // Added by Raminder Gill=======
+include("LIC_LiquorLicenseFee"); // Added by Raminder Gill=======
