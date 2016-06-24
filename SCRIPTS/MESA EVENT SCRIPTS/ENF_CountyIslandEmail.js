49a026224a1c790d3e0ed1412f0099368dd6dc5c
MesaDimes <brian.odell@mesaaz.gov>
2016-06-24 15:52:56 -0700

    v1.0

---
+++ SCRIPTS/MESA EVENT SCRIPTS/ENF_CountyIslandEmail.js
@@ -0,0 +1,25 @@
+﻿//*===================================================================
+// Script Number: 40
+// Script Name: ENF_CountyIslandEmail.js 
+// Script Developer: Brian O'Dell
+// Script Agency: Mesa
+// Script Description: 
+//                  Send an email to the contact of type "Complainant" using the email template "ENF_COUNTY_ISLAND".
+//
+// Script Run Event: IRSA
+// Script Parents:
+//  IRSA:Enforcement/Case/NA/NA
+//
+//==================================================================*
+
+var fromEmail = "noreply@MesaAz.gov";
+var emailAddress = "Brian.ODell@mesaaz.gov";
+var vEParams = aa.util.newHashtable(); 
+//var tmpTable = loadASITable("UTILITY SERVICE INFORMATION");  To", "Southwest Gas");
+
+addParameter(vEParams,"$$RECORD ID$$",capIDString);
+//addParameter(vEParams,"$$CLEARANCE TO$$","Southwest Gas");
+//emailAddress = "Lauren.Lupica@MesaAZ.gov";
+//emailAddress = "";
+sendNotification(fromEmail, emailAddress, "", "ENF_COUNTY_ISLAND", vEParams, null, capId);
+
