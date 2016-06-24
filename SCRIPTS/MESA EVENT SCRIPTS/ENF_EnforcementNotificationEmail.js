45d554a86e9bdc24db5d1a0471c5e4017181b3bd
MesaDimes <brian.odell@mesaaz.gov>
2016-06-24 14:57:28 -0700

    v1.0

---
+++ SCRIPTS/MESA EVENT SCRIPTS/ENF_EnforcementNotificationEmail.js
@@ -0,0 +1,26 @@
+﻿/*===================================================================
+// Script Number: 36
+// Script Name: ENF_EnforcementNotificationEmail.js 
+// Script Developer: Brian O'Dell
+// Script Agency: Mesa
+// Script Description: 
+//              Send email to contact of type complaintant on record creation 
+//
+// Script Run Event: ASA
+// Script Parents:
+//      ASA:Enforcement/Case/NA/NA
+//
+// This script calls the notification template "ENF RECORD OPENED"
+/*==================================================================*/
+
+var fromEmail = "noreply@MesaAz.gov";
+var vEParams = aa.util.newHashtable(); 
+var theAddress = var addressA = aa.address.getAddressByCapId(capId).getOutput();	
+
+addParameter(vEParams,"$$RECORD ID$$",capIDString);
+addParameter(vEParams,"$$THE ADDRESS$$",theAddress);
+emailAddress = "Brian.ODell@mesaaz.gov";
+//emailAddress = "Lauren.Lupica@MesaAZ.gov";
+//emailAddress = "customerinfobillingops@mesaaz.gov";
+sendNotification(fromEmail, emailAddress, "", "ENF RECORD OPENED", vEParams, null, capId);
+
