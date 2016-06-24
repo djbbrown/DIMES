ab35e3f8161343bd563d80bb10cb213e35d9a184
Kevin Ford <kford@accela.com>
2016-06-24 16:20:27 -0700

    Update Script Template.

--- SCRIPTS/MESA EVENT SCRIPTS/000_ScriptTemplate.js
+++ SCRIPTS/MESA EVENT SCRIPTS/000_ScriptTemplate.js
@@ -8,7 +8,7 @@
 // Script Parents:
 //            ASA;Planning!Group Home-Daycare!Application!NA  (example)
 //            ASA;Licensing!General!ParkAndSwap!NA  (example)
-/*==================================================================*/
+===================================================================*/
 
 // Under this line create the function that will need to run at script runtime.
 // the function will be called in the event ("AppSubmitAfter") major event.
