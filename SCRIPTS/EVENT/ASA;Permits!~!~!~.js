9f4661f7a1be2b8c8112b5316e843a6373ba27b3
dhoops <dhoops@accela.com>
2016-06-28 09:46:52 -0400

    New

--- SCRIPTS/EVENT/ASA;Permits!~!~!~.js
+++ SCRIPTS/EVENT/ASA;Permits!~!~!~.js
@@ -1,1 +1,2 @@
 include("PMT_ParcelWarning");
+include("PMT_Hold_Flags")
---
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_Hold_Flags.js
@@ -0,0 +1,25 @@
+tagField = "" + getGISInfo("Accela/AccelaTAGS", "Accela_TAGS", "Accela_TAGS.TAG");
+logDebug("tag attribute = " + tagField);
+switch (tagField) {
+	case "LGAC": addStdCondition("Building Permit", "Large Acreage - NOI"); addStdCondition("Building Permit", "Large Acreage - Dust Control"); 
+	break;
+	case "SMAC":  addStdCondition("Building Permit", "Small Acreage - Dust Control");
+	break;
+	case "CITR":  addStdCondition("Building Permit", "Citrus Grove Sub Area");
+	break;
+	case "SCAL":  addStdCondition("Building Permit", "Scalloped Street Assessment");
+	break;
+	case "FALC":  addStdCondition("Building Permit", "Falcon Field");
+	break;
+	case "PMGA":  addStdCondition("Building Permit", "PMG Airport Approval Required");
+	break;
+	case "PMGU":  addStdCondition("Building Permit", "PMG Fire Utility Fee");
+	break;
+	case "DSUP":  addStdCondition("Building Permit", "Desert Uplands");
+	break;
+	case "HIST":  addStdCondition("Building Permit", "Historical District");
+	break;
+	case "OUTM":  addStdCondition("Building Permit", "Outside City of Mesa"); 
+	break;
+	default: break;
+}
