1f3dcbcb832345b714d7bb1dcea2af53f42ac593
MesaDimes <brian.odell@mesaaz.gov>
2016-06-28 12:29:26 -0700

    Tuesday test 1

--- SCRIPTS/MESA EVENT SCRIPTS/ENF_EnforcementNotificationEmail.js
+++ SCRIPTS/MESA EVENT SCRIPTS/ENF_EnforcementNotificationEmail.js
@@ -1,6 +1,6 @@
-﻿//*===================================================================
+//*===================================================================
 // Script Number: 36
-// Script Name: ENF_EnforcementNotificationEmail.js 
+// Script Name: ENF_EnforcementNotificationEmail.js
 // Script Developer: Brian O'Dell
 // Script Agency: Mesa
 // Script Description: 
@@ -24,4 +24,3 @@ addParameter(vEParams,"$$THE ADDRESS$$",theAddress);
 //emailAddress = "Lauren.Lupica@MesaAZ.gov";
 //emailAddress = "customerinfobillingops@mesaaz.gov";
 sendNotification(fromEmail, emailAddress, "", "ENF RECORD OPENED", vEParams, null, capId);
-
