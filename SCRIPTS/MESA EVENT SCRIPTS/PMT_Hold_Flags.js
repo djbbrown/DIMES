d3ee9d1a7917b428b266be8c9c18a064efaa0cdd
dhoops <dhoops@accela.com>
2016-06-28 16:42:43 -0400

    Check for null

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_Hold_Flags.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_Hold_Flags.js
@@ -1,27 +1,29 @@
 	tagFieldArray = getGISInfoArray("Accela/AccelaTAGS", "Accela_TAGS", "Accela_TAGS.TAG");
-	for (tIndex in tagFieldArray) {
-		thisTag = tagFieldArray[tIndex];
-		switch (thisTag) {
-			case "LGAC": addStdCondition("Building Permit", "Large Acreage - NOI"); addStdCondition("Building Permit", "Large Acreage - Dust Control"); 
-				break;
-			case "SMAC":  addStdCondition("Building Permit", "Small Acreage - Dust Control");
-				break;
-			case "CITR":  addStdCondition("Building Permit", "Citrus Grove Sub Area");
-				break;
-			case "SCAL":  addStdCondition("Building Permit", "Scalloped Street Assessment");
-				break;
-			case "FALC":  addStdCondition("Building Permit", "Falcon Field");
-				break;
-			case "PMGA":  addStdCondition("Building Permit", "PMG Airport Approval Required");
-				break;
-			case "PMGU":  addStdCondition("Building Permit", "PMG Fire Utility Fee");
-				break;
-			case "DSUP":  addStdCondition("Building Permit", "Desert Uplands");
-				break;
-			case "HIST":  addStdCondition("Building Permit", "Historical District");
-				break;
-			case "OUTM":  addStdCondition("Building Permit", "Outside City of Mesa"); 
-				break;
-			default: break;
+	if (tagField && tagField.length > 0) {
+		for (tIndex in tagFieldArray) {
+			thisTag = tagFieldArray[tIndex];
+			switch (thisTag) {
+				case "LGAC": addStdCondition("Building Permit", "Large Acreage - NOI"); addStdCondition("Building Permit", "Large Acreage - Dust Control"); 
+					break;
+				case "SMAC":  addStdCondition("Building Permit", "Small Acreage - Dust Control");
+					break;
+				case "CITR":  addStdCondition("Building Permit", "Citrus Grove Sub Area");
+					break;
+				case "SCAL":  addStdCondition("Building Permit", "Scalloped Street Assessment");
+					break;
+				case "FALC":  addStdCondition("Building Permit", "Falcon Field");
+					break;
+				case "PMGA":  addStdCondition("Building Permit", "PMG Airport Approval Required");
+					break;
+				case "PMGU":  addStdCondition("Building Permit", "PMG Fire Utility Fee");
+					break;
+				case "DSUP":  addStdCondition("Building Permit", "Desert Uplands");
+					break;
+				case "HIST":  addStdCondition("Building Permit", "Historical District");
+					break;
+				case "OUTM":  addStdCondition("Building Permit", "Outside City of Mesa"); 
+					break;
+				default: break;
+			}
 		}
 	}	
