d80ab3a454a941787fce57620fed41aa234d77c3
Bryan de Jesus <bryan.dejesus@woolpert.com>
2016-06-28 22:34:00 -0600

    debugging PMT_ImpactFeesSingleResidence

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_ImpactFeesSingleResidence.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_ImpactFeesSingleResidence.js
@@ -36,7 +36,7 @@ try {
 			if (feeExists("RDIF320", "NEW", "INVOICED")) voidRemoveFee("RDIF320");
 			if (feeExists("RDIF020", "NEW", "INVOICED")) voidRemoveFee("RDIF020");
 			if (feeExists("RDIF070", "NEW", "INVOICED")) voidRemoveFee("RDIF070");
-			// if the fee qty has changed on an invoiced fee, void the fee
+			// if the fee qty has changed, void the fee
 			if (feeExists("RDIF160", "NEW", "INVOICED") && feeQty("RDIF160") != fireQty) voidRemoveFee("RDIF160");
 			if (feeExists("RDIF210", "NEW", "INVOICED") && feeQty("RDIF210") != publicSafetyQty) voidRemoveFee("RDIF210");
 			if (feeExists("RDIF260", "NEW", "INVOICED") && feeQty("RDIF260") != stormWaterQty) voidRemoveFee("RDIF260");
@@ -44,12 +44,12 @@ try {
 			if (feeExists("RDIF010", "NEW", "INVOICED") && feeQty("RDIF010") != waterQty) voidRemoveFee("RDIF010");
 			if (feeExists("RDIF060", "NEW", "INVOICED") && feeQty("RDIF060") != wasteWaterQty) voidRemoveFee("RDIF060");
 			// assess the fee
-			if (!feeExists("RDIF160")) addFee("RDIF160", "PMT_RDIF", "FINAL", fireQty, "N");
-			if (!feeExists("RDIF210")) addFee("RDIF210", "PMT_RDIF", "FINAL", publicSafetyQty, "N");
-			if (!feeExists("RDIF260")) addFee("RDIF260", "PMT_RDIF", "FINAL", stormWaterQty, "N");
-			if (!feeExists("RDIF310")) addFee("RDIF310", "PMT_RDIF", "FINAL", resDevQty, "N");
-			if (!feeExists("RDIF010")) addFee("RDIF010", "PMT_RDIF", "FINAL", waterQty, "N");
-			if (!feeExists("RDIF060")) addFee("RDIF060", "PMT_RDIF", "FINAL", wasteWaterQty, "N");
+			if (!feeExists("RDIF160") && fireQty > 0) addFee("RDIF160", "PMT_RDIF", "FINAL", fireQty, "N");
+			if (!feeExists("RDIF210") && publicSafetyQty > 0) addFee("RDIF210", "PMT_RDIF", "FINAL", publicSafetyQty, "N");
+			if (!feeExists("RDIF260") && stormWaterQty > 0) addFee("RDIF260", "PMT_RDIF", "FINAL", stormWaterQty, "N");
+			if (!feeExists("RDIF310") && resDevQty > 0) addFee("RDIF310", "PMT_RDIF", "FINAL", resDevQty, "N");
+			if (!feeExists("RDIF010") && waterQty > 0) addFee("RDIF010", "PMT_RDIF", "FINAL", waterQty, "N");
+			if (!feeExists("RDIF060") && wasteWaterQty > 0) addFee("RDIF060", "PMT_RDIF", "FINAL", wasteWaterQty, "N");
 		} else if (classification == "Single Family-Attached (per dwelling unit)"){
 			// remove any fees from previous classification
 			if (feeExists("RDIF160", "NEW", "INVOICED")) voidRemoveFee("RDIF160");
@@ -58,7 +58,7 @@ try {
 			if (feeExists("RDIF310", "NEW", "INVOICED")) voidRemoveFee("RDIF310");
 			if (feeExists("RDIF010", "NEW", "INVOICED")) voidRemoveFee("RDIF010");
 			if (feeExists("RDIF060", "NEW", "INVOICED")) voidRemoveFee("RDIF060");			
-			// if the fee qty has changed on an invoiced fee, void the fee
+			// if the fee qty has changed, void the fee
 			if (feeExists("RDIF170", "NEW", "INVOICED") && feeQty("RDIF170") != fireQty) voidRemoveFee("RDIF170");
 			if (feeExists("RDIF220", "NEW", "INVOICED") && feeQty("RDIF220") != publicSafetyQty) voidRemoveFee("RDIF220");
 			if (feeExists("RDIF270", "NEW", "INVOICED") && feeQty("RDIF270") != stormWaterQty) voidRemoveFee("RDIF270");
@@ -66,12 +66,12 @@ try {
 			if (feeExists("RDIF020", "NEW", "INVOICED") && feeQty("RDIF020") != waterQty) voidRemoveFee("RDIF020");
 			if (feeExists("RDIF070", "NEW", "INVOICED") && feeQty("RDIF070") != wasteWaterQty) voidRemoveFee("RDIF070");
 			// assess the fee
-			if (!feeExists("RDIF170")) addFee("RDIF170", "PMT_RDIF", "FINAL", fireQty, "N");
-			if (!feeExists("RDIF220")) addFee("RDIF220", "PMT_RDIF", "FINAL", publicSafetyQty, "N");
-			if (!feeExists("RDIF270")) addFee("RDIF270", "PMT_RDIF", "FINAL", stormWaterQty, "N");
-			if (!feeExists("RDIF320")) addFee("RDIF320", "PMT_RDIF", "FINAL", resDevQty, "N");
-			if (!feeExists("RDIF020")) addFee("RDIF020", "PMT_RDIF", "FINAL", waterQty, "N");
-			if (!feeExists("RDIF070")) addFee("RDIF070", "PMT_RDIF", "FINAL", wasteWaterQty, "N");
+			if (!feeExists("RDIF170") && fireQty > 0) addFee("RDIF170", "PMT_RDIF", "FINAL", fireQty, "N");
+			if (!feeExists("RDIF220") && publicSafetyQty > 0) addFee("RDIF220", "PMT_RDIF", "FINAL", publicSafetyQty, "N");
+			if (!feeExists("RDIF270") && stormWaterQty > 0) addFee("RDIF270", "PMT_RDIF", "FINAL", stormWaterQty, "N");
+			if (!feeExists("RDIF320") && resDevQty > 0) addFee("RDIF320", "PMT_RDIF", "FINAL", resDevQty, "N");
+			if (!feeExists("RDIF020") && waterQty > 0) addFee("RDIF020", "PMT_RDIF", "FINAL", waterQty, "N");
+			if (!feeExists("RDIF070") && wasteWaterQty > 0) addFee("RDIF070", "PMT_RDIF", "FINAL", wasteWaterQty, "N");
 		}
 	}
 } catch (err){
