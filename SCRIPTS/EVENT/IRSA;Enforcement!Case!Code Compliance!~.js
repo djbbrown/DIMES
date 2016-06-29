8f51add29682086c65e63f7f62b555b7b5d0b746
Victor Staggs <victor.staggs@woolpert.com>
2016-06-29 09:48:50 -0400

    Add include statement for ENF_InViolationInspectionLimitReachedUpdateWFStatus.js

--- SCRIPTS/EVENT/IRSA;Enforcement!Case!Code Compliance!~.js
+++ SCRIPTS/EVENT/IRSA;Enforcement!Case!Code Compliance!~.js
@@ -1,3 +1,4 @@
 // Script IRSA;Enforcement!Case!Code Compliance!~.js
 include("ENF_CitationIssuedWFUpdate");
 include("ENF_ScheduleAdditionalCitationInspection");
+include("ENF_InViolationInspectionLimitReachedUpdateWFStatus");
