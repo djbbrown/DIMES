dc18c3818eec3492ccb9924acf00fc268ab09f15
MesaDimes <brian.odell@mesaaz.gov>
2016-06-29 08:13:52 -0700

    testing v1

---
+++ SCRIPTS/MESA EVENT SCRIPTS/TRA_SetApplicationDate.js
@@ -0,0 +1,38 @@
+//*===================================================================
+//
+// Script Number: 280
+// Script Name: TRA_SetApplicationDate.js
+// Script Developer: Brian O'Dell
+// Script Agency: City of Mesa
+// Script Description: 
+// 		Permit application date will be auto filled with current date 
+//		unless application is submitted after 11AM, then application 
+//		date will be for the next City Business Day
+//
+// Script Run Event: ASA
+// Script Parents:
+//             ASA:Transportation/*/*/*
+// 
+//==================================================================*/
+
+try
+{
+  var today = new Date();
+  var currentTime = new Date().getHours();
+
+  if (currentTime >= 11)
+  {
+    today.setDate(today.getDate() + 1);
+  }
+
+  editAppSpecific("Application Date",jsDateToASIDate(today),capId)
+}
+catch (err)
+{
+  logDebug("A JavaScript Error occured: " + err.message);
+}
+
+
+
+
+
