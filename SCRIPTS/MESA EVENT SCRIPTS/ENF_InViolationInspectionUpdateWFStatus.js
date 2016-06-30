f63941ebf7849ad99b56dd01324b7ba674f2382f
Kevin Ford <kford@accela.com>
2016-06-30 10:07:28 -0700

    Update to script 47 so that it doesn't get duplicated with script 21 being programmed by Woolpert.

--- SCRIPTS/MESA EVENT SCRIPTS/ENF_InViolationInspectionUpdateWFStatus.js
+++ SCRIPTS/MESA EVENT SCRIPTS/ENF_InViolationInspectionUpdateWFStatus.js
@@ -10,8 +10,40 @@
 //	IRSA;Enforcement!Environmental!~!~ 
 //            
 /*==================================================================*/
+var inViolationInspectionScriptModels = [];
 
-if ((inspType == "Initial Inspection" || inspType == "Follow-Up Inspection" || inspType == "Follow-up Inspection") && inspResult == "In Violation") {
+var getInspectionsResult = aa.inspection.getInspections(capId);
+
+if (getInspectionsResult.getSuccess()) {
+
+	var inspectionScriptModels = getInspectionsResult.getOutput();
+	var inspectionScriptModel = null;
+
+	for (inspectionScriptModelIndex in inspectionScriptModels) {
+		inspectionScriptModel = inspectionScriptModels[inspectionScriptModelIndex];
+		if (
+			(inspectionScriptModel.getInspectionType().toUpperCase() == "FOLLOW-UP INSPECTION"
+				||inspectionScriptModel.getInspectionType().toUpperCase() == "INITIAL INSPECTION"
+			)
+			&& (inspectionScriptModel.getInspectionStatus().toUpperCase() == "IN VIOLATION")
+		) {
+			inViolationInspectionScriptModels.push(inspectionScriptModel);
+		}
+	}
+	aa.print("inViolationInspectionScriptModels.length:" + inViolationInspectionScriptModels.length);
+	logDebug("inViolationInspectionScriptModels.length:" + inViolationInspectionScriptModels.length);
+}
+// Only do the update like this if there is less than 3 inspections,
+// Else we want to pick up the script with the woolpert developed scirpt 021
+if (
+	(
+		inspType == "Initial Inspection"
+		|| inspType == "Follow-Up Inspection"
+		|| inspType == "Follow-up Inspection"
+	)
+	&& inspResult == "In Violation"
+	&& inViolationInspectionScriptModels.length < 3
+) {
 	if( isTaskActive("Initial Inspection") ) {
 		branchTask("Initial Inspection","In Violation","Closed by Script","Closed by Script");
 	}
@@ -21,6 +53,4 @@ if ((inspType == "Initial Inspection" || inspType == "Follow-Up Inspection" || i
 	if( isTaskActive("Citation Inspections") ) {
 		updateTask("Citation Inspections","In Violation","Updated by Script","Updated by Script");
 	}
-	
 }
-
