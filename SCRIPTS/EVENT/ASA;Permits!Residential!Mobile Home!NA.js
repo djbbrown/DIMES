72fc43cd31d9ebaa153d2ce4c623cda1c49a18d7
Kevin Ford <kford@accela.com>
2016-06-30 10:53:06 -0700

    Update to remove "include("PMT_SignalButteTag");" as this is already in the parent that covers all Residential.

--- SCRIPTS/EVENT/ASA;Permits!Residential!Mobile Home!NA.js
+++ SCRIPTS/EVENT/ASA;Permits!Residential!Mobile Home!NA.js
@@ -1,9 +1,3 @@
-/*===================================================================
-// Script Number:
-// Script Name: ASA;Permits!Residential!Mobile Home!NA
-==================================================================*/
-
-include("PMT_SignalButteTag");
 include("PMT_assessFeeRDIF140OrRDIF150");
 include("PMT_FloodControlPermit");
 include("PMT_WaterMeterAdapterFee");
