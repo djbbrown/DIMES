dccf2e908dea6bd6715442fca68d441953349a12
Bryan de Jesus <bryan.dejesus@woolpert.com>
2016-06-30 12:38:00 -0600

    test parcel access

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_OneTenthAcre.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_OneTenthAcre.js
@@ -12,7 +12,16 @@
 ===================================================================*/
 showDebug = true;
 try{
-	var partialCapId = getIncompleteCapId();
+	var s_id1 = aa.env.getValue("PermitId1");
+	var s_id2 = aa.env.getValue("PermitId2");
+	var s_id3 = aa.env.getValue("PermitId3");
+
+	var result = aa.cap.getCapIDModel(s_id1, s_id2, s_id3);
+	var partialCapId;
+	if(result.getSuccess()) {
+		partialCapId = result.getOutput();
+	}  
+	else { logDebug("ERROR: Failed to get capId: " + result.getErrorType() + " " + result.getErrorMessage()); }
 	if (!!partialCapId){
 		var parcelResult = aa.parcel.getParcelByCapId(partialCapId, null);
 		if (parcelResult.getSuccess()){
