5755cf46b8369d2b7a209a96c15c5670e2b8a95b
dhoops <dhoops@accela.com>
2016-06-30 15:39:26 -0400

    test

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_Hold_Flags.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_Hold_Flags.js
@@ -7,7 +7,6 @@
 // Script Run Event: WTUA
 // Script Parents: WTUA:Permits/*/*/*
 /*==================================================================*/
-
 tagFieldArray = getGISInfoArray("Accela/AccelaTAGS", "Accela_TAGS", "Accela_TAGS.TAG");
     
 	if (tagFieldArray && tagFieldArray.length > 0) {
