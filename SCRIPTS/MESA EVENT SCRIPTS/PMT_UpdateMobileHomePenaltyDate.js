334f43d05c3b0fd3435de39c9c43c50e669d79fa
MesaDimes <brian.odell@mesaaz.gov>
2016-06-30 09:24:23 -0700

    v2.0

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_UpdateMobileHomePenaltyDate.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_UpdateMobileHomePenaltyDate.js
@@ -19,11 +19,16 @@ try
 {
   var wfTaskMatch = isTaskStatus("Application Submittal", "Accepted - Plan Review Req");
   var theDate = new Date();
+  var planReviewPenaltyDate = getAppSpecific["Plan Review Penalty Date"];
+  var theStatusDate = "" + getStatusDateinTaskHistory("Application Submittal", "Accepted - Plan Review Req");
+  var tempDate = "";
 
-  if (wfTaskMatch)
+  if ((planReviewPenaltyDate == null) && (wfTaskMatch))
   {
-    theDate = new Date(getStatusDateinTaskHistory("Application Submittal", "Accepted - Plan Review Req"));
-    theDate = new Date(dateAdd(theDate,10,'Y')); 
+    tempDate = theStatusDate.substring(0,10);
+    var datePieces = tempDate.split('-');
+
+    theDate = new Date(datePieces[1] + "/" + datePieces[2] + "/" + datePieces[0]);
 
     editAppSpecific("Plan Review Penalty Date",jsDateToASIDate(theDate),capId);
   }
