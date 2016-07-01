37f6178e3722d24802f697dcfd7328cde390a177
Bryan de Jesus <bryan.dejesus@woolpert.com>
2016-07-01 15:52:18 -0600

    added script for script #97

--- SCRIPTS/EVENT/WTUA;Permits!Commercial!NA!NA.js
+++ SCRIPTS/EVENT/WTUA;Permits!Commercial!NA!NA.js
@@ -3,3 +3,4 @@
 // Script Name: WTUA;Permits!Commercial!NA!NA.js
 ==================================================================*/
 include("PMT_ResubmittalFee");
+include("PMT_AssessTechFee"); // must be run last
