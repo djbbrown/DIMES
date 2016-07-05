d64a21ff7624e56a9aa3bd37e5cb8d2bf03f7852
MesaDimes <brian.odell@mesaaz.gov>
2016-07-05 10:40:40 -0700

    capture the error

--- SCRIPTS/EVENT/ASA;Permits!Residential!Mobile Home!NA.js
+++ SCRIPTS/EVENT/ASA;Permits!Residential!Mobile Home!NA.js
@@ -4,7 +4,7 @@ include("PMT_WaterMeterAdapterFee");
 // add by Brian O'Dell (Mesa)
 include("PMT_MobileHomeUtilityServiceFeeGasMeter");
 include("PMT_MobileHomeUtilityServiceFeeGasServiceOther");
-//include("PMT_MobileHomeFireImpactFee");  // Fee Schedule has changed
+include("PMT_MobileHomeFireImpactFee");  // Fee Schedule has changed
 include("PMT_MobileHomeStormwaterImpactFee");
 include("PMT_R5AreaCalc");
 include("PMT_R5NAreaCalc");
