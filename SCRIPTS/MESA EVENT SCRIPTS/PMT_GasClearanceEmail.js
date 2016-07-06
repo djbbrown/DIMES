4a2a90d3a6a377f720b092beb4067685de3c2fef
Kevin Ford <kford@accela.com>
2016-07-06 09:48:27 -0700

    Update trying to remove illegal character.

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_GasClearanceEmail.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_GasClearanceEmail.js
@@ -7,7 +7,7 @@
 // …send email to Company named in Utility Service Info ASIT, Clearance To. 
 // Script Run Event: IRSA
 // Script Parents:
-//  IRSA:Permits/Online/NA/NA
+//	IRSA:Permits/Online/NA/NA
 //	IRSA:Permits/Residential/NA/NA
 //	IRSA:Permits/Residential/Mobile Home/NA/NA
 //	IRSA:Permits/Commercial/NA/NA
@@ -16,20 +16,20 @@
 ==================================================================*/
 
 var fromEmail = 'noreply@MesaAz.gov';
-if (inspType == "Gas Pipe Final" && inspResult == "Approved - Utl Clearance Req") {
+if (inspType == "Gas Pipe Final" && inspResult == "Approved - Utl Clearance Req"){
 	var vEParams = aa.util.newHashtable();
 	var tmpTable = loadASITable("UTILITY SERVICE INFORMATION");
 	countCityOfMesa = countASITRows(tmpTable, "Clearance To", "City of Mesa");
 	countSouthwestGas = countASITRows(tmpTable, "Clearance To", "Southwest Gas");
 
-	if (countCityOfMesa > 0 ) {
+	if(countCityOfMesa>0){
 		addParameter(vEParams,"$$RECORD ID$$",capIDString);
 		addParameter(vEParams,"$$CLEARANCE TO$$","City of Mesa");
 		emailAddress = 'Lauren.Lupica@MesaAZ.gov';
 		//emailAddress = 'customerinfobillingops@mesaaz.gov';
 		sendNotification(fromEmail, emailAddress, "", "GAS CLEARANCE", vEParams, null, capId);
 	}
-	if (countSouthwestGas > 0) {
+	if(countSouthwestGas>0){
 		addParameter(vEParams,"$$RECORD ID$$",capIDString);
 		addParameter(vEParams,"$$CLEARANCE TO$$","Southwest Gas");
 		emailAddress = 'Lauren.Lupica@MesaAZ.gov';
