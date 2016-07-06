e881a3992189dbcf7dc38c47eaf706aaae81c67d
Kevin Ford <kford@accela.com>
2016-07-06 13:16:36 -0700

    Update Gas Clearance

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_GasClearanceEmail.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_GasClearanceEmail.js
@@ -1,16 +1,2 @@
-﻿/*===================================================================
+﻿// ==================================================================
 // Script Number: 102
-// Script Name: PMT_GasClearanceEmail.js 
-// Script Developer: Raminder Gill
-// Script Agency: Accela
-// Script Description: When Gas Pipe Final inspection is resulted with "Approved - Utility Clearance Required"
-// send email to Company named in Utility Service Info ASIT, Clearance To. 
-// Script Run Event: IRSA
-// Script Parents:
-//	IRSA:Permits/Online/NA/NA
-//	IRSA:Permits/Residential/NA/NA
-//	IRSA:Permits/Residential/Mobile Home/NA/NA
-//	IRSA:Permits/Commercial/NA/NA
-//
-// This script calls the notification template "GAS CLEARANCE"
-==================================================================*/
