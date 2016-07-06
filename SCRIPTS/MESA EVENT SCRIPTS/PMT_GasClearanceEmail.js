55bdc70112818c998170a3939df0e046f1578313
Kevin Ford <kford@accela.com>
2016-07-06 13:11:51 -0700

    Gas Cleance Email again.

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_GasClearanceEmail.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_GasClearanceEmail.js
@@ -1,4 +1,20 @@
-﻿var fromEmail = 'noreply@MesaAz.gov';
+﻿/*===================================================================
+// Script Number: 102
+// Script Name: PMT_GasClearanceEmail.js 
+// Script Developer: Raminder Gill
+// Script Agency: Accela
+// Script Description: When Gas Pipe Final inspection is resulted with "Approved - Utility Clearance Required"
+// send email to Company named in Utility Service Info ASIT, Clearance To. 
+// Script Run Event: IRSA
+// Script Parents:
+//	IRSA:Permits/Online/NA/NA
+//	IRSA:Permits/Residential/NA/NA
+//	IRSA:Permits/Residential/Mobile Home/NA/NA
+//	IRSA:Permits/Commercial/NA/NA
+//
+// This script calls the notification template "GAS CLEARANCE"
+==================================================================*/
+var fromEmail = "noreply@MesaAz.gov";
 if (inspType == "Gas Pipe Final" && inspResult == "Approved - Utl Clearance Req"){
 	var vEParams = aa.util.newHashtable();
 	var tmpTable = loadASITable("UTILITY SERVICE INFORMATION");
@@ -8,14 +24,14 @@ if (inspType == "Gas Pipe Final" && inspResult == "Approved - Utl Clearance Req"
 	if(countCityOfMesa>0){
 		addParameter(vEParams,"$$RECORD ID$$",capIDString);
 		addParameter(vEParams,"$$CLEARANCE TO$$","City of Mesa");
-		emailAddress = 'Lauren.Lupica@MesaAZ.gov';
+		emailAddress = "Lauren.Lupica@MesaAZ.gov";
 		//emailAddress = 'customerinfobillingops@mesaaz.gov';
 		sendNotification(fromEmail, emailAddress, "", "GAS CLEARANCE", vEParams, null, capId);
 	}
 	if(countSouthwestGas>0){
 		addParameter(vEParams,"$$RECORD ID$$",capIDString);
 		addParameter(vEParams,"$$CLEARANCE TO$$","Southwest Gas");
-		emailAddress = 'Lauren.Lupica@MesaAZ.gov';
+		emailAddress = "Lauren.Lupica@MesaAZ.gov";
 		//emailAddress = 'gasinspectiontag@swgas.com';
 		sendNotification(fromEmail, emailAddress, "", "GAS CLEARANCE", vEParams, null, capId);
 		}
