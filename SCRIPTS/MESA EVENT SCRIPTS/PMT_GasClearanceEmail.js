2833e24baed972b4817eb4f99f63bc678721c70c
Kevin Ford <kford@accela.com>
2016-07-06 09:42:44 -0700

    Update on GasClearance to try removing illegal character.

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_GasClearanceEmail.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_GasClearanceEmail.js
@@ -15,30 +15,25 @@
 // This script calls the notification template "GAS CLEARANCE"
 /*==================================================================*/
 
-
 var fromEmail = 'noreply@MesaAz.gov';
 if (inspType == "Gas Pipe Final" && inspResult == "Approved - Utl Clearance Req") {
-	var vEParams = aa.util.newHashtable(); 
-	var tmpTable = loadASITable("UTILITY SERVICE INFORMATION");  
+	var vEParams = aa.util.newHashtable();
+	var tmpTable = loadASITable("UTILITY SERVICE INFORMATION");
 	countCityOfMesa = countASITRows(tmpTable, "Clearance To", "City of Mesa");
 	countSouthwestGas = countASITRows(tmpTable, "Clearance To", "Southwest Gas");
 
 	if (countCityOfMesa > 0 ) {
 		addParameter(vEParams,"$$RECORD ID$$",capIDString);
 		addParameter(vEParams,"$$CLEARANCE TO$$","City of Mesa");
-		//emailAddress = "rgill@accela.com";
-		//emailAddress = "kford@accela.com";
-		emailAddress = "Lauren.Lupica@MesaAZ.gov";
-		//emailAddress = "customerinfobillingops@mesaaz.gov";
+		emailAddress = 'Lauren.Lupica@MesaAZ.gov';
+		//emailAddress = 'customerinfobillingops@mesaaz.gov';
 		sendNotification(fromEmail, emailAddress, "", "GAS CLEARANCE", vEParams, null, capId);
 	}
 	if (countSouthwestGas > 0) {
 		addParameter(vEParams,"$$RECORD ID$$",capIDString);
 		addParameter(vEParams,"$$CLEARANCE TO$$","Southwest Gas");
-		//emailAddress = "rgill@accela.com";
-		//emailAddress = "kford@accela.com";
-		emailAddress = "Lauren.Lupica@MesaAZ.gov";
-		//emailAddress = "gasinspectiontag@swgas.com";
+		emailAddress = 'Lauren.Lupica@MesaAZ.gov';
+		//emailAddress = 'gasinspectiontag@swgas.com';
 		sendNotification(fromEmail, emailAddress, "", "GAS CLEARANCE", vEParams, null, capId);
 		}
 }	
