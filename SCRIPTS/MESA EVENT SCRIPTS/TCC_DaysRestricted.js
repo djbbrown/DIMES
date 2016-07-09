25c8440184735c28b538287a85387ed73e06414a
Kevin Ford <kford@accela.com>
2016-07-08 17:34:23 -0700

    Update of TCC_DaysRestrited.js test data is missing so it will have to be tested next week.

---
+++ SCRIPTS/MESA EVENT SCRIPTS/TCC_DaysRestricted.js
@@ -0,0 +1,36 @@
+//*===================================================================
+// Script Number: 287
+// Script Name: TCC_DaysRestricted
+// Script Developer: Kevin Ford
+// Script Agency: Accela
+// Script Description: 
+//
+// Script Run Event: ASA / ASIUA
+// Script Parents:
+//		ASA:Transportation/*/*/*
+//		ASIUA;Transportation/*/*/*
+//==================================================================*/
+
+try
+{
+  loadASITables();
+  var tInfo = DURATIONINFORMATION;
+  var rowCount = DURATIONINFORMATION.length;
+  var durationDays = 0;
+  var x = 0;
+  // Get the row count
+  if (rowCount > 0)
+  {
+	  // Loop through each of the rows getting the "" and creating a sum.
+    for (x=0;x<rowCount;x++)
+    {
+      durationDays = durationDays + tInfo[x]["Days Restricted"];
+    }
+
+    editAppSpecific("Total Days Restricted",durationDays,capId);
+  }
+}
+catch (err)
+{
+  logDebug("A JavaScript Error occured: " + err.message);
+}
