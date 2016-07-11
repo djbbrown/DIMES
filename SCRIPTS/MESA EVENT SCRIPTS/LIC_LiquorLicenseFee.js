dea3dccb1ac50adbff9c7ed07b9328189cca73af
Kevin Ford <kford@accela.com>
2016-07-11 14:05:38 -0700

    Update to series that can run for LIC_LiquorLicenseFee.js

--- SCRIPTS/MESA EVENT SCRIPTS/LIC_LiquorLicenseFee.js
+++ SCRIPTS/MESA EVENT SCRIPTS/LIC_LiquorLicenseFee.js
@@ -20,12 +20,12 @@ var valSeries = getAppSpecific("Series Type");
 // Annual Fee's
 if (wfTask == "Issue License" && wfStatus == "Ready To Issue"){
 	//Annual Fee Series 1-4,8 and 13
-	if ((valSeries== "1" ||  valSeries== "2" || valSeries== "3" || valSeries== "4" || valSeries== "13") && !feeExists("L030")) 
+	if ((valSeries== "1" ||  valSeries== "2" || valSeries== "3" || valSeries== "4" || valSeries== "8" || valSeries== "11" || valSeries== "12" || valSeries== "13") && !feeExists("L030")) 
 	{
 		addFee("L030","LIC_LIQUOR", "FINAL",  1, "N");
 	}
 	// Annual Fee Series 6,7,8,10,14
-	if ((valSeries== "6" ||  valSeries== "7" || valSeries== "8" || valSeries== "10" || valSeries== "14") && !feeExists("L040")) 
+	if ((valSeries== "6" ||  valSeries== "7" || valSeries== "9" || valSeries== "10" || valSeries== "14") && !feeExists("L040")) 
 	{
 		addFee("L040","LIC_LIQUOR", "FINAL",  1, "N");
 	}
@@ -33,12 +33,12 @@ if (wfTask == "Issue License" && wfStatus == "Ready To Issue"){
 // 
 if (wfTask == "Renewal Intake" && wfStatus == "Submitted"){
 	//Application Fee
-	if ((valSeries== "1" ||  valSeries== "2" || valSeries== "3" || valSeries== "4" || valSeries== "13") && !feeExists("L010")) 
+	if ((valSeries== "1" ||  valSeries== "2" || valSeries== "3" || valSeries== "4" || valSeries== "8" || valSeries== "11" || valSeries== "12" || valSeries== "13") && !feeExists("L010")) 
 	{
 		addFee("L010","LIC_LIQ_RNWL", "FINAL",  1, "N");
 	}
 	//Issuance Fee
-	if ((valSeries== "6" ||  valSeries== "7" || valSeries== "8" || valSeries== "10" || valSeries== "14") && !feeExists("L020")) 
+	if ((valSeries== "6" ||  valSeries== "7" || valSeries== "9" || valSeries== "10" || valSeries== "14") && !feeExists("L020")) 
 	{
 		addFee("L020","LIC_LIQ_RNWL", "FINAL",  1, "N");
 	}
