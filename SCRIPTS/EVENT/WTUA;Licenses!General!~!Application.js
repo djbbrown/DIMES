e6d4c86c6fe7a34c0d2c8e118bbc26a678966513
Kevin Ford <kford@accela.com>
2016-07-12 15:48:03 -0700

    Updated to remove substantive review date setting.

--- SCRIPTS/EVENT/WTUA;Licenses!General!~!Application.js
+++ SCRIPTS/EVENT/WTUA;Licenses!General!~!Application.js
@@ -6,6 +6,5 @@ include("LIC_AppealDeadlineDateSet");
 include("LIC_AdministrativeReviewDueDateSet");
 include("LIC_AssessLicenseFees");
 include("LIC_CreateLicenseRecord"); // Added by Kevin Ford
-include("LIC_SubstantiveReviewDueDate"); // Added by Kevin Ford
 include("LIC_AdjustSubstantiveReviewDueDate"); // Added by Raminder Gill=======
 include("LIC_AdjustAdministrativeReviewDueDate"); // Added by Mike Linscheid
