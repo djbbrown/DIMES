9efa4944c479d5299b6e7060a637d1a23a7dc34b
MesaDimes <brian.odell@mesaaz.gov>
2016-07-12 08:08:00 -0700

    testing 20160712-2

--- SCRIPTS/EVENT/ASIUA;Permits!Residential!Mobile Home!NA.js
+++ SCRIPTS/EVENT/ASIUA;Permits!Residential!Mobile Home!NA.js
@@ -7,7 +7,7 @@ include("PMT_assessFeeRDIF140OrRDIF150");
 include("PMT_WaterMeterAdapterFee");
 // add by Brian O'Dell (Mesa)
 include("PMT_MobileHomeUtilityServiceFeeGasMeter");
-//include("PMT_MobileHomeUtilityServiceFeeGasServiceOther");
+include("PMT_MobileHomeUtilityServiceFeeGasServiceOther");
 //include("PMT_MobileHomeFireImpactFee");  // Fee schedule has changed
 //include("PMT_MobileHomeStormwaterImpactFee");
 //include("PMT_R5AreaCalc");
