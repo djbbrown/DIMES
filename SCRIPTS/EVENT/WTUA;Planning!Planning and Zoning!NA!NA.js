1283bb2d3ff60de9be852002472d80827c7f60cb
ewylam <ewylam@accela.com>
2016-07-13 12:13:42 -0700

    Added for Calendering Demo

---
+++ SCRIPTS/EVENT/WTUA;Planning!Planning and Zoning!NA!NA.js
@@ -0,0 +1,22 @@
+if (wfTask == 'Substantive Review Distribution' && wfStatus == 'Distributed') {
+	var vMeetingBody = lookup('SetDueDates:Planning/Planning and Zoning/NA/NA', 'Meeting Body');
+	var vToday = new Date();
+	var vDateFrom;
+	var vDateTo;
+	var vSubmittalBuffer = lookup('SetDueDates:Planning/Planning and Zoning/NA/NA', 'Submittal Buffer (Days)');
+	var vTotalTimeTillHearing = vSubmittalBuffer + lookup('SetDueDates:Planning/Planning and Zoning/NA/NA', 'Time Till Hearing (Days)'); ;
+
+	vDateFrom = new Date(dateAdd(vToday, vSubmittalBuffer));
+	vDateTo = new Date(dateAdd(vToday, vTotalTimeTillHearing));
+
+	vDateFrom = aa.date.getScriptDateTime(vDateFrom);
+	vDateTo = aa.date.getScriptDateTime(vDateTo);
+
+	var vScheduledDate = scheduleMeeting_Mesa(vMeetingBody, vDateFrom, vDateTo, capId);
+	vScheduledDate = vScheduledDate.toLocaleString();
+
+	logDebug("Scheduled Date: " + vScheduledDate);
+	
+	//Set Due Dates
+	setWFDueDate_Mesa('SetDueDates:Planning/Planning and Zoning/NA/NA', vScheduledDate);
+}
