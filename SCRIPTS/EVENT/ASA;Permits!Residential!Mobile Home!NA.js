3fbd04528b67d7bfdd1ef811cff7296f05ce1d3a
Bryan de Jesus <bryan.dejesus@woolpert.com>
2016-07-18 10:46:00 -0600

    add scripts for script #147

--- SCRIPTS/EVENT/ASA;Permits!Residential!Mobile Home!NA.js
+++ SCRIPTS/EVENT/ASA;Permits!Residential!Mobile Home!NA.js
@@ -2,6 +2,9 @@ include("PMT_assessFeeRDIF140OrRDIF150");
 include("PMT_FloodControlPermit");
 include("PMT_WaterMeterAdapterFee");
 
+// added by Bryan de Jesus (Woolpert)
+include("PMT_MobileHomeSolidWasteImpactFee");
+
 // add by Brian O'Dell (Mesa)
 include("PMT_MobileHomeUtilityServiceFeeGasMeter");
 //include("PMT_MobileHomeUtilityServiceFeeGasServiceOther");  // recommended removal of this script
