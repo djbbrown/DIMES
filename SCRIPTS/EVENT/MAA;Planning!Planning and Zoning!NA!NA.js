462b1d38fbbdfe3bf946e484fa49b9203c602986
ewylam <ewylam@accela.com>
2016-07-20 12:50:14 -0700

    New Calendaring Functionality

---
+++ SCRIPTS/EVENT/MAA;Planning!Planning and Zoning!NA!NA.js
@@ -0,0 +1,10 @@
+if (wfTask == 'Substantive Review Distribution' && wfStatus == 'Distributed') {
+	var vMeeting = aa.calendar.getEventByEventID(CalendarID, TargetMeetingID).getOutput();
+	var x = 0;
+	vScheduledDate = vMeeting.getStartDate();
+	
+	logDebug("Scheduled Date: " + vScheduledDate);
+	
+	//Set Due Dates
+	setWFDueDate_Mesa('SetDueDates:Planning/Planning and Zoning/NA/NA', vScheduledDate);
+}
