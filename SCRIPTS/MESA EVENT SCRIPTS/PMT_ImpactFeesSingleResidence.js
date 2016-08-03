4a4ea84da40d407e18718862de19133c00f49e56
Christopher <cisensano@yahoo.com>
2016-08-03 15:43:32 -0700

    Added requirements from script 220 to check GIS on RDIF Detached Fees

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_ImpactFeesSingleResidence.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_ImpactFeesSingleResidence.js
@@ -16,10 +16,23 @@
 // Script Parents:
 //            ASA;Residential!NA!NA
 //            ASIUA;Residential!NA!NA
+// 
+// Notes: Added requirements from script 220 to check GIS on RDIF Detached Fees
 /*==================================================================*/
 //showDebug = true;
 try {
 	var classification = AInfo["Classification"];
+	var wmqGisTag = false;
+	var swGisTag = false;
+	tagFieldArray = getGISInfoArray("Accela/AccelaTAGS", "Accela_TAGS", "Accela_TAGS.TAG");
+	if (tagFieldArray && tagFieldArray.length > 0) {
+	   for (tIndex in tagFieldArray) {
+			thisTag = tagFieldArray[tIndex];
+			logDebug(thisTag);
+			if(matches(thisTag, "ASU", "ASUE", "AWCP")) wmqGisTag = true;
+			if(matches(thisTag, "STOR")) swGisTag = true;
+	   }
+	}
 	if (!classification) {
 		logDebug("Classification not found. No impact fees assessed.");
 		// remove any fees from previous classification
@@ -61,9 +74,9 @@ try {
 			// assess the fee
 			if (!feeExists("RDIF160") && !!fireQty && fireQty > 0) addFee("RDIF160", "PMT_RDIF", "FINAL", fireQty, "N");
 			if (!feeExists("RDIF210") && !!publicSafetyQty && publicSafetyQty > 0) addFee("RDIF210", "PMT_RDIF", "FINAL", publicSafetyQty, "N");
-			if (!feeExists("RDIF260") && !!stormWaterQty && stormWaterQty > 0) addFee("RDIF260", "PMT_RDIF", "FINAL", stormWaterQty, "N");
+			if (!feeExists("RDIF260") && !!stormWaterQty && stormWaterQty > 0 && swGisTag == false) addFee("RDIF260", "PMT_RDIF", "FINAL", stormWaterQty, "N");
 			if (!feeExists("RDIF310") && !!resDevQty && resDevQty > 0) addFee("RDIF310", "PMT_RDIF", "FINAL", resDevQty, "N");
-			if (!feeExists("RDIF010") && !!waterQty && waterQty > 0) addFee("RDIF010", "PMT_RDIF", "FINAL", waterQty, "N");
+			if (!feeExists("RDIF010") && !!waterQty && waterQty > 0 && wmqGisTag == false) addFee("RDIF010", "PMT_RDIF", "FINAL", waterQty, "N");
 			if (!feeExists("RDIF060") && !!wasteWaterQty && wasteWaterQty > 0) addFee("RDIF060", "PMT_RDIF", "FINAL", wasteWaterQty, "N");
 		} else if (classification == "Single Family-Attached (per dwelling unit)"){
 			// remove any fees from previous classification
