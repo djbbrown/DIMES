33a09b5bb2e5809be9b94d216973383cf0f4a70e
Christopher <cisensano@yahoo.com>
2016-08-03 12:03:47 -0700

    added PMT_ImpactFeeSingleResDetached

--- SCRIPTS/EVENT/WTUA;Permits!Residential!NA!NA.js
+++ SCRIPTS/EVENT/WTUA;Permits!Residential!NA!NA.js
@@ -3,4 +3,5 @@
 // Script Name: WTUA;Permits!Residential!NA!NA.js
 ==================================================================*/
 include("PMT_ResubmittalFee");
+include("PMT_ImpactFeeSingleResDetached");
 include("PMT_AssessTechFee"); // must be run last
