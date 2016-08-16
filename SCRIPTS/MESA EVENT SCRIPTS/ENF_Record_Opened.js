2c4cbfd0002f801fc0e721fee8fa27f1db53c102
SteveVMesa <Steve.Veloudos@mesaaz.gov>
2016-08-16 07:25:39 -0700

    Adj

--- SCRIPTS/MESA EVENT SCRIPTS/ENF_Record_Opened.js
+++ SCRIPTS/MESA EVENT SCRIPTS/ENF_Record_Opened.js
@@ -14,15 +14,36 @@ try {
       var somevalue;
       var FromEmail = "noreply@mesaaz.gov";
       var vEParams = aa.util.newHashtable();
-      var theAddress = aa.address.getAddressByCapId(capId).getOutput();
-      var ComDesc = AInfo["Complaint Description"];
+      var ComDesc = workDescGet(capId);
       var Url = lookup("Agency_URL","ACA"); 
+
+      //Get the address
+      var capAddResult = aa.address.getAddressByCapId(capId);
+      if (capAddResult.getSuccess())
+      {
+      var addrArray = new Array();
+      var addrArray = capAddResult.getOutput();
+          if (addrArray.length==0 || addrArray==undefined)
+          {
+          logDebug("The current CAP has no address.")
+          }
+      
+      //Break Out each element of the address
+      var hseNum = addrArray[0].getHouseNumberStart();
+      var streetDir = addrArray[0].getStreetDirection();
+      var streetName = addrArray[0].getStreetName();
+      var streetSuffix = addrArray[0].getStreetSuffix();
+      var zip = addrArray[0].getZip();
+ 
+      var theAddress = hseNum + " " + streetDir + " " + streetName + " " + streetSuffix;
+      }
 
       addParameter(vEParams,"$$RECORDID$$",capIDString);
       addParameter(vEParams,"$$Address$$",theAddress);
       addParameter(vEParams,"$$ComplaintDescription$$",ComDesc);
       addParameter(vEParams,"$$URL$$",Url);
       
+      //Get the contact info
       var tInfo = getContactArray();
       var rowCount = tInfo.length;
       var x = 0;
@@ -38,13 +59,13 @@ try {
              }
           }
       }
-      //If email has a value
+      //If email has a value send email
       if (ToEmail != "")
       {
       sendNotification(FromEmail, ToEmail, "", "ENF_RECORD_OPENED", vEParams, null, capId);
       }
     }
 catch (err)
-      {
-	logDebug("A JavaScript Error occured: " + err.message);
-      }
+    {
+      logDebug("A JavaScript Error occured: " + err.message);
+    }
