c5801d87d0de54d68a4d2e919a7989fec16b9c3c
dmhoops <dhoops@accela.com>
2016-08-16 16:56:07 -0400

    Create getPartialCapID.js

---
+++ INCLUDES_CUSTOM/getPartialCapID.js
@@ -0,0 +1,18 @@
+function getPartialCapID(capid) {
+	if (capid == null || aa.util.instanceOfString(capid)) {
+		return null;
+	}
+	//1. Get original partial CAPID  from related CAP table.
+	var result = aa.cap.getProjectByChildCapID(capid, "EST", null);
+	if(result.getSuccess()) {
+		projectScriptModels = result.getOutput();
+		if (projectScriptModels == null || projectScriptModels.length == 0) {
+			logDebug("ERROR: Failed to get partial CAP with CAPID(" + capid + ")");
+			return null;
+		}
+		//2. Get original partial CAP ID from project Model
+		projectScriptModel = projectScriptModels[0];
+		return projectScriptModel.getProjectID();
+	}  
+	else { logDebug("ERROR: Failed to get partial CAP by child CAP(" + capid + "): " + result.getErrorMessage()); return null; }
+}
