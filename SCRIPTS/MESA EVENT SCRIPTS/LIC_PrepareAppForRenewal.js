522497af9c4341c7cb374d54b41135b56186c778
dmhoops <dhoops@accela.com>
2016-08-17 08:51:48 -0400

    Update LIC_PrepareAppForRenewal.js

--- SCRIPTS/MESA EVENT SCRIPTS/LIC_PrepareAppForRenewal.js
+++ SCRIPTS/MESA EVENT SCRIPTS/LIC_PrepareAppForRenewal.js
@@ -1,7 +1,7 @@
 /*===================================================================
 // Script Number: 29
 // Script Name: Copy info to renewal record
-// Script Developer: Kevin Ford
+// Script Developer: Deanna Hoops
 // Script Agency: Accela
 // Script Description: When the renewal record is created, copy info
 //    from the license record to the renewal record.
@@ -9,24 +9,24 @@
 // Script Parents:
 //            ASA;Licenses!~!~!Renewal.js
 /*==================================================================*/
-function prepareAppForRenewal() {
-	var partialCapId = getIncompleteCapId();
-	var parentCapId = aa.env.getValue("ParentCapID");
 
-	logDebug("Parent Cap id from environment = " + parentCapId);
-	//1. Check to see if license is ready for renew
-	if (isRenewProcess(parentCapId, partialCapId)) {
-		logDebug("CAPID(" + parentCapId + ") is ready for renew. PartialCap (" + partialCapId + ")");
-		//2. Associate partial cap with parent CAP.
-		var result = aa.cap.createRenewalCap(parentCapId, partialCapId, true);
-		if (result.getSuccess()) {
-			//3. Copy key information from parent license to partial cap
-			copyKeyInfo(parentCapId, partialCapId);
+var partialCapId = getIncompleteCapId();
+var parentCapId = aa.env.getValue("ParentCapID");
+
+logDebug("Parent Cap id from environment = " + parentCapId);
+//1. Check to see if license is ready for renew
+if (isRenewProcess(parentCapId, partialCapId)) {
+	logDebug("CAPID(" + parentCapId + ") is ready for renew. PartialCap (" + partialCapId + ")");
+	//2. Associate partial cap with parent CAP.
+	var result = aa.cap.createRenewalCap(parentCapId, partialCapId, true);
+	if (result.getSuccess()) {
+		//3. Copy key information from parent license to partial cap
+		copyKeyInfo(parentCapId, partialCapId);
 					
-			//4. Set B1PERMIT.B1_ACCESS_BY_ACA to "N" for partial CAP to not allow that it is searched by ACA user.
-			aa.cap.updateAccessByACA(partialCapId, "N");
-		}
-		else { logDebug("ERROR: Associate partial cap with parent CAP. " + result.getErrorMessage()); }
+		//4. Set B1PERMIT.B1_ACCESS_BY_ACA to "N" for partial CAP to not allow that it is searched by ACA user.
+		aa.cap.updateAccessByACA(partialCapId, "N");
 	}
-	else { logDebug("This is not renewal process. PartialCapId = " + partialCapId + " ParentCapId = " + parentCapId); }
+	else { logDebug("ERROR: Associate partial cap with parent CAP. " + result.getErrorMessage()); }
 }
+else { logDebug("This is not renewal process. PartialCapId = " + partialCapId + " ParentCapId = " + parentCapId); }
+
