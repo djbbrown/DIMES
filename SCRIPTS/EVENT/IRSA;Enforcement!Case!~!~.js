22255949ab8583d77676fbfae445e6c5432037cc
SteveVMesa <Steve.Veloudos@mesaaz.gov>
2016-08-17 12:31:03 -0700

    Adj

--- SCRIPTS/EVENT/IRSA;Enforcement!Case!~!~.js
+++ SCRIPTS/EVENT/IRSA;Enforcement!Case!~!~.js
@@ -3,3 +3,4 @@ include("ENF_ScheduleFollowUpInspection");
 include("ENF_InViolationInspectionUpdateWFStatus");
 // added by Brian O'Dell (Mesa)
 include("ENF_CountyIslandEmail");
+include("ENF_Non_Violation");
