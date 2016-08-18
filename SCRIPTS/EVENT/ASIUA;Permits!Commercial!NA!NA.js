9c39864f888567a7f2137c742e005e47a8f866bc
dhoops <dhoops@accela.com>
2016-08-18 15:54:16 -0400

    debug

--- SCRIPTS/EVENT/ASIUA;Permits!Commercial!NA!NA.js
+++ SCRIPTS/EVENT/ASIUA;Permits!Commercial!NA!NA.js
@@ -1,6 +1,7 @@
 /*===================================================================
 // Script Number: 001
 // Script Name: ASIUA;Permits!Commercial!NA!NA.js
+//
 ==================================================================*/
 include("PMT_TotalSqFt");
 include("PMT_PlanningNumbValidate");
