b24a5ed267b0de02a28ec4d2aebc462442d4ddef
Jody Bearden <jody.bearden@mesaaz.gov>
2016-08-24 14:23:01 -0700

    Add ENF_AutopopulateZoningDistrict include.

--- SCRIPTS/EVENT/ASA;Enforcement!Case!~!~.js
+++ SCRIPTS/EVENT/ASA;Enforcement!Case!~!~.js
@@ -2,3 +2,4 @@ include("ENF_EnforcementCaseDuplicate");
 // add by Brian O'Dell (Mesa)
 include("ENF_EnforcementNotificationEmail");
 include("ENF_Record_Opened");
+include("ENF_AutopopulateZoningDistrict");
