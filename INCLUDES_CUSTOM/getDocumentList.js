f79f5dff5eba8e3810cb49d330c46230538e246b
dhoops <dhoops@accela.com>
2016-08-24 16:59:13 -0400

    add

---
+++ INCLUDES_CUSTOM/getDocumentList.js
@@ -0,0 +1,10 @@
+function getDocumentList()  {
+	// Returns an array of documentmodels if any
+	// returns an empty array if no documents
+	var docListArray = new Array();
+	docListResult = aa.document.getCapDocumentList(capId,currentUserID);
+	if (docListResult != null && docListResult.getSuccess()) {		
+		docListArray = docListResult.getOutput();
+	}
+	return docListArray;
+}
