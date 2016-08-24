b741cb07e682023ad7da806ced47bfddba1f9dbb
SteveVMesa <Steve.Veloudos@mesaaz.gov>
2016-08-24 13:16:55 -0700

    new

---
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_Communications_Towers.js
@@ -0,0 +1,50 @@
+/*===================================================================*/
+// Script Number: 226
+// Script Name: PMT_Communications_Towers.js
+// Script Description: Send an email if  parcel is in Communications Towers GIS layer.   
+// Script Run Event: ASA
+// Script Parents: ASA;Permits/Commercial/NA/NA
+//
+// Version   |Date      |Engineer         |Details
+//  1.0      |08/24/16  |Steve Veloudos   |Initial Release
+/*==================================================================*/
+
+try {
+    var FromEmail = "noreply@mesaaz.gov";
+    var vEParams = aa.util.newHashtable();
+    var ComTowersFlag = 0;
+    var ToEmail;
+    
+    //Get GIS Data
+    var tInfo = getGISInfoArray("Accela/AccelaTAGS", "Accela_TAGS", "Accela_TAGS.TAG");
+    var rowCount = tInfo.length;
+    var x = 0;
+    var TagValue;
+
+    //Iternate for GIS Tags
+    for (x=0;x<=(rowCount-1);x++)
+        {
+            TagValue = tInfo[x];
+            
+            //Check Communications Towers
+            if(TagValue == "TWRS")
+            {
+                ComTowersFlag = 1;
+            }
+            
+        }
+
+      addParameter(vEParams,"$$RECORDID$$",capIDString);
+      
+      //Send Email For Communications Towers
+      if(ComTowersFlag  == 1)
+      {
+      ToEmail = "COM-CommunicationsTower@mesaaz.gov";
+      sendNotification(FromEmail, ToEmail, "", "PMT_COMMUNICATIONS_TOWERS", vEParams, null, capId);
+      }
+
+    }
+    catch (err)
+    {
+      logDebug("A JavaScript Error occured: " + err.message);
+    }
