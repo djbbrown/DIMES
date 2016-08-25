c6ec127946d237a6f6aeaf4478a470af5945735d
dhoops <dhoops@accela.com>
2016-08-25 13:23:17 -0400

    fix

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_PlanReviewHoursFee.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_PlanReviewHoursFee.js
@@ -11,7 +11,7 @@
 
 
 if (matches(wfTask, "Planning Review","Building Review","Fire Review","Civil Engineering Review")) {
-	if (matxhes(wfStatus, "Approved","Approved w/Comments","Revisions Required")) {
+	if (matches(wfStatus, "Approved","Approved w/Comments","Revisions Required")) {
 			// sum hours across tasks
 		totHours  = getHoursSpent("Planning Review") + getHoursSpent("Building Review") + getHoursSpent("Fire Review") + getHoursSpent("Civil Engineering Review");
 		if (totHours > 0) {
