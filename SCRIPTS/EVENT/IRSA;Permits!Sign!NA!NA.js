6945a72fe0afd1396e1d505c1d7757a5f396c625
SteveVMesa <Steve.Veloudos@mesaaz.gov>
2016-08-27 16:01:00 -0700

    Updates

--- SCRIPTS/EVENT/IRSA;Permits!Sign!NA!NA.js
+++ SCRIPTS/EVENT/IRSA;Permits!Sign!NA!NA.js
@@ -1,2 +1,3 @@
 //IRSA
 include("PMT_RolloverInspection");
+include("PMT_Electric_Clearance");
--- SCRIPTS/MESA EVENT SCRIPTS/PMT_Electric_Clearance.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_Electric_Clearance.js
@@ -1,20 +1,23 @@
 /*===================================================================
 // Script Number: 103
 // Script Name: PMT_Electric_Clearance.js
-// Script Description: Send an email when Electrical Final inspection is resulted with “Approved 
-// - Utility Clearance Required” AND the Clearance Date in the ASIT is the current date.
+// Script Description: Send an email when Electrical Final inspection is resulted with Approved 
+// - Utility Clearance Required AND the Clearance Date in the ASIT is the current date.
 // Script Run Event: ISRA
-// Script Parents:ISRA;Pemtits!Online!~!~
+// Script Parents:ISRA;Permits!Online!~!~
+// Permits/Sign/NA/NA
+// Permits/Residential/NA/NA
+// Permits/Commercial/NA/NA
 // PMT16-00392 For testing
 // Version   |Date      |Engineer         |Details
-//  1.0      |08/18/16  |Steve Veloudos   |Initial Release
+//  1.0      |08/26/16  |Steve Veloudos   |Initial Release
 /*==================================================================*/
 
 try {
       var FromEmail = "noreply@mesaaz.gov";
+      var ToEmail = "noreply@mesaaz.gov";
       var vEParams = aa.util.newHashtable();
       var ComDesc = workDescGet(capId);
-      var Url = lookup("Agency_URL","ACA"); 
       var DataTable;
 
       //Get the Inspection results
@@ -27,39 +30,89 @@ try {
 	    var inspectionScriptModel = null;
 
         //Check if Inspection is Electrical Final 
+                for (inspectionScriptModelIndex in inspectionScriptModels)
+                {
+                    inspectionScriptModel = inspectionScriptModels[inspectionScriptModelIndex];
+                    if (inspectionScriptModel.getInspectionType().toUpperCase() == "ELECTRIC FINAL")
+                    {
+                       //Check for APPROVED - UTL CLEARANCE REQ status
+                       if (inspectionScriptModel.getInspectionStatus().toUpperCase() == "APPROVED - UTL CLEARANCE REQ")
+                       {
+                        //Check if Approved and ASIT Clearance Date is the same current date --Verified working need remaining fields & logic --
+                        loadASITables();
+                        var tInfo = UTILITYSERVICEINFO;
+                           
+                        if (UTILITYSERVICEINFO.length > 0) 
+                            {
+                            for (x in tInfo) 
+                                {
+                                var Cdate  = (tInfo[x]["Clearance Date"]);
+                                var ClearanceDate = String(Cdate);
+                                var d = new Date();
+                                var d2 = jsDateToASIDate(d);
+                                var currentDate = String(d2);
+                                DataTable = "Service Type: " + (tInfo[x]["Service Type"]) + " " ;
+                                DataTable = DataTable + "Service Size: "  + (tInfo[x]["Service Size"]) + " " ; 
+                                DataTable = DataTable  + "Meter Size: "  + (tInfo[x]["Meter Size"]) + " ";
+                                DataTable = DataTable  + "BTU Load Number: "  + (tInfo[x]["BTU Load(Number)"]) + " " ; 
+                                DataTable = DataTable  + "Clearance To: "  + (tInfo[x]["Clearance To"]) + " " ;
+                                DataTable = DataTable  + "Clearance Date: " + ClearanceDate + " " ;
+                                DataTable = DataTable  + "Qty of Meters: "  + (tInfo[x]["Qty of Meters(Number)"]) + " " ;
+                                DataTable = DataTable  + "Warranty Status: "  + (tInfo[x]["Warranty Status"]) + " " ;
+                                DataTable = DataTable  + "Comments: "  + (tInfo[x]["Comments(Text Area)"]) ;
+                                var Clearance = (tInfo[x]["Clearance To"]);
+                                    //Set to Email
+                                    if (Clearance == "City of Mesa")
+                                    {
+                                        ToEmail ="customerinfobillingops@mesaaz.gov";
+                                    }
+                                    if (Clearance == "Salt River Project")
+                                    {
+                                        ToEmail ="ccdesk@srpnet.com";
+                                    }
 
-        //Check if Approved and ASIT Clearance Date is the current date
+                                }                     
+                            }
+                                //Compare dates 
+                                if( currentDate == ClearanceDate )
+                                {
+                                
+                                //Get the address
+                                var capAddResult = aa.address.getAddressByCapId(capId);
+                                if (capAddResult.getSuccess())
+                                    {
+                                    var addrArray = new Array();
+                                    var addrArray = capAddResult.getOutput();
+                                        if (addrArray.length==0 || addrArray==undefined)
+                                        {
+                                        logDebug("The current CAP has no address.")
+                                        }
+                                    else
+                                        {
+                                        //Break Out each element of the address
+                                        var hseNum = addrArray[0].getHouseNumberStart();
+                                        var streetDir = addrArray[0].getStreetDirection();
+                                        var streetName = addrArray[0].getStreetName();
+                                        var streetSuffix = addrArray[0].getStreetSuffix();
+                                        var city = addrArray[0].getCity();
+                                        var state = addrArray[0].getState();
+                                        var zip = addrArray[0].getZip();
+                                    
+                                        var theAddress = hseNum + " " + streetDir + " " + streetName + " " + streetSuffix + " " + city + ", " + state + " " + zip;
+                                        }
+                                    }
 
-
-      }
-      //Get the address
-      var capAddResult = aa.address.getAddressByCapId(capId);
-      if (capAddResult.getSuccess())
-      {
-      var addrArray = new Array();
-      var addrArray = capAddResult.getOutput();
-          if (addrArray.length==0 || addrArray==undefined)
-          {
-          logDebug("The current CAP has no address.")
-          }
-      
-      //Break Out each element of the address
-      var hseNum = addrArray[0].getHouseNumberStart();
-      var streetDir = addrArray[0].getStreetDirection();
-      var streetName = addrArray[0].getStreetName();
-      var streetSuffix = addrArray[0].getStreetSuffix();
-      var zip = addrArray[0].getZip();
- 
-      var theAddress = hseNum + " " + streetDir + " " + streetName + " " + streetSuffix;
-      }
-
-      addParameter(vEParams,"$$RECORDID$$",capIDString);
-      addParameter(vEParams,"$$Address$$",theAddress);
-      addParameter(vEParams,"$$URL$$",Url);
-      addParameter(vEParams,"$$DATATABLE$$",DataTable);
-
-      sendNotification(FromEmail, ToEmail, "", "ENF_RECORD_OPENED", vEParams, null, capId);
- 
+                                addParameter(vEParams,"$$RECORDID$$",capIDString);
+                                addParameter(vEParams,"$$Address$$",theAddress);
+                                addParameter(vEParams,"$$DATATABLE$$",DataTable);
+                                
+                                //Send the email
+                                sendNotification(FromEmail, ToEmail, "", "PMT_ELECTRIC_CLEARANCE", vEParams, null, capId);    
+                                }
+                        }
+                       } 
+                    }                 
+                }
     }
 catch (err)
     {
