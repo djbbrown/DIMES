540fd8d95ef6a1512484785d74b8468a61497fda
SteveVMesa <Steve.Veloudos@mesaaz.gov>
2016-08-29 11:01:42 -0700

    New

---
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_Condition_Email.js
@@ -0,0 +1,36 @@
+/*===================================================================*/
+// Script Number: 208
+// Script Name: PMT_Condition_Email.js
+// Script Description: Send an email ASI field Do you need a Grease Trap or Sane/Oil Interceptor is set to a value of Yes 
+// Script Run Event: ASA
+// Script Parents: ASA;Permits/Commercial/NA/NA
+// Testing record:  PMT16-00427
+// Version   |Date      |Engineer         |Details
+//  1.0      |08/29/16  |Steve Veloudos   |Initial Release
+/*==================================================================*/
+
+try {
+    var FromEmail = "noreply@mesaaz.gov";
+    var vEParams = aa.util.newHashtable();
+    var ToEmail = lookup("EMAIL_RECIPIENTS","Industrial_Pretreatment_Supervisor");
+    var GreaseTrapFlag = 0;
+    
+    //Get ASIT value
+    var GTrapSoil = AInfo["Is your project an Industrial, Commercial, Manufacturing, Automotive or Restaurant?"];
+    if(GTrapSoil == "Yes")
+        {
+        GreaseTrapFlag = 1;
+        }
+    //Add parameters
+    addParameter(vEParams,"$$RECORDID$$",capIDString);
+      
+    //Check and send email
+    if(GreaseTrapFlag == 1)
+        {
+        sendNotification(FromEmail, ToEmail, "", "PMT_GREASE_TRAP_CONDITION", vEParams, null, capId); 
+        }   
+    }
+    catch (err)
+    {
+      logDebug("A JavaScript Error occured: " + err.message);
+    }
