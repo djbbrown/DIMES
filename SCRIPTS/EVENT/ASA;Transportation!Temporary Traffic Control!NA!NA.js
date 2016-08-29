9ba7650d3016511c8b469a8dc439ad0dd9d55e1a
MesaDimes <brian.odell@mesaaz.gov>
2016-08-29 14:17:21 -0700

    v1

---
+++ SCRIPTS/EVENT/ASA;Transportation!Temporary Traffic Control!NA!NA.js
@@ -0,0 +1,5 @@
+/*===================================================================
+// Script Number: 283
+// Script Name: ASA;Transportation!Temporary Traffic Control!NA!NA.js
+==================================================================*/
+include("TRA_RelateToSpecialEventRecord");
