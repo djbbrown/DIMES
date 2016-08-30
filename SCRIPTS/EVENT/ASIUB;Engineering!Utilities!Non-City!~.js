c86c7c0b1100e4bf3f0c1a23774e920282ea44ab
MesaDimes <brian.odell@mesaaz.gov>
2016-08-30 11:54:04 -0700

    v3

--- SCRIPTS/EVENT/ASIUB;Engineering!Utilities!Non-City!~.js
+++ SCRIPTS/EVENT/ASIUB;Engineering!Utilities!Non-City!~.js
@@ -1,5 +1,6 @@
 /*===================================================================
-// Script Number: 51
+// Script Number: 51, 53
 // Script Name: ASIUB;Engineering!Utilities!Non City!~.js
 ==================================================================*/
 include("ENG_RowPermitNumberValidation");
+include("ENG_TTCPermitNumberValidation");
