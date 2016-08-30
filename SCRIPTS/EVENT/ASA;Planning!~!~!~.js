b0eea5293447cde460c984c701fc9eb27293579f
MesaDimes <brian.odell@mesaaz.gov>
2016-08-30 14:03:30 -0700

    v1

--- SCRIPTS/EVENT/ASA;Planning!~!~!~.js
+++ SCRIPTS/EVENT/ASA;Planning!~!~!~.js
@@ -1,5 +1,6 @@
 /*===================================================================
-// Script Number: 242, 073
+// Script Number: 242, 073, 70
 ==================================================================*/
 include("PLN_ParcelNoAddress");
 include("PLN_AutopopulateZoningClassification")
+include("PLN_NetSiteSize")
