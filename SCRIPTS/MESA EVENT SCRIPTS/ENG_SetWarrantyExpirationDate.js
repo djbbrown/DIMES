0cbf97645778b6a86155abb30881d9d74f3efa8a
Jody Bearden <jody.bearden@mesaaz.gov>
2016-08-30 16:19:41 -0700

    Update header.

---
+++ SCRIPTS/MESA EVENT SCRIPTS/ENG_SetWarrantyExpirationDate.js
@@ -0,0 +1,13 @@
+/*===================================================================
+// Script Number: 060
+// Script Name: ENG_SetWarrantyExpirationDate.js
+// Script Developer: Jody Bearden
+// Script Agency: Mesa
+// Script Description: When the workflow task "LOA" is set to a status
+//                     of "Letter Sent" set the ASI field "Warranty
+//                     Expiration Date" to 100 days from the status date.
+// Script Run Event: WTUA
+// Script Parents:
+//		WTUA;Engineering!Right of Way!~!~
+//             
+/*==================================================================*/
