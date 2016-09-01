c1e1d361e12caa0ccae783aa25c03d1f595133fe
MesaDimes <brian.odell@mesaaz.gov>
2016-09-01 07:27:00 -0700

    v1

---
+++ SCRIPTS/EVENT/WTUB;License!General!SpecialEvent!Application.js
@@ -0,0 +1,5 @@
+/*===================================================================
+// Script Number: 182
+// Script Name: WTUB;License!General!SpecialEvent!Application.js
+==================================================================*/
+include("LIC_RelateSupPlanningCase");
