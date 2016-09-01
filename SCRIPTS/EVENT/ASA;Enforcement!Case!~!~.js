ebfd5cf30bf17a7916a165ef23844618f56426a9
Vance Smith <VSmith@mesaaz.gov>
2016-08-31 17:19:59 -0700

    had #42 executing on wrong parent scripts

--- SCRIPTS/EVENT/ASA;Enforcement!Case!~!~.js
+++ SCRIPTS/EVENT/ASA;Enforcement!Case!~!~.js
@@ -4,7 +4,3 @@ include("ENF_EnforcementNotificationEmail");
 include("ENF_Record_Opened");
 include("ENF_AutopopulateZoningDistrict");
 include("ENF_CountyIslandEmail");
-
-// added by Vance Smith (Mesa)
-include("ENF_NewRecordPriorityNormal.js");
-include("ENF_NewRecordPriorityImminentHazard.js");
