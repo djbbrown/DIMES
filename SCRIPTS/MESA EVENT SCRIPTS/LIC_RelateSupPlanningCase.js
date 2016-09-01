f92043154bdd0eb96b107521495ef0124c1a766b
MesaDimes <brian.odell@mesaaz.gov>
2016-09-01 07:54:53 -0700

    criteria 2 v1

--- SCRIPTS/MESA EVENT SCRIPTS/LIC_RelateSupPlanningCase.js
+++ SCRIPTS/MESA EVENT SCRIPTS/LIC_RelateSupPlanningCase.js
@@ -5,34 +5,63 @@
 // Script Developer: Brian O'Dell
 // Script Agency: City of Mesa
 // Script Description: 
-// 		On ASA and ASIUA if a value is entered into the ASI field
-//		"SUP Case Number". Then relate the planning record as parent 
-//		to the current record as a child
+//		(updated criteria)
+//		If "Number of Event Days" (ASI) is more than 4, then require 
+//		a valid "SUP Case No" (ASI) before License Application (Workflow Task) 
+//		can be gien a status of Submitted. If valid, relate the
+//		SUP Case No as a parent
 //
-// Script Run Event: ASA / ASIUA
+// Script Run Event: WTUB
 // Script Parents:
-//             ASA:Licenses/General/Special Event/Application
-//             ASIUA:Licenses/General/Special Event/Application
+//             WTUB;License!General!SpecialEvent!Application.js
 // 
 //==================================================================*/
 
 try
 {
-  // check if parent exists
-  var supCaseNumber = AInfo["SUP Case Number"];
-  var getCapResult = aa.cap.getCapID(supCaseNumber);
   
-  // if parent exists, addParent(parent capId)
-  if (getCapResult.getSuccess())
+  var numOfDays = parseInt(AInfo["Number of Event Days"]);
+  numOfDays = 5;
+  //mkyOutput += "numOfDays: " + numOfDays + " \r";
+
+  if ((wfTask == "License Application")
+        && (wfStatus == "Submitted")
+        && (numOfDays > 4))
   {
-    addParent("" + supCaseNumber);
-    //mkyOutput += "supCaseNumber found: " + supCaseNumber + ", parentId: " + parentId + " \r";
-    //logDebug("SUP Case Number found and added as parent");
-  }
-  else
-  { 
-    //mkyOutput += "ERROR: could not find parent supCaseNumber: " + supCaseNumber + " \r";
-    //logDebug("**ERROR: could not find SUP Case Number (" + supCaseNumber + "): " + getCapResult.getErrorMessage());
+
+    var supCaseNumber = AInfo["SUP Case Number"];
+
+    if (supCaseNumber != "")
+    {
+
+      var getCapResult = aa.cap.getCapID(supCaseNumber);
+  
+      // if parent exists, addParent(parent capId)
+      if (getCapResult.getSuccess())
+      {
+        addParent("" + supCaseNumber);
+        //mkyOutput += "supCaseNumber found: " + supCaseNumber + ", parentId: " + parentId + " \r";
+        logDebug("SUP Case Number found and added as parent");
+      }
+      else
+      { 
+        //mkyOutput += "The SUP Case No " + supCaseNumber + " is not a valid Case No \r";
+        showMessage = true;
+        comment("The SUP Case No is required for Special Events over 4 days");
+        cancel = true;
+        //logDebug("The SUP Case No " + supCaseNumber + " is not a valid Case No");
+      }
+
+    } 
+    else 
+    {
+      //mkyOutput += "The SUP Case No is required for Special Events over 4 days";
+      showMessage = true;
+      comment("The SUP Case No is required for Special Events over 4 days");
+      cancel = true;
+      //logDebug("The SUP Case No is required for Special Events over 4 days");  
+    }
+
   }
 
 }
