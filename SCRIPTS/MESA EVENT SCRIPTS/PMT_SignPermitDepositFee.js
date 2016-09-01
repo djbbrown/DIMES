6731f08a1841d7a6735345c427754fd0aa8b29ae
gurneykevin <kgurney@accela.com>
2016-09-01 13:37:24 -0700

    add calculation for sign permit deposit

---
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_SignPermitDepositFee.js
@@ -0,0 +1,21 @@
+/*===================================================================
+// Script Number: Magic Deposit Script
+// Script Name: PMT_SignPermitDepositFee.js
+// Script Developer: Kevin Gurney
+// Script Agency: Accela
+// Script Description: At Application Submittal calculate and assess the Sign Permit Deposit Fee
+// Script Run Event: ASA
+// Script Parents:
+//            ASA;Permits!Sign!NA!NA
+/*==================================================================*/
+
+	var totalSignValuation = AInfo["Total Sign Valuation"];
+	var totalSignSqFt = AInfo["Total Sign Square Footage"];
+	var totalFee = 0;
+	totalFee = (102.4 + (0.03 * totalSignValuation) + (0.3 * totalSignSqFt));
+	logDebug("Total Fee: " + totalFee);
+	var totalFee65pct = Math.ceil((totalFee * .65) * 100)/100;
+	logDebug("totalFee65pct = " + totalFee65pct);
+	if (totalFee65pct > 0 ) {
+		addFee("SGN010", "PMT_SIGNS", "FINAL",  totalFee65pct, "N");
+	} 
