bca404a1abaf9f5e00e7f93cc0fd25a89eebfc75
MesaDimes <brian.odell@mesaaz.gov>
2016-09-01 14:18:16 -0700

    v2

--- SCRIPTS/EVENT/ASB;Planning!Admin Review!NA!NA.js
+++ SCRIPTS/EVENT/ASB;Planning!Admin Review!NA!NA.js
@@ -2,4 +2,4 @@
 // Script #:	332
 // Script Name: ASB;Planning!Admin Review!NA!NA.js
 /*==================================================================*/
-include("PLN_NumberOfElevationsReq.js");
+include("PLN_NumberOfElevationsReq");
