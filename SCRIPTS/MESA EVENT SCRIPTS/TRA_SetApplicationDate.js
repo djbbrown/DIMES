6fdc5ab2551e4af70878a755224cbae606079999
MesaDimes <brian.odell@mesaaz.gov>
2016-09-06 13:15:04 -0700

    b1

--- SCRIPTS/MESA EVENT SCRIPTS/TRA_SetApplicationDate.js
+++ SCRIPTS/MESA EVENT SCRIPTS/TRA_SetApplicationDate.js
@@ -27,7 +27,7 @@ try
   }
 
   editAppSpecific("Application Date",jsDateToASIDate(today),capId)
-  logDebug("Application Date set to: " + jsDateToASIDate(today));
+  //logDebug("Application Date set to: " + jsDateToASIDate(today));
 }
 catch (err)
 {
