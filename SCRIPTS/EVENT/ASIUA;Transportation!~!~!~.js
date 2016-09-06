31a608949d8a6116c1c8634008e187375bfc01ea
MesaDimes <brian.odell@mesaaz.gov>
2016-09-06 10:40:35 -0700

    f2

--- SCRIPTS/EVENT/ASIUA;Transportation!~!~!~.js
+++ SCRIPTS/EVENT/ASIUA;Transportation!~!~!~.js
@@ -2,6 +2,5 @@
 // Script Number: 
 // Script Name: ASIUA;Transportation!~!~!~.js
 ==================================================================*/
-include("TRA_SetApplicationDate");
 include("TRA_SetPermitExpirationDate");
 include("TRA_SetPlanReviewExpirationDate");
