27b394f9e7b0ba74e6ea8731ba43eafc389f993c
SteveVMesa <Steve.Veloudos@mesaaz.gov>
2016-09-07 15:01:48 -0700

    Need

--- SCRIPTS/EVENT/ASA;Transportation!~!~!~.js
+++
@@ -1,8 +0,0 @@
-/*===================================================================
-// Script Number: 
-// Script Name: ASA;Transportation!~!~!~.js
-==================================================================*/
-include("TRA_SetApplicationDate");
-include("TRA_SetPermitExpirationDate");
-include("TRA_SetPlanReviewExpirationDate");
-include("TRA_TTC_After_Hours");
