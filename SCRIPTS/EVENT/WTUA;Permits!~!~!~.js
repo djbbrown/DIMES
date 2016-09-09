43d6ccf4c06fde4b759ba41725694813b69d579c
MesaDimes <brian.odell@mesaaz.gov>
2016-09-09 09:32:22 -0700

    refactor1

--- SCRIPTS/EVENT/WTUA;Permits!~!~!~.js
+++ SCRIPTS/EVENT/WTUA;Permits!~!~!~.js
@@ -1,7 +1,8 @@
 // WTUA;Permits!~!~!~.js
-//include("PMT_PenaltyDate");
+include("PMT_PenaltyDate");
 //include("PMT_ResetPlanReviewPenaltyDate");
 
 // added by Vance Smith (Mesa)
 include("PMT_Issued_FloodCondition");
+include("ENF_NoViolationInspectionUpdateWFStatus");
 include("PMT_PermitExpirationDate");
