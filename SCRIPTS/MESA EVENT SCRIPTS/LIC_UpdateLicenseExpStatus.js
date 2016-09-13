cb494a170eb88e38d85e23742aa714539878d537
MesaDimes <brian.odell@mesaaz.gov>
2016-09-13 15:42:06 -0700

    v1

---
+++ SCRIPTS/MESA EVENT SCRIPTS/LIC_UpdateLicenseExpStatus.js
@@ -0,0 +1,47 @@
+//*===================================================================
+//
+// Script Number: 49
+// Script Name: LIC_UpdateLicenseExpStatus
+// Script Developer: Brian O'Dell
+// Script Agency: City of Mesa
+// Script Description: 
+// 		Updated Criteria/Clarification: when the License Workflow 
+//		task “License Status” is given a status of “Expired”, 
+//		“Inactive”, or “Revoked” then change the Expiration Status 
+//		to “Inactive”. Expiration Status field is on the Renewal 
+//		Info tab.
+//
+// Script Run Event: WTUA
+// Script Parents:
+//              WTUA;Licenses!~!~!License
+// 
+//==================================================================*/
+
+
+try
+{
+
+  if (
+      (wfTask == "License Status") 
+      && 
+      ((wfStatus == "Revoked") || (wfStatus == "Inactive") || (wfStatus == "Expired"))
+     )
+  {
+    licEditExpInfo("Inactive");
+    commit("License Exp Info updated to 'Inactive'");
+  }
+
+}
+catch (err)
+{
+  logDebug("A JavaScript Error occured: " + err.message);
+}
+
+
+
+
+
+
+
+
+
