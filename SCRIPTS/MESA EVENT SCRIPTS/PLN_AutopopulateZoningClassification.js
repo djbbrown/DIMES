c71a7655f8873ff138d40402e4010fd3e831e37d
Jody Bearden <jody.bearden@mesaaz.gov>
2016-09-12 17:02:48 -0700

    Concatenate Overlay to Zoning field.

--- SCRIPTS/MESA EVENT SCRIPTS/PLN_AutopopulateZoningClassification.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PLN_AutopopulateZoningClassification.js
@@ -21,12 +21,20 @@
 try
 {
 	var zoning = getGISInfo("Planning/Zoning", "Zoning Districts", "ZONING");  // use "ZONING" field for zoning info only (e.g. "RS-6") or "DSCR" (i.e. description) field for soning info plus a brief description (e.g. "RS-6 Single Residence 6")
-	if(zoning != null && zoning != false)
+	var overlay = getGISInfo("Planning/Zoning", "Overlay Districts", "OVERLAY"); // get overlay value (e.g. "PAD")
+	
+	if (zoning == null || zoning == false) { zoning = ""; }
+	if (overlay == null || overlay == false) { overlay = ""; }
+	
+	var zoning_overlay = zoning + " " + overlay;
+	zoning_overlay = zoning_overlay.trim();
+	
+	if(zoning_overlay != "")
 	{
-		logDebug("Existing Zoning being updated to: " + zoning);
+		logDebug("Existing Zoning being updated to: " + zoning_overlay);
 		ezb = getAppSpecific("Existing Zoning"); // existing zoning before setting
 		logDebug("Existing Zoning (before setting): " + ezb);
-		editAppSpecific("Existing Zoning", zoning);
+		editAppSpecific("Existing Zoning", zoning_overlay);
 		eza = getAppSpecific("Existing Zoning"); // existing zoning after setting
 		logDebug("Existing Zoning (after setting): " + eza);
 	} else {
