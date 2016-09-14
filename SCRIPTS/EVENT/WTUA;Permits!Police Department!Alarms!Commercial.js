1ef24afa29a215615e9bd345e49e978a92eb4c6e
Kevin Ford <kford@accela.com>
2016-09-14 14:54:40 -0700

    Update to use "include("PMT_PDAlarmSetActiveAndExpiration");"
    This sets active and expiration.

--- SCRIPTS/EVENT/WTUA;Permits!Police Department!Alarms!Commercial.js
+++ SCRIPTS/EVENT/WTUA;Permits!Police Department!Alarms!Commercial.js
@@ -1,9 +1,3 @@
-if (wfTask == "Application Submittal" && wfStatus == "Approved") {
-	//Set License to Active and set Expiration Dates
-	tmpNewDate = dateAddMonths(null, 12);
-	thisLic = new licenseObject(capIDString,capId) ; 
-	thisLic.setExpiration(tmpNewDate);
-	thisLic.setStatus("Active");	
-}
+include("PMT_PDAlarmSetActiveAndExpiration");
 include("PMT_ResetFalseAlarmCountToZero");
 include("PMT_RenewalRecordUpdateParentPermitIssuedDate");
