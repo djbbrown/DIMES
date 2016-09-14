49f4af1b42d6df7f3ee5fab128e4846a89892d32
Kevin Ford <kford@accela.com>
2016-09-14 14:48:51 -0700

    Update for script 199

--- SCRIPTS/EVENT/ASA;AnimalControl!Complaint!NA!NA.js
+++ SCRIPTS/EVENT/ASA;AnimalControl!Complaint!NA!NA.js
@@ -1,9 +1,3 @@
 // Script ASA;AnimalControl!Complaint!NA!NA.js
 include("ENF_populateCitationNumber");//This script must run before updateViolationInfoCustomList
-
-//ID-199: On ASA and ASIUA, check the rows in the VIOLATION INFORMATION ASIT and create a row in the CITATION CHECKLIST ASIT for each. The column Citation Number in the VIOLATION INFORMATION ASIT should be used to populate the same column in the CITATION CHECKLIST ASIT. This script is dependent on script ENF_populateCitationNumber
-try{
-	updateViolationInfoCustomList();
-}catch (err){
-	logDebug("A JavaScript Error occurred: ASA:Enforcement/*/*/*: ID-199: " + err.message);
-}
+include("ENF_AutopopulateCitationChecklistASIT"); // Script 199
