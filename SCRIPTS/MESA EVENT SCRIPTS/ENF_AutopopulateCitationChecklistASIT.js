9987bf133cb1cdf12e0933abac24283d552b8003
Kevin Ford <kford@accela.com>
2016-09-14 14:32:01 -0700

    Update for script 199

---
+++ SCRIPTS/MESA EVENT SCRIPTS/ENF_AutopopulateCitationChecklistASIT.js
@@ -0,0 +1,8 @@
+//ID-199: On ASA and ASIUA, check the rows in the VIOLATION INFORMATION ASIT and create a row in the CITATION CHECKLIST ASIT for each. The column Citation Number in the VIOLATION INFORMATION ASIT should be used to populate the same column in the CITATION CHECKLIST ASIT.
+try{
+	if(matches(appTypeArray[1],"Case", "Environmental")){
+		updateViolationInfoCustomList();
+	}
+}catch (err){
+	logDebug("A JavaScript Error occurred: ASA:Enforcement/*/*/*: ID-199: " + err.message);
+}
