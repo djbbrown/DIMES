ba194429e1de1517bee32186a4d153d3f7265481
Kevin Ford <kford@accela.com>
2016-09-14 14:11:41 -0700

    Update for script 320

--- SCRIPTS/EVENT/WTUA;Permits!Police Department!Alarms!Commercial.js
+++ SCRIPTS/EVENT/WTUA;Permits!Police Department!Alarms!Commercial.js
@@ -5,18 +5,7 @@ if (wfTask == "Application Submittal" && wfStatus == "Approved") {
 	thisLic.setExpiration(tmpNewDate);
 	thisLic.setStatus("Active");	
 }
-/*  ID-320 lwacht: When the active Alarm Permit record moves to an application status of "Issued"
-	the script will populate the current date into the Custom Field named "Date of Issuance". 
-*/
-try{
-	if(capStatus=="Issued") {
-		logDebug("Executing ID-320");
-		editAppSpecific("Date of Issuance",sysDateMMDDYYYY);
-	}
-}catch (err) {
-    logDebug("A JavaScript Error occurred: WTUA:Permits/Police Department/Alarms/Commercial: #320: " + err.message);
-	logDebug(err.stack);
-}
+include("PMT_ResetFalseAlarmCountToZero");
 /* script #325
 	1.	Alarm PD Permit Renewal (Permits/Police Department/Alarms/Commercial Renewal) when the workflow task Closed is set to Issued
 	1.	Update the Alarm PD Permit Record expiration date to one year in the future.
