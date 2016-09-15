559c2f0f15352adcb34a7bae4345b14599a45040
Jody Bearden <jody.bearden@mesaaz.gov>
2016-09-15 14:57:01 -0700

    add logDebug statement to see if this is being called.

--- SCRIPTS/EVENT/ISA;Permits!~!~!~.js
+++ SCRIPTS/EVENT/ISA;Permits!~!~!~.js
@@ -1,3 +1,4 @@
 //ISA: Permits!~!~!~
 //include("PMT_Inspection_Added_Manually");
+logDebug("........ISA;Permits!~!~!~ called.................");
 include("PMT_SetInspectionScheduledDate"); // Script 343
