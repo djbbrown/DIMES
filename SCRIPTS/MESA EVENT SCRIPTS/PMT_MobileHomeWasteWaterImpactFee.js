c92b6c5a122a569f1d017dcc64c195114f93aa3d
John Cheney <JCheney@mesaaz.gov>
2016-09-15 15:34:59 -0700

    PMT_MobileHomeWaterImpactFee and PMT_MobileHomeWasteWaterImpactFee

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_MobileHomeWasteWaterImpactFee.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_MobileHomeWasteWaterImpactFee.js
@@ -1,34 +1,35 @@
 /*===================================================================
  Versions:
  9/15/2016-A	John Cheney			initial
- 9/15/2016-B	John Cheney			fixed field Water/Wastewater Meter Size 1 (added space before 1)
  ---------------------------------------------------------------------
- Script Number: 335
- Script Name: PMT_MobileHomeWaterImpactFee.js
+ Script Number: 336
+ Script Name: PMT_MobileHomeWasteWaterImpactFee.js
  Script Developer: John Cheney
  Script Agency: Mesa
  Script Description:
-   
-   Water Impact Fee for Mobile Home
+    	
+    Waste Water Impact Fee for Mobile Home
 
- 	When workflow task Permit Issuance is activated AND/OR – 
-	 on ApplicationSpecificInfoUpdateAfter ONLY IF workflow task Permit Issuance is activated, 
-	 then if ASI dropdown value for field “Water/Wastewater Meter Size x” (x = 1-4) = 
-	 ‘Res Units’ and ASI Group Impact Fees, Field Name= “Classification”  = 
-	 either “Manufactured Home (on platted lot)“ or “Mfg. Home/Park Model/RV (per space or lot)” 
-	 then assess appropriate fee.  
-	 This will result in cancelled fee if all the water meter quantities add up to 0.
+    When workflow task Permit Issuance is activated AND/OR – 
+    on ApplicationSpecificInfoUpdateAfter ONLY IF workflow task Permit Issuance is activated, 
+    then if ASI dropdown value for field “Water/Wastewater Meter Size x”  (x = 1-4) = ‘Res Units’ 
+    and ASI Group Impact Fees, Field Name= “Classification”  = 
+    either “Manufactured Home (on platted lot)“ or “Mfg. Home/Park Model/RV (per space or lot)” 
+    then assess appropriate fee.
 
-Test records: PMT16-00606, PMT16-00483 
+	This will result in cancelled fee if all the waste water meter quantities add up to 0.	
 
-specs: https://portal.accelaops.com/projects/Mesa/Lists/Script%20Tracker/DispForm.aspx?ID=335&Source=https%3A%2F%2Fportal%2Eaccelaops%2Ecom%2Fprojects%2FMesa%2FLists%2FScript%2520Tracker%2Factive%2Easpx&ContentTypeId=0x010300D70AB462012E604593F2EB837FB5F3FD 
+Test records: PMT16-00606, PMT16-00483
+
+
+specs: https://portal.accelaops.com/projects/Mesa/Lists/Script%20Tracker/DispForm.aspx?ID=336&Source=https%3A%2F%2Fportal%2Eaccelaops%2Ecom%2Fprojects%2FMesa%2FLists%2FScript%2520Tracker%2Factive%2Easpx&ContentTypeId=0x010300D70AB462012E604593F2EB837FB5F3FD 
 
 Script run events: WTUA
 Script Parents:  WTUA;Permits!Residential!Mobile Home!NA
 
 /*==================================================================*/
 
-//logDebug("---------- start  PMT_MobileHomeWaterImpactFee ----------");
+//logDebug("---------- start  PMT_MobileHomeWasteWaterImpactFee ----------");
 try {
 	var isActive = isTaskActive("Permit Issuance");
 
@@ -47,36 +48,35 @@ try {
 
 				// check Meter 1
 				var meterType = AInfo["Water/Wastewater Meter Size 1"]; 
-				var meterQty = AInfo["Water Meter Qty 1"];
+				var meterQty = AInfo["Waste Water Qty 1"];
 				if (meterType && meterType == "Res Units" && meterQty && Number(meterQty) > 0){
 					meterQtyTotal = meterQtyTotal + Number(meterQty);
 				}
 
 				// check Meter 2
 				meterType = AInfo["Water/Wastewater Meter Size 2"]; 
-				meterQty = AInfo["Water Meter Qty 2"];
+				meterQty = AInfo["Waste Water Qty 2"];
 				if (meterType && meterType == "Res Units" && meterQty && Number(meterQty) > 0){
 					meterQtyTotal = meterQtyTotal + Number(meterQty);
-					logDebug("- meter2 inside.."); 
 				}
 
 				// check Meter 3
 				meterType = AInfo["Water/Wastewater Meter Size 3"]; 
-				meterQty = AInfo["Water Meter Qty 3"];
+				meterQty = AInfo["Waste Water Qty 3"];
 				if (meterType && meterType == "Res Units" && meterQty && Number(meterQty) > 0){
-					meterQtyTotal = meterQtyTotal + Number(meterQty); 
+					meterQtyTotal = meterQtyTotal + Number(meterQty);
 				}
 
 				// check Meter 4
 				meterType = AInfo["Water/Wastewater Meter Size 4"]; 
-				meterQty = AInfo["Water Meter Qty 4"];
+				meterQty = AInfo["Waste Water Qty 4"];
 				if (meterType && meterType == "Res Units" && meterQty && Number(meterQty) > 0){
 					meterQtyTotal = meterQtyTotal + Number(meterQty); 
 				}
 
 				// feeCode is based on classification
-				var feeCode = "RDIF040";
-				if (classification == "Mfg. Home/Park Model/RV (per space or lot)"){feeCode = "RDIF050";}
+				var feeCode = "RDIF090";
+				if (classification == "Mfg. Home/Park Model/RV (per space or lot)"){feeCode = "RDIF100";}
 
 				// cancel existing fee (if found)
 				if (feeExists(feeCode, "NEW", "INVOICED")) voidRemoveFee(feeCode);
@@ -86,19 +86,19 @@ try {
 					addFee(feeCode, "PMT_RDIF", "FINAL", meterQtyTotal, "N");
 				}
 
-				logDebug("PMT_MobileHomeWaterImpactFee - Classification = " + classification + ", set fee with code = " + feeCode + " for meterQtyTotal = " + meterQtyTotal);
+				logDebug("PMT_MobileHomeWasteWaterImpactFee - Classification = " + classification + ", set fee with code = " + feeCode + " for meterQtyTotal = " + meterQtyTotal);
 			} else {
-				logDebug("PMT_MobileHomeWaterImpactFee - No Action - Classification does not match criteria.");	
+				logDebug("PMT_MobileHomeWasteWaterImpactFee - No Action - Classification does not match criteria.");	
 			}
 
 		} else {
-			logDebug("PMT_MobileHomeWaterImpactFee - No Action - Classification is null.");
+			logDebug("PMT_MobileHomeWasteWaterImpactFee - No Action - Classification is null.");
 		}
 	} else {
-		logDebug("PMT_MobileHomeWaterImpactFee - No Action - Permit Issuance task is null or not active.");
+		logDebug("PMT_MobileHomeWasteWaterImpactFee - No Action - Permit Issuance task is null or not active.");
 	}
 } catch (err) {
-	logDebug("A JavaScript Error occured in PMT_MobileHomeWaterImpactFee: " + err.message);
+	logDebug("A JavaScript Error occured in PMT_MobileHomeWasteWaterImpactFee: " + err.message);
 	logDebug(err.stack);
 }
-//logDebug("---------- end  PMT_MobileHomeWaterImpactFee ----------");
+//logDebug("---------- end  PMT_MobileHomeWasteWaterImpactFee ----------");
