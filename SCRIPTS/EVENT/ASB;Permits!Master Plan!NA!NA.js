77c3a2142dba954ff6110270fb4f090d44dc73b5
MesaDimes <brian.odell@mesaaz.gov>
2016-09-15 16:29:39 -0700

    v1

---
+++ SCRIPTS/EVENT/ASB;Permits!Master Plan!NA!NA.js
@@ -0,0 +1,5 @@
+/*===================================================================
+// Script Number: 68
+// Script Name: ASB;Permits!Master Plan!NA!NA.js
+==================================================================*/
+include("PMT_MasterPlanBlockTester_ASBonly");  // for testing only
