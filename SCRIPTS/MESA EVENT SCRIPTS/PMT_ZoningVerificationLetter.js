fa7dc3d22d04223c9a0645d547e0d9363c39cd45
MesaDimes <brian.odell@mesaaz.gov>
2016-09-16 07:50:09 -0700

    friday basic ASB tester 1

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_ZoningVerificationLetter.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_ZoningVerificationLetter.js
@@ -29,49 +29,11 @@ try
   var docFound = false;
   var curReqDocType = "";
 
-  for (x=0;x<tInfoCount;x++)
-  {
-    curReqDocType = tInfo[x]["Document Type"];
   
-    if (curReqDocType == "Zoning Verification Letter - One Parcel Request")
-    {
-      docNeeded = true;
-      var docListResult = aa.document.getCapDocumentList(capId,currentUserID);
-
-      if (docListResult == null)
-      {
-        showMessage = true;
-        comment("The document List is null");
-      }
-      else
-      {
-
-        if (docListResult.getSuccess())
-        {
-          var docListArray = docListResult.getOutput()
-          var docCount = docListArray.length;
-          var docCat = "";
-
-          for(x=0;x<docCount;x++)
-          {
-            docCat = docListArray[x].getDocCategory();
-    
-            if (docCat == "Zoning Verification Request")
-            {
-              docFound = true;
-            }
-          }  
-        } 
-      }
-    }
-  }
-
-  if ((docNeeded) && (!(docFound)))
-  {
     showMessage = true;
     comment("The document Zoning Verification Letter is required. Please add this document and submit again.");
     cancel = true;
-  }
+
 
 
 }
