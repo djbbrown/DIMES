7c5b9be162864088a5b90368e6247bd723eb6f84
Jody Bearden <jody.bearden@mesaaz.gov>
2016-09-16 16:28:40 -0700

    initial commit

--- SCRIPTS/EVENT/ISA;Permits!~!~!~.js
+++ SCRIPTS/EVENT/ISA;Permits!~!~!~.js
@@ -1,4 +1,3 @@
 //ISA: Permits!~!~!~
 //include("PMT_Inspection_Added_Manually");
-logDebug("........ISA;Permits!~!~!~ called.................");
 include("PMT_SetInspectionScheduledDate"); // Script 343
