494a79a54488e4a1c6b0d0126904bb41f955eb90
Kevin Ford <kford@accela.com>
2016-09-16 07:22:18 -0700

    Minor update, removing the extra spaces

--- SCRIPTS/EVENT/ASB;Permits!Document Retrieval!NA!NA.js
+++ SCRIPTS/EVENT/ASB;Permits!Document Retrieval!NA!NA.js
@@ -2,4 +2,4 @@
 // Script Number: 68
 // Script Name: ASB;Permits!Document Retrieval!NA!NA.js
 ==================================================================*/
-include("PMT_ZoningVerificationLetter_ASBonly");
+include("PMT_ZoningVerificationLetter");
