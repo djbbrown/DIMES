b3d90ac3e4b15086d6fa076153046ea97056c03d
Jody Bearden <jody.bearden@mesaaz.gov>
2016-09-18 16:54:28 -0700

    Commit current - waiting on info to complete.

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_CommercialImpactFees.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_CommercialImpactFees.js
@@ -33,10 +33,16 @@ try {
 
 		// permit issuance task exists and is active, so ok to continue
 
-        var classification = AInfo["Classification"];
-
-        var hasWaterTag = false;
+		// get ASI vars
+        var classification = getAppSpecific("Classification");
+		var nonRes = (classification == "Non-Residential (per sq ft)");
+		var hotMot = (classification == "Hotels/Motels (per room)");
+		var fire = getAppSpecific("Fire");
+		var pubSaf = getAppSpecific("Public Safety");
+		var storm = getAppSpecific("Stormwater");
+        var hasWaterTag = false; // parcel in ASU or AZ Water area? (i.e. has an 'ASU', 'ASUE', 'AWCP' tag)
         var hasStormTag = false;
+
         var tagArray = getGISInfoArray("Accela/AccelaTAGS", "Accela_TAGS", "Accela_TAGS.TAG");
         if (tagArray && tagArray.length > 0) {
             for (tIndex in tagArray) {
@@ -47,57 +53,143 @@ try {
         }
 
         if (classification && matches(classification, "Non-Residential (per sq ft)", "Hotels/Motels (per room)") {
-
-            var units = AInfo["Number of Units"];
-
-			if (units && units > 1){
-				
-				// two more more units.. can now get to work!
-
-				// fire impact fee = (RDIF180, PMT_RDIF) - remove existing if found and add correct fee
-				if (feeExists("RDIF180", "NEW", "INVOICED")) voidRemoveFee("RDIF180");
-				addFee("RDIF180", "PMT_RDIF", "FINAL", units, "N");
-				
-				// public safety fee = (RDIF230, PMT_RDIF)  - remove existing if found and add correct fee
-				if (feeExists("RDIF230", "NEW", "INVOICED")) voidRemoveFee("RDIF230");
-				addFee("RDIF230", "PMT_RDIF", "FINAL", units, "N");
-
-				// solid waste fee = (RDIF330, PMT_RDIF)  - remove existing if found and add correct fee
-				if (feeExists("RDIF330", "NEW", "INVOICED")) voidRemoveFee("RDIF330");
-				addFee("RDIF330", "PMT_RDIF", "FINAL", units, "N");
-
-				// waste water fee = (RDIF080, PMT_RDIF) - remove existing if found and add correct fee
-				if (feeExists("RDIF080", "NEW", "INVOICED")) voidRemoveFee("RDIF080");
-				addFee("RDIF080", "PMT_RDIF", "FINAL", units, "N");
-
-				// water fee  = (RDIF030, PMT_RDIF) - remove existing if found and add correct fee if no waterTag found 
-				if (feeExists("RDIF030", "NEW", "INVOICED")) voidRemoveFee("RDIF030");
-				if(hasWaterTag == false){
-					addFee("RDIF030", "PMT_RDIF", "FINAL", units, "N");
+			
+			// my junk here
+			
+			//==========================================================
+			// Commercial Fire, Public Safety and Stormwater Impact Fees
+			//==========================================================
+			
+			//////////////////////////
+			var fcv = ?????; // Fee Calc Variable - TBD?
+			//////////////////////////
+			
+			/* Non-Residential (per sq ft) */
+			if (nonRes) {
+				if (fire && fire > 0) {
+					// assess Fire fee.
+					if (feeExists("RDIF207", "NEW", "INVOICED")) voidRemoveFee("RDIF207");
+					addFee("RDIF207", "PMT_RDIF", "FINAL", fire * fcv, "N");
 				}
-
-				// stormWater fee = (RDIF280, PMT_RDIF) - remove existing if found and add correct fee if found stormTag
-				if (feeExists("RDIF280", "NEW", "INVOICED")) voidRemoveFee("RDIF280");
-				if(hasStormTag == true){
-					addFee("RDIF280", "PMT_RDIF", "FINAL", units, "N");
+				
+				if (pubSaf && pubSaf > 0) {
+					// assess Public Safety fee.
+					if (feeExists("RDIF257", "NEW", "INVOICED")) voidRemoveFee("RDIF257");
+					addFee("RDIF257", "PMT_RDIF", "FINAL",  pubSaf * fcv, "N");
 				}
-
-				logDebug("PMT_ImpactFeesMultiResidence - Fees successfully set!");
-			} else{
-				logDebug("PMT_ImpactFeesMultiResidence - No Action - Number of Units is null or < 2.");	
+				
+				if (hasStormTag && (storm && storm > 0)) {
+					// assess Stormwater fee.
+					if (feeExists("RDIF307", "NEW", "INVOICED")) voidRemoveFee("RDIF307");
+					addFee("RDIF307", "PMT_RDIF", "FINAL",  storm * fcv, "N");
+				}
 			}
+			
+			/* Hotels/Motels (per room) */
+			if (hotMot) {
+				if (fire && fire > 0) {
+					// assess Fire fee.
+					if (feeExists("RDIF205", "NEW", "INVOICED")) voidRemoveFee("RDIF205");
+					addFee("RDIF205", "PMT_RDIF", "FINAL",  fire * fcv, "N");
+				}
+				
+				if (pubSaf && pubSaf > 0) {
+					// assess Public Safety fee.
+					if (feeExists("RDIF255", "NEW", "INVOICED")) voidRemoveFee("RDIF255");
+					addFee("RDIF255", "PMT_RDIF", "FINAL",  pubSaf * fcv, "N");
+				}
+				
+				if (hasStormTag && (storm && storm > 0)) {
+					// assess Stormwater fee.
+					if (feeExists("RDIF305", "NEW", "INVOICED")) voidRemoveFee("RDIF305");
+					addFee("RDIF305", "PMT_RDIF", "FINAL",  storm * fcv, "N");
+				}
+			}
+			
+			//==============================================
+			// Commercial Water and Waster Water Impact Fees
+			//==============================================
+			var wmq1 = getAppSpecific("Water Meter Qty 1");
+			var wmq2 = getAppSpecific("Water Meter Qty 2");
+			var wmq3 = getAppSpecific("Water Meter Qty 3");
+			var wmq4 = getAppSpecific("Water Meter Qty 4");
+			
+			var wwms1 = getAppSpecific("Water/Wastewater Meter Size 1");
+			var wwms2 = getAppSpecific("Water/Wastewater Meter Size 2");
+			var wwms3 = getAppSpecific("Water/Wastewater Meter Size 3");
+			var wwms4 = getAppSpecific("Water/Wastewater Meter Size 4");
+		
+			/* Non-Residential (per sq ft) */
+			if (nonRes && !hasWaterTag) {
+				//if (matches(
+			}
+			
+			/* Hotels/Motels (per room) */
+			if (hotMot && !hasWaterTag) {
+				if (wmq1 && wmq1 > 0) {
+					switch("" + wwms1) {
+						case "Com-3/4":
+							// assess fee: "RDIF055", "Water - Hotel/Motel", wmq1 * 2220
+							// assess fee: "RDIF105", "Waste Water - Hotel/Motel", wmq1 * 2659
+							break;
+						case "Com-1":
+							// assess fee: "RDIF044
+					}
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			//=========================
+			// John's stuff below here.
+			//=========================
+			// fire impact fee = (RDIF180, PMT_RDIF) - remove existing if found and add correct fee
+			if (feeExists("RDIF180", "NEW", "INVOICED")) voidRemoveFee("RDIF180");
+			addFee("RDIF180", "PMT_RDIF", "FINAL", units, "N");
+			
+			// public safety fee = (RDIF230, PMT_RDIF)  - remove existing if found and add correct fee
+			if (feeExists("RDIF230", "NEW", "INVOICED")) voidRemoveFee("RDIF230");
+			addFee("RDIF230", "PMT_RDIF", "FINAL", units, "N");
+
+			// solid waste fee = (RDIF330, PMT_RDIF)  - remove existing if found and add correct fee
+			if (feeExists("RDIF330", "NEW", "INVOICED")) voidRemoveFee("RDIF330");
+			addFee("RDIF330", "PMT_RDIF", "FINAL", units, "N");
+
+			// waste water fee = (RDIF080, PMT_RDIF) - remove existing if found and add correct fee
+			if (feeExists("RDIF080", "NEW", "INVOICED")) voidRemoveFee("RDIF080");
+			addFee("RDIF080", "PMT_RDIF", "FINAL", units, "N");
+
+			// water fee  = (RDIF030, PMT_RDIF) - remove existing if found and add correct fee if no waterTag found 
+			if (feeExists("RDIF030", "NEW", "INVOICED")) voidRemoveFee("RDIF030");
+			if(hasWaterTag == false){
+				addFee("RDIF030", "PMT_RDIF", "FINAL", units, "N");
+			}
+
+			// stormWater fee = (RDIF280, PMT_RDIF) - remove existing if found and add correct fee if found stormTag
+			if (feeExists("RDIF280", "NEW", "INVOICED")) voidRemoveFee("RDIF280");
+			if(hasStormTag == true){
+				addFee("RDIF280", "PMT_RDIF", "FINAL", units, "N");
+			}
+
+			logDebug("PMT_CommercialImpactFees - Fees successfully set!");
         }else {
-            logDebug("PMT_ImpactFeesMultiResidence - No Action - Classification not found.");
+            logDebug("PMT_CommercialImpactFees - No Action - Classification not found.");
         }
 	} else {
-		logDebug("PMT_ImpactFeesMultiResidence - No Action - Permit Issuance task is null or not active.");
+		logDebug("PMT_CommercialImpactFees - No Action - Permit Issuance task is null or not active.");
 	}
 
 } catch (err) {
-	logDebug("A JavaScript Error occured in PMT_ImpactFeesMultiResidence: " + err.message);
+	logDebug("A JavaScript Error occured in PMT_CommercialImpactFees: " + err.message);
 	logDebug(err.stack);
 }
-logDebug("---------- end  PMT_ImpactFeesMultiResidence ----------");
+logDebug("---------- end  PMT_CommercialImpactFees ----------");
  
-
+function clobber(
  
