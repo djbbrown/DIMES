09c01cff1dae82d3a421400f15e133a19fbbe9a9
MesaDimes <brian.odell@mesaaz.gov>
2016-09-20 15:17:59 -0700

    9

--- SCRIPTS/EVENT/ASB;Planning!Planning and Zoning!NA!NA.js
+++ SCRIPTS/EVENT/ASB;Planning!Planning and Zoning!NA!NA.js
@@ -2,7 +2,7 @@
 // Script #:	312, 311
 // Script Name: ASB;Planning!Planning and Zoning!NA!NA.js
 /*==================================================================*/
-include("PLN_PrePlatReqDoc");
+include("PLN_PrePlatReqDoc_ASBOnly");
 
 // added by Vance Smith (Mesa)
 include("PLN_DevelopmentUnitPlan"); // 311
