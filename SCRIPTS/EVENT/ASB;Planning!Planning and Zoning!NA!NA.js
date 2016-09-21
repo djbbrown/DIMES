e16e6a031a196d29b610cf45bc5130814b4666c1
MesaDimes <brian.odell@mesaaz.gov>
2016-09-21 13:57:37 -0700

    adding 275 ref

--- SCRIPTS/EVENT/ASB;Planning!Planning and Zoning!NA!NA.js
+++ SCRIPTS/EVENT/ASB;Planning!Planning and Zoning!NA!NA.js
@@ -1,8 +1,9 @@
 /*===================================================================
-// Script #:	312, 311
+// Script #:	312, 311, 275
 // Script Name: ASB;Planning!Planning and Zoning!NA!NA.js
 /*==================================================================*/
 include("PLN_PrePlatReqDoc_ASBOnly");
+include("PLN_RequireDrawingsDoc_ASBOnly");
 
 // added by Vance Smith (Mesa)
 include("PLN_DevelopmentUnitPlan"); // 311
--- SCRIPTS/EVENT/ASIUB;Planning!Planning and Zoning!NA!NA.js
+++ SCRIPTS/EVENT/ASIUB;Planning!Planning and Zoning!NA!NA.js
@@ -1,5 +1,7 @@
 /*===================================================================
-// Script #:	312
+// Script #:	312, 275
 // Script Name: ASIUB;Planning!Planning and Zoning!NA!NA.js
 /*==================================================================*/
 include("PLN_PrePlatReqDoc");
+include("PLN_RequireDrawingsDoc");
+
