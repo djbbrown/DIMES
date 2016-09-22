a7ca486923f4718af7db4bab86787695bc8c4594
John Cheney <JCheney@mesaaz.gov>
2016-09-22 13:03:15 -0700

    implementing PLN_ExistingZoning

--- SCRIPTS/EVENT/ASA;Planning!Admin Review!NA!NA.js
+++ SCRIPTS/EVENT/ASA;Planning!Admin Review!NA!NA.js
@@ -2,3 +2,6 @@
 // Script Number: 73
 ==================================================================*/
 include("PLN_AutopopulateZoningClassification")
+
+// added by John Cheney (Mesa) 9/22/2016
+include("PLN_ExistingZoning");
--- SCRIPTS/EVENT/ASIUA;Planning!Subdivision!NA!NA.js
+++ SCRIPTS/EVENT/ASIUA;Planning!Subdivision!NA!NA.js
@@ -1,2 +1,5 @@
 // ASIUA;Planning!Subdivision!NA!NA
 include("PLN_AutopopulateZoningClassification"); // Script 73
+
+// added by John Cheney (Mesa) 9/22/2016
+include("PLN_ExistingZoning");
