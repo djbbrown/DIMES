ac72ea0c0ad85cb78912d0ba847ab38b883ddd9a
Kevin Ford <kford@accela.com>
2016-09-22 15:48:36 -0700

    Update to trigger on workflow

--- SCRIPTS/EVENT/WTUA;Permits!Sign!NA!NA.js
+++ SCRIPTS/EVENT/WTUA;Permits!Sign!NA!NA.js
@@ -3,6 +3,7 @@
 // Script Name: WTUA;Permits!Sign!NA!NA.js
 ==================================================================*/
 include("PMT_SignPermitFee");
+include("PMT_SignFeesWorkflow");
 include("PMT_AssessTechFee"); // must be run last
 include("PMT_ElectricalFeeforSigns"); //
 
