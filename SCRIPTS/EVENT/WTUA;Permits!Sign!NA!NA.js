4d4f16acdffe17a2342933a5b547a2f717840154
Kevin Ford <kford@accela.com>
2016-09-26 08:01:02 -0700

    Updated fee order
    This was required to ensure that "SGN040" applied correctly.

--- SCRIPTS/EVENT/WTUA;Permits!Sign!NA!NA.js
+++ SCRIPTS/EVENT/WTUA;Permits!Sign!NA!NA.js
@@ -4,8 +4,8 @@
 ==================================================================*/
 include("PMT_SignPermitFee");
 include("PMT_SignFeesWorkflow");
-include("PMT_AssessTechFee"); // must be run last
 include("PMT_ElectricalFeeforSigns"); //
+include("PMT_AssessTechFee"); // must be run last
 
 // added by Vance Smith (Mesa)
 include("PMT_SubmittalCycle");
