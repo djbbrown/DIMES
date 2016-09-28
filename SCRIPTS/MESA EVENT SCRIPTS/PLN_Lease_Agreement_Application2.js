07007143e9f3d4af4f4c64d6ecdb85915b9e1d15
SteveVMesa <Steve.Veloudos@mesaaz.gov>
2016-09-28 09:21:09 -0700

    Updates

--- SCRIPTS/MESA EVENT SCRIPTS/PLN_Lease_Agreement_Application2.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PLN_Lease_Agreement_Application2.js
@@ -1,12 +1,11 @@
 /*===================================================================*/
 // Script Number: 254
-// Script Name: PLN_Lease_Agreement_Application.js
+// Script Name: PLN_Lease_Agreement_Application2.js
 // Script Description: 	If the Applicant is not the same as the Owner then the document Lease Agreement for Application is required to submit the application
-// Script Run Event: ASIUB;Planning!Group Home!Application!NA.js
+// Script Run Event: ASB;Planning/Group Home/Application/!~.js
 // Testing Record: GHAP16-00324 Doc: PLN_GH
 // Version   |Date      |Engineer         |Details
-//  1.0      |09/19/16  |Steve Veloudos   |Initial Release
-//  2.0      |09/27/16  |Steve Veloudos   |Adjusted for not stopping submission
+//  1.0      |09/28/16  |Steve Veloudos   |Initial Release
 /*==================================================================*/
 
 try {
@@ -64,21 +63,35 @@ try {
         }
         //Compare Names & set flag
         if(FullName == OwnerName)
-        {
-        SameNameFlag = 1; 
-        }
+          {
+            SameNameFlag = 1; 
+          }
     
         //Get documents
-        docListArray = getDocumentList();
-        for (doc in docListArray)
+        var docList = aa.env.getValue("DocumentModelList");
+        var docListCount = 0;
+
+      if((docList == null) || (docList == ""))
         {
-            DocCat =  docListArray[doc].getDocCategory();
-            
-            //Check doc category
-            if(DocCat == "Lease Agreement for Application")
+            docList = aa.document.getDocumentListByEntity(capId.toString(),"TMP_CAP").getOutput();
+            docListCount = docList.size();
+        }
+      else
+        {
+            docListCount = docList.size();
+        }
+      
+      if (docListCount > 0)
+        {
+            for(x=0;x<docListCount;x++)
+            { 
+            if((docList.get(x) != null)
+                && (docList.get(x).getDocGroup() == "PLN_GH")
+                && (docList.get(x).getDocCategory() == "Lease Agreement for Application")) 
             {
                 DocFlag = 1; 
                 break; 
+            }
             }
         }
                 
