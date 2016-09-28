bdbb6e65f77e8a771c069fa6b70e5d6f51dfb0f5
Vance Smith <VSmith@mesaaz.gov>
2016-09-28 10:05:08 -0700

    added getBuildingInspectorObject function to custom

--- INCLUDES_CUSTOM.js
+++ INCLUDES_CUSTOM.js
@@ -2156,3 +2156,47 @@ function isInFloodZone()
         return false;
     }
 }
+
+function getBuildingInspectorObject() // optional altId
+{
+    // code officer aka inspector
+
+    if ( arguments.length == 1 )
+    {
+        capId = aa.cap.getCapID(arguments[0]).getOutput(); // this is expected in getGisInfo
+    }
+
+    var inspector = getGISInfo("Accela/AccelaBoundaries", "Building_Inspection_Areas", "NAME");
+    if (inspector) 
+    {
+        logDebug("Inspector: " + inspector);
+        var nameArray = inspector.split(" ");
+        var inspRes = null;
+        switch (nameArray.length)
+        {
+            case 1:
+                inspRes = aa.person.getUser(inspector);
+                break;
+            case 2:
+                inspRes = aa.person.getUser(nameArray[0], "", nameArray[1]);
+                break;
+            case 3:
+                inspRes = aa.person.getUser(nameArray[0], nameArray[1], nameArray[2]);
+                break;
+        }
+        
+		if (inspRes.getSuccess())
+        {
+			return inspRes.getOutput();
+		}
+        else
+        {
+            logDebug("Failed to create building inspector object!");
+            return false;
+        }
+    }
+	else 
+	{
+		return false;
+	}
+}
