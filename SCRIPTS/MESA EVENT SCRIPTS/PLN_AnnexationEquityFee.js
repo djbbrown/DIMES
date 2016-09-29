645d8bee0c044b5758da96974ebf1aff94600906
John Cheney <JCheney@mesaaz.gov>
2016-09-29 16:01:47 -0700

    added PLN_AnnexationEquityFe

---
+++ SCRIPTS/MESA EVENT SCRIPTS/PLN_AnnexationEquityFee.js
@@ -0,0 +1,67 @@
+/*===================================================================
+ Versions:
+ 9/29/2016-A	John Cheney			initial
+ ---------------------------------------------------------------------
+ Script Number: 329
+ Script Name: PLN_AnnexationEquityFee.js
+ Script Agency: Mesa
+ Script Description: 	
+
+Calculate and assess the Annexation Equity Fee when ASI fields “Use Classification” and “Equity Fee Variable” are entered.
+
+Note: we cannot get previous values in ASIUA event, 
+so levy fee every ASIUA if other conditions are met
+
+Specs:
+    https://portal.accelaops.com/projects/Mesa/Lists/Script%20Tracker/DispForm.aspx?ID=329 
+
+Script Run Events: ASIUA
+
+Script Parents:
+      ASIUA;Planning!Annexation!NA!NA
+
+==================================================================*/
+
+// logDebug("---------- start  PLN_AnnexationEquityFee ----------");
+
+try
+{
+    var useClass = AInfo["Use Classification"];
+    var equityFee = AInfo["Equity Fee Variable"];
+
+    if(useClass && equityFee){
+        var feeCode = "ANX020";
+        // found the required values..
+
+        // remove existing fee if found and not invoiced
+        if (feeExists(feeCode, "NEW")){
+            voidRemoveFee(feeCode);
+    		logDebug("PLN_AnnexationEquityFee - Removed fee with code " + feeCode + " and status NEW");
+        }
+
+        // levy fee
+        var rate = lookup("PLN ANX Equity_Fee_Rates", useClass);
+
+        if(rate){
+            var fee = Number(rate) * Number(equityFee);
+            if(fee > 0){
+                addFee(feeCode, "PLN_ANX", "FINAL", fee, "N");
+                logDebug("PLN_AnnexationEquityFee - Assessed fee of " + String(fee));
+            } else {
+                logDebug("PLN_AnnexationEquityFee - fee = 0!");
+            }
+        } else {
+            logDebug("PLN_AnnexationEquityFee - No Action - Value Desc not found.");
+        }
+    } else {
+        logDebug("PLN_AnnexationEquityFee - No Action - Out of Scope.  Use Classification = " + useClass + " and Equity Fee Variable = " + equityFee);
+    }
+}
+catch (err)
+{
+  logDebug("PLN_AnnexationEquityFee - JavaScript Error: " + err.message);
+  logDebug(err.stack);
+}
+
+
+// logDebug("---------- end  PLN_AnnexationEquityFee ----------");
