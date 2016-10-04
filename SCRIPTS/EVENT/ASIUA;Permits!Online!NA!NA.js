d1b647f83f6c12a9d28523a79efeae455fe1f7e3
Jody Bearden <jody.bearden@mesaaz.gov>
2016-10-04 08:50:49 -0700

    Removed reference to Script 326 (PMT_OnlineCommercialPropertyType.js) as it is being canceled and replaced by an expression.

--- SCRIPTS/EVENT/ASIUA;Permits!Online!NA!NA.js
+++ SCRIPTS/EVENT/ASIUA;Permits!Online!NA!NA.js
@@ -1,8 +1,6 @@
 /*===================================================================
 // Script Name: ASIUA;Permits!Online!NA!NA.js
 ==================================================================*/
-include("PMT_OnlineCommercialPropertyType"); // Script 326
-
 // added by Vance Smith (Mesa)
 include("PMT_AutopopulateASIFieldsFromGISAttributes"); // 109
 
