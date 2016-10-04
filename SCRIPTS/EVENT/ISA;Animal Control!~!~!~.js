8c772452478aa39a6848ab189eae768114783d58
Jody Bearden <jody.bearden@mesaaz.gov>
2016-10-04 15:55:14 -0700

    Add debug statement to see if ISA event is being called.

--- SCRIPTS/EVENT/ISA;Animal Control!~!~!~.js
+++ SCRIPTS/EVENT/ISA;Animal Control!~!~!~.js
@@ -1,4 +1,5 @@
 //========================
 //ISA;Animal Control!~!~!~
 //========================
+logDebug("ISA;Animal Control!~!~!~ called....");
 include("ANI_UpdateWorkflowOnInspectionResult"); // Script 359
