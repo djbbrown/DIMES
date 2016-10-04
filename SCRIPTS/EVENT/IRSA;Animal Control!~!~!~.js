a712c8df8fed80ce7d9dbb23a9c146edc39c8544
Jody Bearden <jody.bearden@mesaaz.gov>
2016-10-04 16:03:55 -0700

    Add debug info.

--- SCRIPTS/EVENT/IRSA;Animal Control!~!~!~.js
+++ SCRIPTS/EVENT/IRSA;Animal Control!~!~!~.js
@@ -1,4 +1,5 @@
 //========================
 //IRSA;Animal Control!~!~!~
 //========================
+logDebug("IRSA;Animal Control!~!~!~ called...");
 include("ANI_UpdateWorkflowOnInspectionResult"); // Script 359
