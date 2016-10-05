6427b286ccac25907201bf6c9de674f442f2f46f
Vance Smith <VSmith@mesaaz.gov>
2016-10-04 20:58:45 -0700

    new function

--- INCLUDES_CUSTOM.js
+++ INCLUDES_CUSTOM.js
@@ -2287,3 +2287,40 @@ function getTaskAssignedStaff(wfstr) // optional process name.
 	}
 	return false;
 }
+
+function editAppSpecific_Mesa(itemName,itemValue)  // optional: itemCap
+{
+	var itemCap = capId;
+	var itemGroup = null;
+
+	if (arguments.length == 3) 
+    {
+        itemCap = arguments[2]; // use cap ID specified in args
+    }
+   	
+  	if (useAppSpecificGroupName)
+	{
+		if (itemName.indexOf(".") < 0)
+        { 
+            logDebug("**WARNING: editAppSpecific requires group name prefix when useAppSpecificGroupName is true"); 
+            return false; 
+        }		
+		
+		itemGroup = itemName.substr(0,itemName.indexOf("."));
+		itemName = itemName.substr(itemName.indexOf(".") + 1);
+	}
+   	
+   	var appSpecInfoResult = aa.appSpecificInfo.editSingleAppSpecific(itemCap,itemName,itemValue,itemGroup);
+
+	if (appSpecInfoResult.getSuccess())
+	 {
+	 	if(arguments.length < 3) // if no capId passed update the ASI Array
+        {
+            AInfo[itemName] = itemValue;
+        } 
+	} 	
+	else
+    { 
+        logDebug( "WARNING: " + itemName + " was not updated. Error message: " + appSpecInfoResult.getErrorMessage()); 
+    }
+}
