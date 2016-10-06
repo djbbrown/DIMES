001868f2a586fcbc834ef3700b3e0a466f19d49d
Kevin Ford <kford@accela.com>
2016-10-06 08:08:26 -0700

    Update to run on ASIUA for USF150

--- SCRIPTS/EVENT/ASIUA;Permits!Online!NA!NA.js
+++ SCRIPTS/EVENT/ASIUA;Permits!Online!NA!NA.js
@@ -6,3 +6,4 @@ include("PMT_OnlineCommercialPropertyType"); // Script 326
 // added by Vance Smith (Mesa)
 include("PMT_AutopopulateASIFieldsFromGISAttributes"); // 109
 
+include("PMT_ONLFeeUSF150"); // Fee Scripting USF150 for Online
