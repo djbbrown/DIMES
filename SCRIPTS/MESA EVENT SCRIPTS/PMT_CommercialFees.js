e6206c8dbf8bfd141971b8c2a6b3f84ffd352abd
Kevin Ford <kford@accela.com>
2016-10-06 11:27:44 -0700

    Adding Commercial fee for COM440

---
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_CommercialFees.js
@@ -0,0 +1,16 @@
+/*===================================================================
+// Script Number: 
+// Script Name: Commercial Fees
+// Script Developer: Kevin Ford
+// Script Agency: Accela
+// Script Description: 
+// Script Run Event: Application Submit After
+// Script Parents:
+//		ASA;Permits!Commercial!~!~
+//             
+/*==================================================================*/
+var classification = AInfo["Classification Type"];
+if (classification && classification =='Construction Trailer') {
+	if (feeExists("COM440", "NEW", "INVOICED")) voidRemoveFee("COM440");
+	addFee("COM440", "PMT_RDIF", "FINAL", 1, "N");
+}
