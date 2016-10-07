787d71022c80e289349a2d6b9337119815752961
gurneykevin <kgurney@accela.com>
2016-10-06 22:33:32 -0700

    added call to admin review fees.

--- SCRIPTS/EVENT/ASA;Planning!Admin Review!NA!NA.js
+++ SCRIPTS/EVENT/ASA;Planning!Admin Review!NA!NA.js
@@ -5,3 +5,6 @@ include("PLN_AutopopulateZoningClassification")
 
 // added by John Cheney (Mesa) 9/22/2016
 include("PLN_ExistingZoning");
+
+// added by Kevin Gurney (Accela)
+include("PLN_AdministrativeReviewFees");
