6facd84023ad74f34cd014b35d3d64591cbaefcd
gurneykevin <kgurney@accela.com>
2016-10-07 15:34:49 -0700

    added additional fee logic for more fee assessment

--- SCRIPTS/MESA EVENT SCRIPTS/PLN_PlanningZoningFees.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PLN_PlanningZoningFees.js
@@ -3,11 +3,11 @@
 // Script Name: PLN_PlanningZoningFees
 // Script Developer: Bryan de Jesus
 // Script Agency: Woolpert
-// Script Description: On application submitttal Assess fee “Rezoning”  
+// Script Description: On application submitttal Assess fee Rezoning  
 //		(FP030) at application submittal when Address and Parcel are 
-//		located outside of the “downtown” district (downtown layer 
+//		located outside of the downtown district (downtown layer 
 //		currently exists in GIS)
-//		If inside Downtown district, “Rezoning Downtown” fee should be assessed instead. 
+//		If inside Downtown district, Rezoning Downtown fee should be assessed instead. 
 // Script Run Event: ASA
 // Script Parents:
 //            ASA;Planning!Planning and Zoning!NA!NA 
@@ -74,7 +74,7 @@ using ASI field Total New Lots, Tracts, Parcels.
 //            ASA;Planning!Planning and Zoning!NA!NA 
 /*==================================================================*/
 try{
-	if (AInfo["Pre-Plat"] == "CHECKED" && AInfo["Total New Lots, Tracts, Parcels"] != null) {
+	if (AInfo["Pre-Plat"] == "CHECKED" && AInfo["Rezone"] != "CHECKED" && AInfo["Site Plan Review/Modification"] != "CHECKED" && AInfo["Combined Rezone and Site Plan Review /Modification"] != "CHECKED" && AInfo["Council Use Permit"] != "CHECKED" && AInfo["Development Unit Plan"] != "CHECKED" && AInfo["Rezone - Infill Development District 2"] != "CHECKED" && AInfo["Rezone - Planned Community Development"] != "CHECKED" && AInfo["Planned Community Minor Amendment"] != "CHECKED" && AInfo["Special Use Permit"] != "CHECKED" && AInfo["Minor General Plan Amendment"] != "CHECKED" && AInfo["Total New Lots, Tracts, Parcels"] != null) {
 		addFee("PZ010","PLN_PZ","FINAL",AInfo["Total New Lots, Tracts, Parcels"],"N");
 	}
 } catch (err){
@@ -93,7 +93,7 @@ try{
 /*==================================================================*/
 try{
 	if (AInfo["Rezone - Infill Development District 2"] == "CHECKED" && AInfo["Total New Lots, Tracts, Parcels"] != null) {
-		addFee("PZ090","PLN_PZ","FINAL",AInfo["Total New Lots, Tracts, Parcels"],"N");
+		addFee("PZ090","PLN_PZ","FINAL",acres,"N");
 	}
 } catch (err){
 	logDebug("A JavaScript Error occured: " + err.message);
@@ -111,7 +111,7 @@ try{
 /*==================================================================*/
 try{
 	if (AInfo["Rezone - Planned Community District"] == "CHECKED" && AInfo["Total New Lots, Tracts, Parcels"] != null) {
-		addFee("PZ100","PLN_PZ","FINAL",AInfo["Total New Lots, Tracts, Parcels"],"N");
+		addFee("PZ100","PLN_PZ","FINAL",acres,"N");
 	}
 } catch (err){
 	logDebug("A JavaScript Error occured: " + err.message);
@@ -147,7 +147,7 @@ try{
 /*==================================================================*/
 try{
 	if (AInfo["Planned Community Minor Amendment"] == "CHECKED" && AInfo["Total New Lots, Tracts, Parcels"] != null) {
-		addFee("PZ120","PLN_PZ","FINAL",AInfo["Total New Lots, Tracts, Parcels"],"N");
+		addFee("PZ120","PLN_PZ","FINAL",acres,"N");
 	}
 } catch (err){
 	logDebug("A JavaScript Error occured: " + err.message);
@@ -158,14 +158,50 @@ try{
 // Script Name: PLN_PlanningZoningFees
 // Script Developer: Kevin Gurney
 // Script Agency: Accela
-// Script Description: If Council Use Permit is checked assess Council Use Permit
+// Script Description: If Council Use Permit and no other application types are checked assess Council Use Permit
 // Script Run Event: ASA
 // Script Parents:
 //            ASA;Planning!Planning and Zoning!NA!NA 
 /*==================================================================*/
 try{
-	if (AInfo["Planned Community Minor Amendment"] == "CHECKED") {
+	if (AInfo["Council Use Permit"] == "CHECKED" && AInfo["Pre-Plat"] != "CHECKED" && AInfo["Rezone"] != "CHECKED" && AInfo["Site Plan Review/Modification"] != "CHECKED" && AInfo["Combined Rezone and Site Plan Review /Modification"] != "CHECKED" && AInfo["Development Unit Plan"] != "CHECKED" && AInfo["Rezone - Infill Development District 2"] != "CHECKED" && AInfo["Rezone - Planned Community Development"] != "CHECKED" && AInfo["Planned Community Minor Amendment"] != "CHECKED" && AInfo["Special Use Permit"] != "CHECKED" && AInfo["Minor General Plan Amendment"] != "CHECKED") {
 		addFee("PZ130","PLN_PZ","FINAL",1,"N");
+	}
+} catch (err){
+	logDebug("A JavaScript Error occured: " + err.message);
+}
+
+/*===================================================================
+// Script Number: TBD
+// Script Name: PLN_PlanningZoningFees
+// Script Developer: Kevin Gurney
+// Script Agency: Accela
+// Script Description: If Council Use Permit and any other application type are checked assess Council Use Permit with another application
+// Script Run Event: ASA
+// Script Parents:
+//            ASA;Planning!Planning and Zoning!NA!NA 
+/*==================================================================*/
+try{
+	if (AInfo["Council Use Permit"] == "CHECKED" && (AInfo["Pre-Plat"] == "CHECKED" || AInfo["Rezone"] == "CHECKED" || AInfo["Site Plan Review/Modification"] == "CHECKED" || AInfo["Combined Rezone and Site Plan Review /Modification"] == "CHECKED" || AInfo["Development Unit Plan"] == "CHECKED" || AInfo["Rezone - Infill Development District 2"] == "CHECKED" || AInfo["Rezone - Planned Community Development"] == "CHECKED" || AInfo["Planned Community Minor Amendment"] == "CHECKED" || AInfo["Special Use Permit"] == "CHECKED" || AInfo["Minor General Plan Amendment"] == "CHECKED")) {
+		addFee("PZ135","PLN_PZ","FINAL",1,"N");
+	}
+} catch (err){
+	logDebug("A JavaScript Error occured: " + err.message);
+}
+
+/*===================================================================
+// Script Number: TBD
+// Script Name: PLN_PlanningZoningFees
+// Script Developer: Kevin Gurney
+// Script Agency: Accela
+// Script Description: If Minor General Plan Amendment is CHECKED then assess Minor General Plan Amendment fee
+// Script Run Event: ASA
+// Script Parents:
+//            ASA;Planning!Planning and Zoning!NA!NA 
+/*==================================================================*/
+try{
+	if (AInfo["Minor General Plan Amendment"] == "CHECKED") {
+		addFee("PZ220","PLN_PZ","FINAL",1,"N");
 	}
 } catch (err){
 	logDebug("A JavaScript Error occured: " + err.message);
