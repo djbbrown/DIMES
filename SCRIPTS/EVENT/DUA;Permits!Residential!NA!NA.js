aa0acc15ca61379cdcb3d9ec418125859818dea1
gurneykevin <kgurney@accela.com>
2016-10-10 10:23:03 -0700

    Test Residential Script for document upload

---
+++ SCRIPTS/EVENT/DUA;Permits!Residential!NA!NA.js
@@ -0,0 +1,6 @@
+/*===================================================================
+// Script Number: TBD
+// Script Name: DUA;Permits!Residential!NA!NA.js
+//
+==================================================================*/
+include("PMT_Email_Resdiential_DocUpload");
