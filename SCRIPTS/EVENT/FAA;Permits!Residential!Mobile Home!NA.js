f448814bec265b3b27b1f60a734a918a8d7b8562
gurneykevin <kgurney@accela.com>
2016-10-11 14:29:37 -0700

    adding fee assessment after for manual storage enclosure fee

---
+++ SCRIPTS/EVENT/FAA;Permits!Residential!Mobile Home!NA.js
@@ -0,0 +1,4 @@
+/*===================================================================
+// Script Name: FAA;Permits!Residential!Mobile Home!NA.js
+==================================================================*/
+include("PMT_MobileHomeAssessStorageEnclosureFee");
