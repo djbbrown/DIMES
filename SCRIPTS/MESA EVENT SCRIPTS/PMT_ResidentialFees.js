edcfa7451e61cbd9274126e58d9096642f622c29
Kevin Ford <kford@accela.com>
2016-10-12 07:56:56 -0700

    Update for Civil Engineering Deposit

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_ResidentialFees.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_ResidentialFees.js
@@ -53,3 +53,22 @@ if(wfTask == "Plans Coordination" && wfStatus=="Ready to Issue") {
 		voidRemoveFee("RES290");
 	}
 }
+// RES020 - Civil Engineering Deposit
+try {
+	// Count up the sheets from the ASIT
+	pRInfoTable = loadASITable("PLAN REVIEW INFORMATION");
+	var totalSheets = 0;
+	totalSheets += sumASITColumn(pRInfoTable, "Number of Civil Engineering Sheets");
+	
+	if (feeExists("RES020", "NEW", "INVOICED")) {
+		voidRemoveFee("RES020");
+	}
+	addFee("RES020", "PMT_RES","FINAL", totalSheets, "N");
+	if(!totalSheets > 0 && feeExists("RES020", "NEW", "INVOICED")){
+		logDebug("")
+		voidRemoveFee("RES020");
+	}
+}
+catch (err) {
+	aa.print("A JavaScript Error occurred: " + err.message);
+}
