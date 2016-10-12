78fb884c885b8544fdf62b4c671875de6e19a2b0
Jody Bearden <jody.bearden@mesaaz.gov>
2016-10-11 18:49:48 -0700

    Re-commit lost files from 10/4

---
+++ SCRIPTS/MESA EVENT SCRIPTS/ANI_UpdateWorkfowOnInspectionResult.js
@@ -0,0 +1,28 @@
+/*===================================================================
+// Script Number: 359
+// Script Name: ANI_UpdateWorkfowOnInspectionResult.js
+// Script Developer: Jody Bearden
+// Script Agency: Mesa
+// Script Description: When any inspection is resulted with any
+//                     result, update the workflow task "Animal
+//                     Control Officer" to the same status.
+// Script Run Event: ISA
+// Script Parents:
+//            ISA;Animal Control!~!~!~
+===================================================================*/
+/* test with  ANI16-00033 */
+
+try
+{
+	// get result of currently resulted inspection
+	var status = inspResult;
+	if (status) {
+		// update WF task
+		setTask("Animal Control Officer", "Y", "N");
+		updateTask("Animal Control Officer", status, "Updated by script.", "Updated by script.");
+	}
+}
+catch (err)
+{
+  logDebug("A JavaScript Error occured: " + err.message);
+}
