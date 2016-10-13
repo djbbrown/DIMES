2d7811af653208a47c12f8a6d9f178ce1118a096
SteveVMesa <Steve.Veloudos@mesaaz.gov>
2016-10-13 15:52:57 -0700

    Updated

--- SCRIPTS/EVENT/ASA;Enforcement!Environmental!Construction!NA.js
+++ SCRIPTS/EVENT/ASA;Enforcement!Environmental!Construction!NA.js
@@ -1,3 +1,4 @@
 //====================================================
 // Script ASA;Enforcement!Environmental!Construction!NA.js
 //====================================================
+include("ENF_AutoPopulateInspection");
