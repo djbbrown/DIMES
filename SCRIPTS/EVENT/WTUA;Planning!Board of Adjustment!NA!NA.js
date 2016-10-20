9869202777f8e4a784819353125a8c2efb58081c
Vance Smith <VSmith@mesaaz.gov>
2016-10-19 17:17:13 -0700

    pushed for now, with include commented out

--- SCRIPTS/EVENT/WTUA;Planning!Board of Adjustment!NA!NA.js
+++ SCRIPTS/EVENT/WTUA;Planning!Board of Adjustment!NA!NA.js
@@ -1,2 +1,5 @@
 //Schedule Board of Adjustment meeting and set workflow due dates
 include("PLN_ScheduleBoardOfAdjustment");
+
+// added by Vance Smith (Mesa)
+include("PLN_CreateDocXDaysBefore"); // 276
