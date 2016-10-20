80bb010bc0087bb808e028314c9868e3d03a3ca5
gurneykevin <kgurney@accela.com>
2016-10-20 12:08:47 -0700

    temporarily remarking out script includes to check performance issue.

--- SCRIPTS/EVENT/WTUA;Planning!~!~!~.js
+++ SCRIPTS/EVENT/WTUA;Planning!~!~!~.js
@@ -1,2 +1,2 @@
-include("PLN_AdjustSubstantiveReviewExpirationDate");
-include("PLN_NumberOfSubmittals");
+//include("PLN_AdjustSubstantiveReviewExpirationDate");
+//include("PLN_NumberOfSubmittals");
