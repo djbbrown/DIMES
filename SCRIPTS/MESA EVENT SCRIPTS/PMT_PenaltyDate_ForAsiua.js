8ebff8e2b83b73d10b1f672ee14759cd076b299a
MesaDimes <brian.odell@mesaaz.gov>
2016-10-24 15:35:25 -0700

    hunt spec char 1-2

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_PenaltyDate_ForAsiua.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_PenaltyDate_ForAsiua.js
@@ -1,4 +1,29 @@
-﻿ //---------------------------------------------------------------------
+ //---------------------------------------------------------------------
+// Script Number: 66, 169
+// Script Name: PMT_PenaltyDate_ForAsiua.js 
+// Script Developer: Brian O'Dell
+// Script Agency: City of Mesa
+// Script Description: 
+// 		Script when a status of Accepted Plan Review Req is 
+//		applied to wf task Application Submittal then auto-fill 
+//		Penalty Date ASI field with date based on 
+//		the number of days identified in the Turn around time field.  
+//		The date generated should be based on a 4 day work week, 
+//		Monday through Thursday, should not include Fridays, Weekends 
+//		or Holidays.
+//
+//		This has been changed to include script 169 and the
+//		"Plan Review Penalty Date" ASI field and Workflow task
+//		"Plans Distribution"
+//
+//		**NOTE: User changed spec to include ASIUA if workflow after "Application Submittal",
+//		so this various of script 66 was created.
+//
+//
+// Script Run Event: WTUA, ASIUA
+// Script Parents:
+//              WTUA;Permits!~!~!~  (see other script for specs)
+//		ASIUA;Permits!~!~!~ (this script is hooked to this parent)
 // 
 //==================================================================*/
 
