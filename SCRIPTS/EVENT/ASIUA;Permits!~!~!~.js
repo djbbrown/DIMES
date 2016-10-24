bbc9e42bbc42050539b63ee972d1a7f049aee787
MesaDimes <brian.odell@mesaaz.gov>
2016-10-24 15:11:08 -0700

    user mod 20161024-2

--- SCRIPTS/EVENT/ASIUA;Permits!~!~!~.js
+++ SCRIPTS/EVENT/ASIUA;Permits!~!~!~.js
@@ -1,3 +1,6 @@
 include("PMT_ClearanceTo")
 // added by John Cheney (Mesa) 9/19/2016
 include("PMT_CopyPenaltyDateToDueDate")
+
+// added by Brian ODell (Mesa) 10/24/2016
+include("PMT_PenaltyDate_ForAsiua")
