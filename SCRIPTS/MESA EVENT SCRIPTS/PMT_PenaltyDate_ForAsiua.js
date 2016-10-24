c845aa431c60fb544f1fd001ef1f343935b18ce8
MesaDimes <brian.odell@mesaaz.gov>
2016-10-24 15:12:41 -0700

    removing spec chars

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_PenaltyDate_ForAsiua.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_PenaltyDate_ForAsiua.js
@@ -6,13 +6,13 @@
 
  //---------------------------------------------------------------------
 // Script Number: 66, 169
-// Script Name: PMT_PenaltyDate.js 
+// Script Name: PMT_PenaltyDate_ForAsiua.js 
 // Script Developer: Brian O'Dell
 // Script Agency: City of Mesa
 // Script Description: 
-// 		Script � when a status of �Accepted � Plan Review Req� is 
-//		applied to wf task �Application Submittal� then auto-fill 
-//		�Penalty Date� ASI field with date based on 
+// 		Script when a status of Accepted Plan Review Req is 
+//		applied to wf task Application Submittal then auto-fill 
+//		Penalty Date ASI field with date based on 
 //		the number of days identified in the Turn around time field.  
 //		The date generated should be based on a 4 day work week, 
 //		Monday through Thursday, should not include Fridays, Weekends 
