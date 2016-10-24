12a9633fd59c349982d78e9035ae0d905c1ea242
MesaDimes <brian.odell@mesaaz.gov>
2016-10-24 15:31:38 -0700

    spec char hunt 1-1

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_PenaltyDate_ForAsiua.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_PenaltyDate_ForAsiua.js
@@ -1,35 +1,4 @@
-﻿//*===================================================================
-// Versions:
- // 9/?/2016	 Brian O'Dell			initial
- // 9/28/2016	 John Cheney			added include("PMT_CopyPenaltyDateToDueDate") 
-// 10/17/2016	 John Cheney			removed include("PMT_CopyPenaltyDateToDueDate")
-
- //---------------------------------------------------------------------
-// Script Number: 66, 169
-// Script Name: PMT_PenaltyDate_ForAsiua.js 
-// Script Developer: Brian O'Dell
-// Script Agency: City of Mesa
-// Script Description: 
-// 		Script when a status of Accepted Plan Review Req is 
-//		applied to wf task Application Submittal then auto-fill 
-//		Penalty Date ASI field with date based on 
-//		the number of days identified in the Turn around time field.  
-//		The date generated should be based on a 4 day work week, 
-//		Monday through Thursday, should not include Fridays, Weekends 
-//		or Holidays.
-//
-//		This has been changed to include script 169 and the
-//		"Plan Review Penalty Date" ASI field and Workflow task
-//		"Plans Distribution"
-//
-//		**NOTE: User changed spec to include ASIUA if workflow after "Application Submittal",
-//		so this various of script 66 was created.
-//
-//
-// Script Run Event: WTUA, ASIUA
-// Script Parents:
-//              WTUA;Permits!~!~!~  (see other script for specs)
-//		ASIUA;Permits!~!~!~ (this script is hooked to this parent)
+﻿ //---------------------------------------------------------------------
 // 
 //==================================================================*/
 
@@ -84,12 +53,3 @@ catch (err)
 {
   logDebug("A JavaScript Error occured: " + err.message);
 }
-
-
-
-
-
-
-
-
-
