bc5a62422917ec3f76701cf4c2e60afaa50f25f3
Michael Kniskern <MKniske@mesaaz.gov>
2016-10-25 12:31:13 -0700

    script 310 - rollback code change - MRK

--- SCRIPTS/EVENT/ASB;Permits!Addenda or Deferred!NA!NA.js
+++ SCRIPTS/EVENT/ASB;Permits!Addenda or Deferred!NA!NA.js
@@ -1,3 +1,3 @@
 //include("PMT_OneTenthAcre");
 //added by Michael Kniskern (City of Mesa) - Script 310
-include("PMT_BuildingAddendaCodeModificationForm");
+//include("PMT_BuildingAddendaCodeModificationForm");
