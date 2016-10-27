4409baf377e8d95ca0acf3465a48d5e4bae882cf
Kevin Ford <kford@accela.com>
2016-10-27 16:14:40 -0700

    Updated to remove ONL010

--- SCRIPTS/EVENT/ASA;Permits!Online!NA!NA.js
+++ SCRIPTS/EVENT/ASA;Permits!Online!NA!NA.js
@@ -5,7 +5,7 @@ include("PMT_OnlineCommercialPropertyType"); // Script 326
 include("PMT_PopulateGasServiceAndElectricService"); // Script 347
 include("PMT_ONLFee030"); // Fee scripting 030
 include("PMT_ONLFee060"); // Fee scripting 060
-include("PMT_ONLFee010"); // Fee scripting 010
+// include("PMT_ONLFee010"); // Fee scripting 010
 include("PMT_ONLFeeUSF150"); // Fee Scripting USF150 for Online
 include("PMT_ONLFeeTech"); // Note the Tech Fee needs to be called last of any other fee calc scripts.
 
