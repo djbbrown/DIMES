5583697ed5339cb8d7ec8f7c9f486622c7c9898a
gurneykevin <kgurney@accela.com>
2016-11-01 10:50:55 -0700

    removing script 343

--- SCRIPTS/EVENT/ISA;Permits!~!~!~.js
+++ SCRIPTS/EVENT/ISA;Permits!~!~!~.js
@@ -1,3 +1,3 @@
 //ISA: Permits!~!~!~
 //include("PMT_Inspection_Added_Manually");
-include("PMT_SetInspectionScheduledDate"); // Script 343
+//include("PMT_SetInspectionScheduledDate"); // Script 343  Removing since not needed for manually scheduled inspections in AA.
