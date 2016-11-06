c53ace89ea7d590e89ca92d306831bad4a3f76d7
MesaDimes <brian.odell@mesaaz.gov>
2016-11-06 16:34:03 -0700

    v1

--- SCRIPTS/MESA EVENT SCRIPTS/TRA_RelateToSpecialEventRecord.js
+++ SCRIPTS/MESA EVENT SCRIPTS/TRA_RelateToSpecialEventRecord.js
@@ -9,7 +9,14 @@
 //		Application No" ASI field, Validate and relate LIC number 
 //		as the parent record on ASA.
 //
-// Script Run Event: ASA / ASIUA
+//		updated spec (per Mong)
+//		Test if valid Special Event record type, if not
+//		block submission/save and display error message
+//
+//		due to change in spec, changing from ASA/ASIUA to ASB/ASIUB
+//
+//
+// Script Run Event: ASB / ASIUB
 // Script Parents:
 //             ASA:Transporation/Temporary Traffic Control/NA/NA
 //             ASIUA:Transporation/Temporary Traffic Control/NA/NA
@@ -19,22 +26,61 @@
 
 try
 {
-  // check if parent exists
   var specialEvent = AInfo["Special Event Application No."];
   var getCapResult = aa.cap.getCapID(specialEvent);
+  var goodParent = false;
+  var commentBlah = "";
+
+  //mkyOutput += "specialEvent: "+specialEvent+" \r";
   
-  // if parent exists, addParent(parent capId)
   if (getCapResult.getSuccess())
   {
-    addParent("" + specialEvent );
-    //mkyOutput += "specialEvent   found: " + specialEvent + " \r";
-    //logDebug("Special Event App found and added as parent");
+    //mkyOutput += "Record found \r";
+
+    var seCapId = aa.cap.getCapID(specialEvent).getOutput();
+    //mkyOutput += "seCapId: "+seCapId+" \r";
+
+    var seCapTypeStr = aa.cap.getCap(seCapId).getOutput().getCapType().toString();
+    //mkyOutput += "seCapTypeStr: "+seCapTypeStr+" \r";
+
+    var seCapTypeArray = seCapTypeStr.split("/");
+    //mkyOutput += "seCapTypeArray[2]: "+seCapTypeArray[2]+" \r";
+
+    if (seCapTypeArray[2] == "SpecialEvent")
+    {
+      goodParent = true;
+    }    
+
+    if (goodParent)
+    {
+      addParent("" + specialEvent );
+      //mkyOutput += "Special Event record type confirmed and added as parent \r";
+      //comment("Special Event record type confirmed and added as parent");
+    }
+    else
+    {
+      //mkyOutput += "The Special Event Application No ("+specialEvent+") is not a Special Event. ";
+      //mkyOutput += Please enter a valid Special Event Application No. \r";
+      commentBlah = "The Special Event Application No ("+specialEvent+") is not a Special Event. ";
+      commentBlah += "Please enter a valid Special Event Application No.";
+      showMessage = true;
+      comment(commentBlah);
+      cancel = true;  
+    }
+
   }
   else
   { 
-    //mkyOutput += "ERROR: could not find parent specialEvent : " + specialEvent + " \r";
-    //logDebug("**ERROR: could not find Special Event App (" + specialEvent + "): " + getCapResult.getErrorMessage());
-  }
+    commentBlah = "The Special Event Application No is required.";
+    showMessage = true;
+    comment(commentBlah);
+    cancel = true;  
+  }  
+
+
+
+
+
 
 }
 catch (err)
