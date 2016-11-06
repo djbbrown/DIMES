d3db1a5eae20a0994772ec5a83fab1ce3be07a45
MesaDimes <brian.odell@mesaaz.gov>
2016-11-06 16:30:22 -0700

    user mod 20161106-01

--- SCRIPTS/EVENT/ASIUA;Transportation!Temporary Traffic Control!NA!NA.js
+++ SCRIPTS/EVENT/ASIUA;Transportation!Temporary Traffic Control!NA!NA.js
@@ -2,4 +2,4 @@
 // Script Number: 283
 // Script Name: ASIUA;Transportation!Temporary Traffic Control!NA!NA.js
 ==================================================================*/
-include("TRA_RelateToSpecialEventRecord");
+//include("TRA_RelateToSpecialEventRecord");  // moving to ASIUB (bodell)
