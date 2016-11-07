7a28e1d85119b0027438e598f31d571540f66b9f
MesaDimes <brian.odell@mesaaz.gov>
2016-11-06 18:46:25 -0700

    v1.0

---
+++ SCRIPTS/MESA EVENT SCRIPTS/TRA_TestRelateToSpecialEventRecord.js
@@ -0,0 +1,79 @@
+//*===================================================================
+//
+// Script Number: 283
+// Script Name: TRA_RelateToSpecialEventRecord.js
+// Script Developer: Brian O'Dell
+// Script Agency: City of Mesa
+// Script Description: 
+// 		When LIC number is entered into "Special Event License 
+//		Application No" ASI field, Validate and relate LIC number 
+//		as the parent record on ASA.
+//
+//		updated spec (per Mong)
+//		Test if valid Special Event record type, if not
+//		block submission/save and display error message
+//
+//		due to change in spec, changing from ASA/ASIUA to ASB/ASIUB
+//
+//
+// Script Run Event: ASB / ASIUB
+// Script Parents:
+//             ASB:Transporation/Temporary Traffic Control/NA/NA
+//             ASIUB:Transporation/Temporary Traffic Control/NA/NA
+// 
+//==================================================================*/
+
+
+try
+{
+  var specialEvent = AInfo["Special Event Application No."];
+  var getCapResult = aa.cap.getCapID(specialEvent);
+  var goodParent = false;
+  var commentBlah = "";
+
+  if (getCapResult.getSuccess())
+  {
+    var seCapId = aa.cap.getCapID(specialEvent).getOutput();
+
+    var seCapTypeStr = aa.cap.getCap(seCapId).getOutput().getCapType().toString();
+    
+    var seCapTypeArray = seCapTypeStr.split("/");
+    
+    if (seCapTypeArray[2] == "SpecialEvent")
+    {
+      goodParent = true;
+    }    
+
+    if (goodParent)
+    {
+      // do nothing, need to addParent on ASA event (need capID established before it can create relationship)
+      //addParent("" + specialEvent);
+    }
+    else
+    {
+      commentBlah += "The Special Event Application No ("+specialEvent+") is not a Special Event. ";
+      commentBlah += "Please enter a valid Special Event Application No.";
+      showMessage = true;
+      comment(commentBlah);
+      cancel = true;  
+    }
+
+  }
+  else
+  { 
+    commentBlah += "The Special Event Application No is required.";
+    showMessage = true;
+    comment(commentBlah);
+    cancel = true;  
+  }  
+
+}
+catch (err)
+{
+  logDebug("A JavaScript Error occured: " + err.message);
+}
+
+
+
+
+
