f457253f549edd416e23a75e75d43c784f2159ae
MesaDimes <brian.odell@mesaaz.gov>
2016-11-16 10:30:44 -0700

    v1

---
+++ SCRIPTS/EVENT/ASB;Permits!Online!NA!NA.js
@@ -0,0 +1,6 @@
+/*===================================================================
+// Script Name: ASB;Permits!Online!NA!NA.js
+==================================================================*/
+
+// added by Brian O'Dell (MESA) - 382
+include("PMT_OwnerBuilderDocReq_ASBOnly");
