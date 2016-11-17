f5b825a4dd77a00db7bd354278a59c317dd09b5d
Kevin Ford <kford@accela.com>
2016-11-17 11:41:45 -0800

    Update for Master Plan

--- SCRIPTS/EVENT/WTUA;Permits!Master Plan!NA!NA.js
+++ SCRIPTS/EVENT/WTUA;Permits!Master Plan!NA!NA.js
@@ -2,6 +2,7 @@
 // Script Number: 001
 // Script Name: WTUA;Permits!Master Plan!NA!NA.js
 ==================================================================*/
+include("PMT_ResubmittalFee");
 include("PMT_PlanReviewHoursFee");
 include("PMT_SetPermitIssuedDate");
 // added by Vance Smith (Mesa)
@@ -9,5 +10,3 @@ include("PMT_SubmittalCycle");
 
 // added by Kevin Gurney (Accela)
 include("PMT_AssessTechFee");
-
-include("PMT_ResubmittalFee");	
