bed05680be56608ad685d2c4a6aa3853243324e2
nical09 <nalbert@accela.com>
2016-11-29 12:33:56 -0500

    Added additional criteria and assignment

--- SCRIPTS/MESA EVENT SCRIPTS/ENF_ScheduleFollowUpInspection.js
+++ SCRIPTS/MESA EVENT SCRIPTS/ENF_ScheduleFollowUpInspection.js
@@ -1,4 +1,4 @@
-/*===================================================================
+/*==================================================================================================
 // Script Number: 019
 // Script Name:ENF_ScheduleFollowUpInspection
 // Script Developer: Raminder Gill
@@ -9,12 +9,15 @@
 // Script Parents:
 //	IRSA;Enforcement!Case!~!~ 
 //	IRSA;Enforcement!Environmental!~!~ 
-//	11/23/2016 - possible fix included - nalbert
-//      
-//            
-/*==================================================================*/
+//	11/29/2016 nalbert - Added assignment to inspector of Initial inspection or to Environmental Department
+//               		Also added from defect #103 - 
+//						follow-up insp status = extension -> create follow-up
+// 						follow-up insp status = in violation -> create follow-up
+//				
+/*=================================================================================================*/
 
-if (inspType == "Initial Inspection" && matches(inspResult, "In Violation", "3rd Party Abatement")) {
+if (inspType == "Initial Inspection" && matches(inspResult, "In Violation", "3rd Party Abatement") ||
+	inspType == "Follow-Up Inspection" && matches(inspResult, "Extension", "In Violation")) {
 		
 	var inspInterval = AInfo["Inspection Interval"];
 	var daysOut = 14;
@@ -24,7 +27,50 @@ if (inspType == "Initial Inspection" && matches(inspResult, "In Violation", "3rd
 		daysOut = 7;
 	if (inspInterval == "3 Days")
 		daysOut = 5;
+	
+// get inspector of Initial inspection or assign to Environmental Department
+	
+	var inspUserId = getInspector("Initial Inspection");
+	logDebug("inspector: " + inspUserId);
+	
+	if (!matches(inspUserId, " ", null)){
+		scheduleInspection("Follow-Up Inspection", daysOut, inspUserId);
+		logDebug("scheduled inspection with last inspector");
+	}else{
+		scheduleInspection("Follow-Up Inspection", daysOut);
+		var needDept = true;
+		logDebug("need department");
+	}
 
-	scheduleInspection("Follow-Up Inspection", daysOut);
+	if (needDept){
+		var dept = "Environmental";
+		setInspectionDepartment(dept);
+	}
+}
+
+// helper function
+function setInspectionDepartment(dept){
+	logDebug("in function: " + dept);
+  if(dept == null || dept == "")
+    return;
+// get list of inspections associated with record
+		var inspObj = aa.inspection.getInspections(capId);
+		if (inspObj.getSuccess()) {
+			var inspList = inspObj.getOutput();
+			logDebug("Number of inspections: " + inspList.length);
+			for(i in inspList) {
+				if (matches(inspList[i].getInspectionStatus(), "Scheduled")) {
+					var inspType = inspList[i].getInspectionType();
+						if (inspType == "Follow-Up Inspection"){
+						logDebug("Inspection type: " + inspList[i].getInspectionType());
+						logDebug("Inspection status: " + inspList[i].getInspectionStatus());
+						logDebug("Inspection ID: " + inspList[i].getIdNumber());
+						
+						assignInspection(inspList[i].getIdNumber(), dept);
+						}
+				}
+			}
+		}
+
 }
 
