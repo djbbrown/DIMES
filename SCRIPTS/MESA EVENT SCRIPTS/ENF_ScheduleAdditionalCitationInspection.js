cd3dca854fd90796915fd99a84149058ac65f5a4
nical09 <nalbert@accela.com>
2016-11-29 12:34:47 -0500

    Added additional criteria

--- SCRIPTS/MESA EVENT SCRIPTS/ENF_ScheduleAdditionalCitationInspection.js
+++ SCRIPTS/MESA EVENT SCRIPTS/ENF_ScheduleAdditionalCitationInspection.js
@@ -10,6 +10,9 @@
 //	IRSA;Enforcement/Case/Code Rental Issue/NA
 //	IRSA;Enforcement/Case/Code Sign Issue/NA
 //	IRSA;Enforcement/Environmental/NA/NA 
+// 11/29/2016 nalbert - Adding from defect #103: 
+//						follow-up insp status = Citation Issued -> create citation insp
+//						initial inspection status = Citation -> create citation insp
 //            
 /*==================================================================*/
 
@@ -23,4 +26,12 @@ if (inspType == "Citation Inspection" && inspResult == "In Violation - Expedite"
 	// The following will assign to a specific person, this was not a requirement in the spec.
 	// scheduleInspection("Citation Inspection", 3, "ADMIN", null, "ADMIN");
 	scheduleInspection("Citation Inspection", 3);
+}
+
+if (inspType == "Follow-Up Inspection" && inspResult == "Citation Issued") {
+	scheduleInspection("Citation Inspection", 7);
+}
+
+if (inspType == "Initial Inspection" && inspResult == "Citation") {
+	scheduleInspection("Citation Inspection", 7);
 }
