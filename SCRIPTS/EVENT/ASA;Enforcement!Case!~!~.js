cd2418998c0caa6b0b8c3a6b41fab3f1c5bf81bc
gurneykevin <kgurney@accela.com>
2016-12-01 07:20:53 -0800

    added duplicate case check to main part.

--- SCRIPTS/EVENT/ASA;Enforcement!Case!~!~.js
+++ SCRIPTS/EVENT/ASA;Enforcement!Case!~!~.js
@@ -1,4 +1,23 @@
-var matchDupENFCase=false;
+// Duplicate Case Check added in main event to exclude other scripts if record is closed.
+
+	var capIds = capIdsGetByAddr();
+	var matchesENFCase=0;
+	if (capIds && capIds.length > 1){
+		logDebug(capIds.length);
+		for (i in capIds){
+			if (capId.toString() == capIds[i].toString()) continue;
+			if (appMatch(appTypeString, capIds[i])){
+				var table = loadASITable("VIOLATION INFORMATION", capIds[i]);
+				for (row in table){
+					if (table[row]["Status"] == "Open"){
+						logDebug("Violation " + table[row]["Violation Description"] + " is open");
+						matchesENFCase++;
+					}
+				}
+			}
+		}
+	}
+	
 include("ENF_EnforcementCaseDuplicate");
 // add by Brian O'Dell (Mesa)
 include("ENF_EnforcementNotificationEmail");
@@ -7,8 +26,9 @@ include("ENF_AutopopulateZoningDistrict");
 include("ENF_CountyIslandEmail");
 
 // added by Vance Smith (Mesa)
-logDebug("matchDupENFCase = " + matchDupENFCase);
-include("ENF_NewRecordPriorityNormal"); // 26
-
+// adding if duplicate case found then no need to run this script which schedules inspection since case will be closed.
+if (matchesENFCase = 0) { 
+	include("ENF_NewRecordPriorityNormal"); // 26
+}
 //added by Michael Kniskern (Mesa)
 include("ENF_AddViolationOrdinanceOnViolationCode");
