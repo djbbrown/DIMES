527cc0814cbb2a64ea65cebf5d87153cde062f83
gurneykevin <kgurney@accela.com>
2016-12-01 08:28:52 -0800

    Alarm Permit Fees

---
+++ SCRIPTS/MESA EVENT SCRIPTS/ALR_PermitFees.js
@@ -0,0 +1,20 @@
+/*===================================================================
+// Script Number: TBD
+// Script Name: ALR_PermitFees.js
+// Script Developer: Kevin Gurney
+// Script Agency: Accela
+// Script Description: When an Alarm Permit is submitted apply the appropriate Permit Fees 
+//
+// Script Run Event: ASA
+// Script Parents:
+//             ASA;Permits!Police Department!Alarms!Commercial
+==================================================================*/
+if(publicUser){
+	removeAllFees(capId);
+}
+if (AInfo["Burglary Function"] == "Yes"){
+	addFee("PD_ALARM_01","PMT_PD_ALARM_010","FINAL",1,"N");
+}
+if (AInfo["Panic/Robbery/Hold-Up Function"] == "Yes"){
+	addFee("PD_ALARM_02","PMT_PD_ALARM_010","FINAL",1,"N");
+}
