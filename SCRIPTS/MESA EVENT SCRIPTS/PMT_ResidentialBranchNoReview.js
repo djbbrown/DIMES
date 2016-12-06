46757f4f097464bf6d82e1a868892d32a15a5f11
gurneykevin <kgurney@accela.com>
2016-12-06 09:09:25 -0800

    Added script for additional branch

---
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_ResidentialBranchNoReview.js
@@ -0,0 +1,6 @@
+/*===================================================================
+// Script Number: TBD
+// Script Name: Branch Task
+==================================================================*/
+setTask("Application Submittal","N","Y");
+activateTask("Plans Coordination");
