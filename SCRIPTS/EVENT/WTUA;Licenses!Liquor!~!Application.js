c8c6c8ff6aceb6afdba7ce35f5bf5eb53afb69a9
Kevin Ford <kford@accela.com>
2016-12-05 17:35:08 -0800

    Rolling back 161 fix
    There doesn't seem to be a script defined for this.

--- SCRIPTS/EVENT/WTUA;Licenses!Liquor!~!Application.js
+++ SCRIPTS/EVENT/WTUA;Licenses!Liquor!~!Application.js
@@ -3,4 +3,4 @@
 ==================================================================*/
 include("LIC_AppealDeadlineDateSet");
 include("LIC_AdministrativeReviewDueDateSet_2"); // Added by Raminder Gill=======
-include("LIC_AdjustAdministrativeReviewDueDate"); // Fix for 161
+// include("LIC_AdjustAdministrativeReviewDueDate"); // Fix for 161
