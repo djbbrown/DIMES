5de27172cdd9561c9a7ccd9e0cde32945c40621c
Jody Bearden <jody.bearden@mesaaz.gov>
2016-12-08 17:50:58 -0700

    Updated per newest specs.

--- SCRIPTS/MESA EVENT SCRIPTS/PLN_Copy_WF_ASIT.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PLN_Copy_WF_ASIT.js
@@ -4,71 +4,31 @@
 // Script Description: Whenever the Workflow Task Hearing is updated to a status that is NOT Note, or Hearing Scheduled, or SP Hearing Complete; 
 // Then create a new row in the ASIT DECISIONS AND RECOMMENDATIONS
 // Script Run Event: WTUA
-// Script Parents:WTUA;Planning/General Plan Amendment - Major/!~/!~.js    
-// WTUA;Planning/Planning and Zoning/!~/!~.js
-// WTUA;Planning/Subdivision/!~/!~.js
-// Test Record: ZON16-00249
+// Script Parents:
+//		WTUA;Planning/Subdivision/NA/NA
+//		WTUA;Planning/General Plan Amendment – Major/NA/NA
+//		WTUA;Planning/Planning and Zoning/NA/NA
+//		WTUA;Planning/Design Review/NA/NA
+//		WTUA;Planning/Board of Adjustment/NA/NA
+//		WTUA;Planning/Admin Review/NA/NA
+//
 // Version   |Date      |Engineer         |Details
 //  1.0      |09/21/16  |Steve Veloudos   |Initial Release
+//  1.1      |12/08/1   |Jody Bearden     |Redid to current specs - used environment variables, removed Java API calls
 ==================================================================*/
+/* Test Record: ZON16-00249 */
 
 try {
-    var NotNoteFlag = 0;
-    var NotHearingFlag = 0;
-    var NotSpHearingFlag = 0;
-    var StatusDate;
-    var Status;
-    var HearingValue = "None";
-
-    //Set workflow name based on the type of record
-    if(appTypeString == "Planning/Planning and Zoning/NA/NA" ||appTypeString ==  "Planning/General Plan Amendment - Major/NA/NA")
-    {
-        HearingValue = "Hearing(s)";
-    }
-    if(appTypeString == "Planning/Subdivision/NA/NA")
-    {
-        HearingValue = "Hearing";
-    }
-
-    //Get Hearing Workflow task
-    var tasks = aa.workflow.getTasks(capId).getOutput();
-    for (t in tasks) 
-    {
-        //Check task name and status & set flags
-        if (tasks[t].getTaskDescription() == HearingValue)
-        { 
-            if(tasks[t].getDisposition() != "Note")
-            {
-               NotNoteFlag = 1; 
-            }
-            if(tasks[t].getDisposition() != "Hearing Scheduled")
-            {
-               NotHearingFlag = 1; 
-            }
-            if(tasks[t].getDisposition() != "SP Hearing Complete")
-            {
-               NotSpHearingFlag = 1; 
-            }
-            //Get workflow info
-            Status = tasks[t].getDisposition();
-            StatusDate = "" + (tasks[t].getStatusDate().getMonth() + 1) + "/" + tasks[t].getStatusDate().getDate() + "/" + (parseInt(tasks[t].getStatusDate().getYear()) + 1900);
-            
-            break;          
-        }
-    }               
-    
-    //Check flags & create new ASIT row if true
-    if(NotNoteFlag == 1 || NotHearingFlag == 1 || NotSpHearingFlag == 1)
-        {
-            newDescASIT = [];
-            newRow = [];
-			newRow["Date"] =  new asiTableValObj("Date", StatusDate,"N");
-			newRow["Decision"] =  new asiTableValObj("Decision",Status,"N") ;
-			newDescASIT.push(newRow);
-            addASITable("DECISIONS AND RECOMMENDATIONS",newDescASIT);
-        }
-    }
-catch (err)
-    {
-      logDebug("A JavaScript Error occured: " + err.message);
-    }
+	if (matches(wfTask, "Hearing(s)", "Hearing", "Hearings")) {
+		if (!matches(wfStatus, "Note", "Hearing Scheduled", "Hearing Complete") {
+			var asitRow = new Array();
+			asitRow["Date"] = wfDateMMDDYYYY;
+			asitRow["Decision"] = wfStatus;
+			asitRow["Review/Decision By"] = AInfo["Hearing Body"];
+			addToASITable("DECISIONS AND RECOMMENDATIONS", asitRow, capId);
+		}
+	}
+}
+catch (err){
+	logDebug("A JavaScript Error occured: " + err.message);
+}
