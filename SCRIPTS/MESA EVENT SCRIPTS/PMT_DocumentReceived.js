c4acdba765e753f852d4c9e85f5b32eb95ef8a90
gurneykevin <kgurney@accela.com>
2016-12-09 16:05:32 -0800

    Document update workflow

---
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_DocumentReceived.js
@@ -0,0 +1,22 @@
+//Start Script Permits DUA
+
+var wfTaskStatusCheck = false;
+if (capStatus == "Revisions Required" && isTaskActive("Plans Distribution")) {
+	var wfTaskStatusCheck = true;
+}
+//logDebug("wfTaskStatusCheck = " + wfTaskStatusCheck);
+
+if ((documentModelArray.size() > 0) && (wfTaskStatusCheck)) 	{
+	for(var index = 0; index < documentModelArray.size(); index++){
+		
+		// Update workflow task for document to be reviewed
+
+		if (documentModelArray.get(index).getDocCategory()=="Construction Plans") 
+		{
+			updateTask("Plans Distribution", "Revisions Received", "Updated by DUA event", "Updated by DUA event");
+			//potentially send email
+		}	
+  		
+	}
+}
+//End Script Permits DUA
