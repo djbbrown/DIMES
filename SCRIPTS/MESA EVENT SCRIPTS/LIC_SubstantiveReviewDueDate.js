efca445c824cf580ea2a395391b6013a9ea31aff
nical09 <nalbert@accela.com>
2016-12-19 16:10:29 -0500

    Added an 's' to Fireworks

--- SCRIPTS/MESA EVENT SCRIPTS/LIC_SubstantiveReviewDueDate.js
+++ SCRIPTS/MESA EVENT SCRIPTS/LIC_SubstantiveReviewDueDate.js
@@ -39,7 +39,7 @@ if(
 	if(appTypeArray[2]=="SpecialEvent"
 		|| appTypeArray[2]=="ExtensionOfPremise-Temporary"
 		|| appTypeArray[2]=="ExtensionOfPremise-Permanent"
-		|| appTypeArray[2]=="Firework"
+		|| appTypeArray[2]=="Fireworks"
 		) {
 		// Special Event + 30 needs to be set
 		editAppSpecific("Substantive Review Due", dateAdd(null,30));
