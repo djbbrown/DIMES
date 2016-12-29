cc0835b460611253f327d5daca1f0b2cef13e1a4
nical09 <nalbert@accela.com>
2016-12-29 14:25:31 -0500

    Debugging for ACA

--- SCRIPTS/EVENT/PRA;Licenses!General!~!Application.js
+++ SCRIPTS/EVENT/PRA;Licenses!General!~!Application.js
@@ -1,4 +1,12 @@
 /*===================================================================
 // Script Name: PRA;Licenses!General!~!Application.js
 ==================================================================*/
+
+// sending debug email to see if firing from ACA
+	var message = "Writing from PRA:Licenses!General!~!Application";
+	var vEParams = aa.util.newHashtable(); 
+	addParameter(vEParams,"$$capId$$",capId);
+	addParameter(vEParams, "$$error$$", message);
+	sendNotification("", "nalbert@accela.com", "", "ERROR TRAPPING", vEParams, null);
+	
 include("LIC_IssueLicenseOnFeePaymentACA"); // Added by Kevin Ford
