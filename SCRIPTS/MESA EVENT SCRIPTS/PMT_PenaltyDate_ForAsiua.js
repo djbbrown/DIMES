548b668f5bc0ae4176bbde0e2be54d259686b48d
MesaDimes <brian.odell@mesaaz.gov>
2017-01-03 07:12:54 -0700

    user mods 20170103a

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_PenaltyDate_ForAsiua.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_PenaltyDate_ForAsiua.js
@@ -49,12 +49,12 @@ try
     // assign to Penalty Date ASI field if exists
     if (typeof penaltyDate == "undefined")
     {
-      //comment("The ASI field 'Penalty Date' does not exist, skipping date assignment");
+      comment("The ASI field 'Penalty Date' does not exist, skipping date assignment");
     }
     else
     {
-      //comment("The ASI field 'Penalty Date' exists, setting date");     
-      //comment("futureDate: " + futureDate );
+      comment("The ASI field 'Penalty Date' exists, setting date");     
+      comment("futureDate: " + futureDate );
       editAppSpecific("Penalty Date", jsDateToASIDate(futureDate));
       setDate = true;
     }
@@ -62,12 +62,12 @@ try
     // assign to Plan Review Penalty Date ASI field if exists
     if (typeof planReviewPenaltyDate == "undefined")
     {
-      //comment("The ASI field 'Plan Review Penalty Date' does not exist, skipping date assignment");
+      comment("The ASI field 'Plan Review Penalty Date' does not exist, skipping date assignment");
     }
     else
     {
-      //comment("The ASI field 'Plan Review Penalty Date' exists, setting date");      
-      //comment("futureDate: " + futureDate );
+      comment("The ASI field 'Plan Review Penalty Date' exists, setting date");      
+      comment("futureDate: " + futureDate );
       editAppSpecific("Plan Review Penalty Date", jsDateToASIDate(futureDate));
       setDate = true;
     }
