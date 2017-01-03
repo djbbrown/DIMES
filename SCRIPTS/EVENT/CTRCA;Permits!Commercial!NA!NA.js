f2f3ece0822685f26b4bf43315c2ce958daa81dc
gurneykevin <kgurney@accela.com>
2017-01-03 10:00:54 -0800

    adding CTRCA event for ACA.

---
+++ SCRIPTS/EVENT/CTRCA;Permits!Commercial!NA!NA.js
@@ -0,0 +1,7 @@
+/*===================================================================
+// Script Number: TBD
+// Script Desc: auto populate GIS information
+==================================================================*/
+if(publicUser){
+	include("PMT_AutopopulateASIFieldsFromGISAttributes");
+}
