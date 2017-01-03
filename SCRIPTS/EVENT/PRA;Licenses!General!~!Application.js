6e7b8ea68105c64bc32a9e1fe903adfe338ee8af
nical09 <nalbert@accela.com>
2017-01-03 12:47:50 -0500

    Adding debugging.

--- SCRIPTS/EVENT/PRA;Licenses!General!~!Application.js
+++ SCRIPTS/EVENT/PRA;Licenses!General!~!Application.js
@@ -1,4 +1,5 @@
 /*===================================================================
 // Script Name: PRA;Licenses!General!~!Application.js
 ==================================================================*/
+	logDebug("in PRA:LICENSE/GENERAL/NA/APPLICATION");
 	include("LIC_IssueLicenseOnFeePaymentACA"); // Added by Kevin Ford
