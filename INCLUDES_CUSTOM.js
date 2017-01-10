5ede89732a52b1034e89a25ed027ede5fb4142c1
Vance Smith <VSmith@mesaaz.gov>
2017-01-10 14:27:02 -0700

    addeded getEnforcementInspectorObject function

--- INCLUDES_CUSTOM.js
+++ INCLUDES_CUSTOM.js
@@ -2141,6 +2141,50 @@ function getBuildingInspectorObject() // optional altId
 	}
 }
 
+function getEnforcementInspectorObject() // optional altId
+{
+    // code officer aka inspector
+
+    if ( arguments.length == 1 )
+    {
+        capId = aa.cap.getCapID(arguments[0]).getOutput(); // this is expected in getGisInfo
+    }
+
+    var inspector = getGISInfo("Accela/AccelaBoundaries", "Code_Officer_Boundary", "CODE_OFFICER");
+    if (inspector) 
+    {
+        logDebug("Inspector: " + inspector);
+        var nameArray = inspector.split(" ");
+        var inspRes = null;
+        switch (nameArray.length)
+        {
+            case 1:
+                inspRes = aa.person.getUser(inspector);
+                break;
+            case 2:
+                inspRes = aa.person.getUser(nameArray[0], "", nameArray[1]);
+                break;
+            case 3:
+                inspRes = aa.person.getUser(nameArray[0], nameArray[1], nameArray[2]);
+                break;
+        }
+        
+		if (inspRes.getSuccess())
+        {
+			return inspRes.getOutput();
+		}
+        else
+        {
+            logDebug("Failed to create enforcement inspector object!");
+            return false;
+        }
+    }
+	else 
+	{
+		return false;
+	}
+}
+
 function getDocs()
 {
     // works after a record has been submitted.
