9576b2c114bf90d04896a5888e23b956c809c44d
mlucward <mong.ward@mesaaz.gov>
2017-02-01 11:00:43 -0700

    Initial commit for Script 379

--- SCRIPTS/EVENT/WTUA;Transportation!~!~!~.js
+++ SCRIPTS/EVENT/WTUA;Transportation!~!~!~.js
@@ -1,5 +1,6 @@
 //include("TRA_TTC_Approved");  
 include("TRA_TTC_Incomplete_Submittal");
 include("TRA_TTC_Revisions_Required");
+include("TRA_TTC_Submittal_Cycle");
 include("TRA_TTC_Signalized_Intersection");
 include("TRA_TTC_Bus_Stop");
