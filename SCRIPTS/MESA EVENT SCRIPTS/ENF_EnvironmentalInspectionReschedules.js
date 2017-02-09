14b0512086d0e38aabd2dda727f91cc3455fd289
Vance Smith <VSmith@mesaaz.gov>
2017-02-09 08:34:35 -0700

    added addDays function to 354

--- SCRIPTS/MESA EVENT SCRIPTS/ENF_EnvironmentalInspectionReschedules.js
+++ SCRIPTS/MESA EVENT SCRIPTS/ENF_EnvironmentalInspectionReschedules.js
@@ -275,3 +275,10 @@ catch (err)
 /* Test Record: ENVC16-00064
 
 */
+
+function addDays(date, days) 
+{
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+}
