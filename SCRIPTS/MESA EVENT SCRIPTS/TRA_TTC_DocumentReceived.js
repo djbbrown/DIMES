8b05257381636aedb0dda67da62d88030e3fece9
mlucward <mong.ward@mesaaz.gov>
2017-02-09 08:16:42 -0700

    Update

--- SCRIPTS/MESA EVENT SCRIPTS/TRA_TTC_DocumentReceived.js
+++ SCRIPTS/MESA EVENT SCRIPTS/TRA_TTC_DocumentReceived.js
@@ -18,21 +18,28 @@
 //Start Script 
  try 
  { 
-
 	var wfTaskStatusCheck = false;
-	if (capStatus == "Revisions Required" && isTaskActive("Plans Distribution")) {
+		if (capStatus == "Revisions Required" && isTaskActive("Plans Distribution")) {
 	var wfTaskStatusCheck = true;
-	}
-	//logDebug("wfTaskStatusCheck = " + wfTaskStatusCheck);
+		}
+			//logDebug("wfTaskStatusCheck = " + wfTaskStatusCheck);
 
-	if ((documentModelArray.size() > 0) && (wfTaskStatusCheck)) {
-	
-		updateTask("Plans Distribution", "Revisions Received", "Updated by DUA event", "Updated by DUA event");
-		//Send email with Mesa Event Script TRA_Revisions_Required_New_Doc.js
-	  		
+			if ((documentModelArray.size() > 0) && (wfTaskStatusCheck)) 	{
+						for(var index = 0; index < documentModelArray.size(); index++){
+		
+						// Update workflow task for document to be reviewed
+
+			if (documentModelArray.get(index).getDocCategory()=="Construction Documents") 
+		{
+			updateTask("Plans Distribution", "Revisions Received", "Updated by DUA event", "Updated by DUA event");
+			//potentially send email
+		}	
+  		
 	}
- }
+}
  
+ 
+ }
  catch (err) 
  { 
    logDebug("A JavaScript error occurred: " + err.message); 
