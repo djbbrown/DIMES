a8569d6891f5c0bcd07cfe24eda95588f6f3c9e9
sallred <steve.allred@mesaaz.gov>
2017-02-21 14:32:44 -0700

    Added comments for display

--- SCRIPTS/MESA EVENT SCRIPTS/ENG_UTL_Fees_Inspection.js
+++ SCRIPTS/MESA EVENT SCRIPTS/ENG_UTL_Fees_Inspection.js
@@ -14,16 +14,19 @@ try {
 	if (inspType == "After Hours Inspection – Immediately After Normal Work Hours") {
 		if (feeExists("UTL060", "NEW", "INVOICED")){voidRemoveFee("UTL060");}
 			addFee("UTL060", "ENG_NON-CITY UTILITES","FINAL", 1, "N");
+			comment("Attempting to add fee UTL060");
 	}
 
 	if (inspType == "After Hours Inspection – Returning to Site") {
 		if (feeExists("UTL070", "NEW", "INVOICED")){voidRemoveFee("UTL070");}
 			addFee("UTL070", "ENG_NON-CITY UTILITES","FINAL", 1, "N");
+			comment("Attempting to add fee UTL070");
 	}
 
 	if (inspType == "After Hours Inspection – Weekends and Holidays") {
 		if (feeExists("UTL080", "NEW", "INVOICED")){voidRemoveFee("UTL080");}
 			addFee("UTL080", "ENG_NON-CITY UTILITES","FINAL", 1, "N");
+			comment("Attempting to add fee UTL080");
 	}	
 }	
 	
