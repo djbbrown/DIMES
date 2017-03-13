c66fed3b0fc15971574b19ea523991af8be681d2
sallred <steve.allred@mesaaz.gov>
2017-03-13 10:16:29 -0700

    Removed

--- SCRIPTS/EVENT/WTUA;Engineering!~!~!~.js
+++ SCRIPTS/EVENT/WTUA;Engineering!~!~!~.js
@@ -1,5 +1,3 @@
 // WTUA;Engineering!~!~!~
 include("ENG_SetPermitIssuedExpirationDate"); // Script 366
-include("ENG_UTL_Fees_Workflow");
-
 //include("ENG_UTL_Fees_Workflow");  Disabled per Steve DiDomenico 3/9/17
