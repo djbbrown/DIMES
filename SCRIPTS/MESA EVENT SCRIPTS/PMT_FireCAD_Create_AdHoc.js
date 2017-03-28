06a2e0e54a013f11cf74ba98f026a4cf43aaf988
sallred <steve.allred@mesaaz.gov>
2017-03-28 10:43:51 -0700

    date

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_FireCAD_Create_AdHoc.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_FireCAD_Create_AdHoc.js
@@ -22,7 +22,7 @@ if (documentModelArray.size() > 0) 	{
 			var adt = mesaWorkingDays(aa.util.now(), 1);  //Assigned Date: today 
 			var ddt = mesaWorkingDays(aa.util.now(), 4);  //Due Date: today + 3 days based on 4-day work-week
 			var xdt = new Date();
-			var zdt = convertDate2(xdt);
+			var zdt = convertDate(xdt);
 			var taskObj = aa.workflow.getTasks(thisCap).getOutput()[0].getTaskItem();
 			
 			logDebug("thisCap:" + thisCap);
@@ -47,45 +47,5 @@ if (documentModelArray.size() > 0) 	{
   		
 	}
 }
-//functions
-function convertDate2(thisDate)
-{
-	if (typeof(thisDate) == "string")
-		{
-		var retVal = new Date(String(thisDate));
-		if (!retVal.toString().equals("Invalid Date"))
-			return retVal;
-		}
-	if (typeof(thisDate)== "object")
-		{
-		if (!thisDate.getClass) // object without getClass, assume that this is a javascript date already
-			{
-			return thisDate;
-			}
-		if (thisDate.getClass().toString().equals("class com.accela.aa.emse.dom.ScriptDateTime"))
-			{
-			return new Date(thisDate.getMonth() + "/" + thisDate.getDayOfMonth() + "/" + thisDate.getYear());
-			}
-		if (thisDate.getClass().toString().equals("class com.accela.aa.emse.util.ScriptDateTime"))
-			{
-			return new Date(thisDate.getMonth() + "/" + thisDate.getDayOfMonth() + "/" + thisDate.getYear());
-			}			
-		if (thisDate.getClass().toString().equals("class java.util.Date")
-			|| thisDate.getClass().toString().equals("class java.sql.Timestamp")
-		)
-			{
-			return new Date(thisDate.getTime());
-			}
-		if (thisDate.getClass().toString().equals("class java.lang.String"))
-			{
-			return new Date(String(thisDate));
-			}
-		}
-	if (typeof(thisDate) == "number")
-		{
-		return new Date(thisDate);  // assume milliseconds
-		}
-	logDebug("**WARNING** convertDate2 cannot parse date : " + thisDate);
-	return null;
-}
+
 //End Script 
