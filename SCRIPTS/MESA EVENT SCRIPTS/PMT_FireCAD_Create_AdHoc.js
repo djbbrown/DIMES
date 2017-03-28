b50e80a8b5e696533aea3c5c2875a128cada72ef
sallred <steve.allred@mesaaz.gov>
2017-03-28 10:25:02 -0700

    dates

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_FireCAD_Create_AdHoc.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_FireCAD_Create_AdHoc.js
@@ -21,12 +21,16 @@ if (documentModelArray.size() > 0) 	{
 			var thisUser = "RPEREZ3";
 			var adt = mesaWorkingDays(aa.util.now(), 1);  //Assigned Date: today 
 			var ddt = mesaWorkingDays(aa.util.now(), 4);  //Due Date: today + 3 days based on 4-day work-week
+			var xdt = new Date();
+			var zdt = convertDate2(xdt);
 			var taskObj = aa.workflow.getTasks(thisCap).getOutput()[0].getTaskItem();
 			
 			logDebug("thisCap:" + thisCap);
 			logDebug("thisUser:" + thisUser);
 			logDebug("adt:" + adt);
 			logDebug("ddt:" + ddt);
+			logDebug("xdt:" + xdt);
+			logDebug("zdt:" + zdt);
 			
 			taskObj.setProcessCode("WFADHOC_PROCESS");
 			taskObj.setTaskDescription("Fire CAD Review");
