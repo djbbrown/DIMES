4b87a40ece2fa20f1da51d24b21dcc627437f6b3
sallred <steve.allred@mesaaz.gov>
2017-03-28 10:57:23 -0700

    date

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_FireCAD_Create_AdHoc.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_FireCAD_Create_AdHoc.js
@@ -19,10 +19,10 @@ if (documentModelArray.size() > 0) 	{
 			//Create Ad Hoc Task
 			var thisCap = capId;
 			var thisUser = "RPEREZ3";
-			var adt = mesaWorkingDays(aa.util.now(), 4) - 3;  //Assigned Date: today 
+			var adt = mesaWorkingDays(aa.util.now(), 1);  //Assigned Date: today 
 			var ddt = mesaWorkingDays(aa.util.now(), 4);  //Due Date: today + 3 days based on 4-day work-week
 			var xdt = new Date();
-			var zdt = convertDate(xdt);
+			var zdt = convertDate(new Date());
 			var taskObj = aa.workflow.getTasks(thisCap).getOutput()[0].getTaskItem();
 			
 			logDebug("thisCap:" + thisCap);
