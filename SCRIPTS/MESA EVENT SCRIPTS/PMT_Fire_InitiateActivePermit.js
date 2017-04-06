c5978f57d9b6030c5ffb8f43145267f5ba2a39aa
Michael VanWie <MVanWie@mesaaz.gov>
2017-04-06 12:25:15 -0700

    Update Fire Script 406

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_Fire_InitiateActivePermit.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_Fire_InitiateActivePermit.js
@@ -1,26 +1,20 @@
 /*===================================================================
- Script Number: 406
- Script Name: PMT_Fire_InitiateActivePermit.js
- Script Developer: Michael VanWie
- Script Agency: Mesa
- Script Description:
-   
-When Workflow Task "Permit Issuance" is updated to a status of "Issued"; 
-- create a Permits/Fire/FSOP/Permit.  
-- Copy all ASI data from the Application record type to the new Permit record (like for like). 
-- Set new record's "Expiration Date" 365 days in the future.
-- set new record's workflow task "FSOP Status" to active
-This creates a new Permit that is a child of the original Application.
-
-Test records:   
-
-Script run events: WTUA
-Script Parents:  WTUA;Permits!Fire!FSOP!Application
+// Script Number: 406
+// Script Name: PMT_Fire_InitiateActivePermit.js
+// Script Developer: Michael VanWie
+// Script Agency: Mesa
+// Script Description: When Workflow Task "Permit Issuance" is updated to a status of "Issued"; 
+//						- create a Permits/Fire/FSOP/Permit.  
+//						- Copy all ASI data from the Application record type to the new Permit record (like for like). 
+//						- Set new record's "Expiration Date" 365 days in the future.
+//						- set new record's workflow task "FSOP Status" to active
+//						This creates a new Permit that is a child of the original Application.
+// Script run events: WTUA
+// Script Parents:  WTUA;Permits!Fire!FSOP!Application
 /*==================================================================*/
-
 try {
-    if (wfTask.equals("Permit Issuance") && wfStatus.equals("Issued")){
-
+	if (wfTask.equals("Permit Issuance") && wfStatus.equals("Issued"))
+	{	
         // create child record that is a Permit, then copy data over
         // createChild() copies the following data from the current record to the new child record: parcels, contacts, property addresses
         var newId = createChild("Permits","Fire", "FSOP", "Permit", capName);
@@ -28,25 +22,24 @@ try {
         // copy asi fields (which also copies their values in contradiction to documentation .. go figure..)
         copyASIFields(capId,newId); 
 
-        // set expiration date of new record to 365 days in future
-        var expireDate = new Date(dateAdd(null,365));
-        editAppSpecific("Expiration Date", jsDateToASIDate(expireDate),newId);
-
+        // set expiration date of new record to 365 days in future and status to active
+        var exp = aa.expiration.getLicensesByCapID(newId).getOutput();
+        exp.setExpDate(aa.date.parseDate(dateAddMonths(null,12)));
+        exp.setExpStatus('Active');
+        
         // set task "FSOP Status" status = active
-        activateTaskInRecord("FSOP Status", newId);
-
+        activateTaskInRecord('FSOP Status', newId);
+        aa.workflow.adjustTask(newId, 'FSOP Status', 'Active', 'ADMIN');
+        
         // get the id that humans use
         var newCustomId = newId.getCustomID();
-        logDebug("PMT_Fire_InitiateActiveRegistration - Created Permit with ID = " + newCustomId);
-
-    } else {
-		logDebug("PMT_Fire_InitiateActiveRegistration - No Action - current task and status out of scope.");
+        logDebug("PMT_Fire_InitiateActivePermit - Created Permit with ID = " + newCustomId);
+    } 
+	else 
+	{
+		logDebug("PMT_Fire_InitiateActivePermit - No Action - current task and status out of scope.");
 	}
-} catch (err) {
-	logDebug("A JavaScript Error occured in PMT_Fire_InitiateActiveRegistration: " + err.message);
-	logDebug(err.stack);
-}
-logDebug("---------- end  PMT_Fire_InitiateActiveRegistration ----------");
+logDebug("---------- end  PMT_Fire_InitiateActivePermit ----------");
 
 //////////////////// functions /////////////////////////////////////
 
@@ -54,8 +47,8 @@ function activateTaskInRecord(wfstr, recId)
 {
 	var useProcess = false;
 	var processName = "";
-	if (arguments.length == 2) {
-		processName = arguments[1]; // subprocess
+	if (arguments.length > 2) {
+		processName = arguments[2]; // subprocess
 		useProcess = true;
 	}
 
