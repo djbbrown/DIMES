79a805f5a3b7ca1b5c95b91a2f35e04d12925bb8
Michael VanWie <MVanWie@mesaaz.gov>
2017-05-01 14:20:47 -0700

    Update ENF workflow scripts

--- SCRIPTS/EVENT/IRSA;Enforcement!Environmental!Complaint!~.js
+++ SCRIPTS/EVENT/IRSA;Enforcement!Environmental!Complaint!~.js
@@ -1,5 +1,7 @@
 // Script IRSA;Enforcement!Environmental!Complaint!~.js
 
 // added by Vance Smith (Mesa)
-include("ENF_NoViolationInspectionUpdateWFStatus");
+
+//Script Canceled - Duplicate of 354 - Per Even's Comment on Script Tracker on 03/29/2017
+//include("ENF_NoViolationInspectionUpdateWFStatus"); 
 include("ENF_EnvironmentalInspectionReschedules"); //354
