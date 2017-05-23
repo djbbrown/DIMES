e32b01bce4abe692a5b433e22a498aeaaa63d9c7
sallred <steve.allred@mesaaz.gov>
2017-05-23 07:52:38 -0700

    Exclude Fire Permits

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_preventIssuanceWithBalanceDue.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_preventIssuanceWithBalanceDue.js
@@ -6,15 +6,26 @@
 // Script Description: Prevent the status of "Issued" being set on the workflow task "Permit Issuance" if there is a balance due or assessed fees on the record that have not been invoiced. 
 // Script Run Event: WorkflowTaskUpdateBefore
 // Script Parents:
-//            WTUB;Licenses!~!~!~
+//            WTUB;Permits!~!~!~
+//
+//==================================================================*
+// MODIFICATIONS
+//
+// DATE       ENGINEER        DESCRIPTION
+// 05/23/17   Steve Allred    Added code to exclude Permits/Fire/!/!
+//
 /*==================================================================*/
 
 // Under this line create the function that will need to run at script runtime.
 // the function will be called in the event ("WorkflowTaskUpdateBefore") major event.
 
-if(matches(wfTask,"Permit Issuance","Document Issuance") && matches(wfStatus,"Issued","Complete") && (balanceDue > 0 || feeTotalByStatus("NEW") > 0)){
-	showMessage = true;
-	message = "";
-	comment("All invoiced fees must be paid before selecting this status.");
-	cancel = true;
+// Exclude Fire
+if(!matches(appTypeArray[1], "Fire")) {
+
+	if(matches(wfTask,"Permit Issuance","Document Issuance") && matches(wfStatus,"Issued","Complete") && (balanceDue > 0 || feeTotalByStatus("NEW") > 0)){
+		showMessage = true;
+		message = "";
+		comment("All invoiced fees must be paid before selecting this status.");
+		cancel = true;
+	}
 }
