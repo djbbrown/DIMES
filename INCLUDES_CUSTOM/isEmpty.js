b75173203e2392ec9f379e7c12b4fc6316e1aec6
gurneykevin <kgurney@accela.com>
2017-06-12 16:33:47 -0700

    adding new function

---
+++ INCLUDES_CUSTOM/isEmpty.js
@@ -0,0 +1,7 @@
+function isEmpty(pVariable) {
+                if (pVariable === undefined || pVariable == null || pVariable == "") {
+                                return true;
+                } else {
+                                return false;
+                }
+}
