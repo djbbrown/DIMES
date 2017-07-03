a518e273e5de32502bd034f009268c4084ce5ca7
sallred <steve.allred@mesaaz.gov>
2017-07-03 10:35:25 -0700

    Mod to criteria.

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_ONLFee060.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_ONLFee060.js
@@ -3,18 +3,22 @@
 // Script Name: PMT_ONLFee060
 // Script Developer: Kevin Ford
 // Script Agency: Accela
-// Script Description: Auto Assess at application submittal when Type of Work = Construction Noise Permit,
-	Auto Invoice at submittal
+// Script Description: Auto Assess and auto invoice at application submittal
 // Script Run Event: ASA 
 // Script Parents:
 //            ASA;Permit/Online/NA/NA
-===================================================================*/
-var sameDayGas = (AInfo["Same Day Turn On - Gas"] != undefined) ? AInfo["Same Day Turn On - Gas"] : AInfo["Same Day Turn On - Gas"];
+// =========================================================================================================== 
+// MODIFICATION HISTORY
+// Date          Analyst              Description
+// 07/03/2017    Steve Allred         Added elecService criteria.  Commented out var sameDayGas and var Online.
+==============================================================================================================*/
+//var sameDayGas = (AInfo["Same Day Turn On - Gas"] != undefined) ? AInfo["Same Day Turn On - Gas"] : AInfo["Same Day Turn On - Gas"];
 var sameDayElec = (AInfo["Same Day Turn On - Electric"] != undefined) ? AInfo["Same Day Turn On - Electric"] : AInfo["Same Day Turn On - Electric"];
+var elecService = (AInfo["Electric Service"] != undefined) ? AInfo["Electric Service"] : AInfo["Electric Service"];
 
-var Online = ["Construction Noise Permit"];
+//var Online = ["Construction Noise Permit"];
 //if(appTypeArray[1]=='Online' && (sameDayGas == 'CHECKED' || sameDayElec == 'CHECKED'))
-if(appTypeArray[1]=='Online' && (sameDayElec == 'CHECKED'))
+if(appTypeArray[1]=='Online' && (sameDayElec == 'CHECKED') && (elecService == 'City of Mesa Electric')
 {
 	addFee("ONL060","PMT_ONL", "FINAL",  1, "Y");
 }
