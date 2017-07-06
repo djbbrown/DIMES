cdbaa6203a0d265563f182088c2611d820d24094
gurneykevin <kgurney@accela.com>
2017-07-06 16:26:02 -0700

    remarked out duplicate case check per Mesa feedback

--- SCRIPTS/EVENT/ASA;Enforcement!Case!~!~.js
+++ SCRIPTS/EVENT/ASA;Enforcement!Case!~!~.js
@@ -1,24 +1,5 @@
-// Duplicate Case Check added in main event to exclude other scripts if record is closed.
-
-	var capIds = capIdsGetByAddr();
-	var matchesENFCase=0;
-	if (capIds && capIds.length > 1){
-		logDebug(capIds.length);
-		for (i in capIds){
-			if (capId.toString() == capIds[i].toString()) continue;
-			if (appMatch(appTypeString, capIds[i])){
-				var table = loadASITable("VIOLATION INFORMATION", capIds[i]);
-				for (row in table){
-					if (table[row]["Status"] == "Open"){
-						logDebug("Violation " + table[row]["Violation Description"] + " is open");
-						matchesENFCase++;
-					}
-				}
-			}
-		}
-	}
 	
-include("ENF_EnforcementCaseDuplicate");
+//include("ENF_EnforcementCaseDuplicate");  remarked out since it has been determined that duplicates are wanted
 // add by Brian O'Dell (Mesa)
 //include("ENF_EnforcementNotificationEmail");  Looks to have been replaced with ENF_Record_Opened
 
@@ -31,9 +12,7 @@ if (!publicUser){
 
 
 // added by Vance Smith (Mesa)
-// adding if duplicate case found then no need to run this script which schedules inspection since case will be closed.
-if (matchesENFCase == 0) { 
-	include("ENF_NewRecordPriorityNormal"); // 26
-}
+include("ENF_NewRecordPriorityNormal"); // 26
+
 //added by Michael Kniskern (Mesa)
 include("ENF_AddViolationOrdinanceOnViolationCode");
