7f7e2a018fd5b927926cef5c23e12fafa56fb7d2
sallred <steve.allred@mesaaz.gov>
2017-07-24 08:51:12 -0700

    Disabled PLN_Daycare_Handicap_Buffer

--- SCRIPTS/EVENT/ASB;Planning!Group Home!Application!NA.js
+++ SCRIPTS/EVENT/ASB;Planning!Group Home!Application!NA.js
@@ -2,6 +2,6 @@
 //
 // Script Name: ASB: Planning/Group Home/Application/NA.js
 /*==================================================================*/
-include("PLN_Daycare_Handicap_Buffer");
+//include("PLN_Daycare_Handicap_Buffer");  Disabled 7/24/17 by Steve Allred
 include("PLN_GroupHomeNumberOfClients");
 //include("PLN_Lease_Agreement_Application2");
