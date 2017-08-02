146154b209ff374828c8e198d77a3efd9dd4bd06
mlucward <mong.ward@mesaaz.gov>
2017-08-02 09:15:42 -0700

    update to syntax

--- SCRIPTS/MESA EVENT SCRIPTS/TRA_TTC_PermitIssuedEmail.js
+++ SCRIPTS/MESA EVENT SCRIPTS/TRA_TTC_PermitIssuedEmail.js
@@ -88,7 +88,7 @@ try {
 
    
 		//Get record detailed desc
-			DetailedDesc = AInfo["AInfo["Permit Expiration Date"];
+			DetailedDesc = AInfo["Permit Expiration Date"];
 				                           
         
 		
