acde25e93e888415e8b85fb5b862adbe3a2a0fab
sallred <steve.allred@mesaaz.gov>
2017-08-04 05:52:09 -0700

    Temporary deactivation of PLN_SchedulePreApplication

--- SCRIPTS/EVENT/CTRCA;Planning!Pre-Application!NA!NA.js
+++ SCRIPTS/EVENT/CTRCA;Planning!Pre-Application!NA!NA.js
@@ -3,6 +3,7 @@
 // Script Desc: To schedule Pre-Application Meeting
 ==================================================================*/
 //adding publicUser criteria check to execute scripts based on AA or ACA
-if(publicUser){
-	include("PLN_SchedulePreApplication");
-}
+//temporary deactivation of script - Steve Allred 08/04/17
+//if(publicUser){
+//	include("PLN_SchedulePreApplication");
+//}
