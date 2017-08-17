ddbfefa40686360f4276b13f6458899c92307ad2
sallred <steve.allred@mesaaz.gov>
2017-08-17 14:08:11 -0700

    Mods per TE.

--- SCRIPTS/MESA EVENT SCRIPTS/PLN_Applicant_Status_Change.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PLN_Applicant_Status_Change.js
@@ -7,21 +7,22 @@
 //  1.0      |08/31/16  |Steve Veloudos   |Initial Release  
 //  1.1      |09/15/16  |Steve Veloudos   |Adjusted Phone Incomplete Status  
 //  1.2      |12/06/16  |Steve Veloudos   |Added the workflow comments when app status is Incomplete 
+//  1.3      |07/19/17  |Steve Allred     |Disabled email to Applicant on AppStatus = "Distributed"
 /*==================================================================*/
 
 try {
-      var FromEmail = "noreply@mesaaz.gov";
-      var ToEmail;
-      var vEParams = aa.util.newHashtable();
-      var AppStatus;
-      var StatusType = 0;
-      var ConType;
-      var AppToEmail;
-      var PlanningPhone;
-      var tStatus = "Incomplete";
+	var FromEmail = "noreply@mesaaz.gov";
+    var ToEmail;
+    var vEParams = aa.util.newHashtable();
+    var AppStatus;
+    var StatusType = 0;
+    var ConType;
+    var AppToEmail;
+    var PlanningPhone;
+    var tStatus = "Incomplete";
  
-    //Get application status
-    AppStatus = capStatus;
+	//Get application status
+	AppStatus = capStatus;
     
     //Set status type
     if (AppStatus == "Withdrawn")
@@ -51,68 +52,70 @@ try {
         {
             ConType = tInfo[x]["contactType"];
             if(ConType == "Applicant" )
-            {
+				{
             var AppToEmail = tInfo[x]["email"];
-            }
+				}
         } 
 
-    //Send Email if correct doc category
+    //Send Email if correct record status
     if(StatusType != 0)
-      {
-        //Withdrawn
-        if(StatusType == 1)
-        {
-            //Email Staff
-            ToEmail = lookup("EMAIL_RECIPIENTS","PLN_Planning_Staff_Application_Withdrawn");
-            sendNotification(FromEmail, ToEmail, "", "WITHDRAWN NOTIFICATION - STAFF", vEParams, null, capId);
-            
-            //Email Applicant
-            if(AppToEmail !="")
-            {
-            sendNotification(FromEmail, AppToEmail, "", "WITHDRAWN NOTIFICATION - APPLICANT", vEParams, null, capId);
-            }
-        }
-        //Distributed
-        if(StatusType == 2)
-        {
-            //Email Staff
-            ToEmail = lookup("EMAIL_RECIPIENTS","PLN_Planning_Staff_Application_Distribution");
-            sendNotification(FromEmail, ToEmail, "", "DISTRIBUTION LIST - STAFF", vEParams, null, capId);
-            
-            //Email Applicant
-            if(AppToEmail !="")
-            {
-            sendNotification(FromEmail, AppToEmail, "", "DISTRIBUTION LIST - APPLICANT", vEParams, null, capId);
-            }
-        }
-        //Incomplete
-        if(StatusType == 3)
-        {           
-            //Get the employee that put the record in Incomplete
-            var taskActBy = getTaskActionBy("Application Acceptance");
-            var iNameResult = aa.person.getUser(taskActBy);
-            
-            //Get phone number
-            if (iNameResult.getSuccess())
-            {
-            var iUserObj = iNameResult.getOutput();
-            PlanningPhone = iUserObj.getPhoneNumber();
-            }
+		{
+			//Get Planner
+			var taskResult = aa.workflow.getTask(capId, wfTask); 
 
-            //Get workflow comments
-            var comments = wfComment;
-            
-            addParameter(vEParams,"$$PLANNINGPHONE$$",PlanningPhone);
-            addParameter(vEParams,"$$COMMENTS$$",comments);
-            
-            //Email Applicant
-            if(AppToEmail !="")
-            {
-            sendNotification(FromEmail, AppToEmail, "", "INCOMPLETE SUBMITTAL - APPLICANT", vEParams, null, capId);
-            }
-        }
-      
-      }
+			if (taskResult.getSuccess()) 	
+				{ 
+				var taskOutput = taskResult.getOutput(); 
+				var taskAssignStaff = taskOutput.getAssignedStaff(); 
+				var taskAssignStaffUser = aa.person.getUser(taskAssignStaff.getFirstName(), taskAssignStaff.getMiddleName(), taskAssignStaff.getLastName()).getOutput(); 
+				ToEmail = taskAssignStaffUser.getEmail(); 
+				}		
+			//Withdrawn
+			if(StatusType == 1)
+				{
+					//Email Planner
+					sendNotification(FromEmail, ToEmail, "", "WITHDRAWN NOTIFICATION - STAFF", vEParams, null, capId);
+					
+					//Email Applicant
+					if(AppToEmail !="")
+					{
+					sendNotification(FromEmail, AppToEmail, "", "WITHDRAWN NOTIFICATION - APPLICANT", vEParams, null, capId);
+					}
+				}
+			//Distributed
+			if(StatusType == 2)
+				{
+				//Email Planner
+				sendNotification(FromEmail, ToEmail, "", "DISTRIBUTION LIST - STAFF", vEParams, null, capId);
+				}
+			//Incomplete
+			if(StatusType == 3)
+				{           
+					//Get the employee that put the record in Incomplete
+					var taskActBy = getTaskActionBy("Application Acceptance");
+					var iNameResult = aa.person.getUser(taskActBy);
+					
+					//Get phone number
+					if (iNameResult.getSuccess())
+						{
+					var iUserObj = iNameResult.getOutput();
+					PlanningPhone = iUserObj.getPhoneNumber();
+						}
+
+					//Get workflow comments
+					var comments = wfComment;
+					
+					addParameter(vEParams,"$$PLANNINGPHONE$$",PlanningPhone);
+					addParameter(vEParams,"$$COMMENTS$$",comments);
+					
+					//Email Applicant
+					if(AppToEmail !="")
+						{
+					sendNotification(FromEmail, AppToEmail, "", "INCOMPLETE SUBMITTAL - APPLICANT", vEParams, null, capId);
+						}
+				}
+		  
+		}
     }
 catch (err)
     {
