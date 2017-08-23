29ad627a7e042517bc633c5007d9549f0d4d6ff2
sallred <steve.allred@mesaaz.gov>
2017-08-23 16:36:13 -0700

    removed tst script

--- SCRIPTS/EVENT/ASIUA;Permits!Master Plan!NA!NA.js
+++ SCRIPTS/EVENT/ASIUA;Permits!Master Plan!NA!NA.js
@@ -1,8 +1,5 @@
 //include("PMT_PlanningNumbValidate");
 include("PMT_RelateToPlanningRecord");
 
-// For Brown Bag Training - will remove after
-include("TST_Example1");
-
 //Deposit Fee
 include("PMT_MSTDeposit");
