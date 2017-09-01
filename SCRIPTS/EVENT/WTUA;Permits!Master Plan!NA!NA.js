b9458e783019e3686978e903b1a9f92d9eb7bf6f
sallred <steve.allred@mesaaz.gov>
2017-09-01 09:33:14 -0700

    replace

---
+++ SCRIPTS/EVENT/WTUA;Permits!Master Plan!NA!NA.js
@@ -0,0 +1,13 @@
+/*===================================================================
+// Script Number: 001
+// Script Name: WTUA;Permits!Master Plan!NA!NA.js
+==================================================================*/
+include("PMT_Email_Workflow_Status_Change");
+include("PMT_ResubmittalFee");
+include("PMT_PlanReviewHoursFee");
+include("PMT_SetPermitIssuedDate");
+// added by Vance Smith (Mesa)
+include("PMT_SubmittalCycle");
+
+// added by Kevin Gurney (Accela)
+include("PMT_AssessTechFee");
