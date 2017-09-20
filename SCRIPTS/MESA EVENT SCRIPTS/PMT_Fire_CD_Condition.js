b7b6e1c47fd5a0939a8532d719f030685c9eaae2
sallred <steve.allred@mesaaz.gov>
2017-09-20 16:04:31 -0700

    SP # 21

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_Fire_CD_Condition.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_Fire_CD_Condition.js
@@ -7,12 +7,18 @@
 // Script Run Event: WTUA
 // Script Parents:
 //			WTUA;Permits!Commercial!NA!NA
-===================================================================*/
+===================================================================
+// MODIFICATIONS:
+// DATE      ENGINEER         DESCRIPTION
+// 09/20/17  Steve Allred     SP #21 - Changed name of condition.
+//
+===================================================================
+//*/
 //showDebug = true;
 try{
     var recordAppType = appTypeArray[1];
 	
-	if  (recordAppType == "Commercial")	addStdCondition("Building Permit","Fire CD Required to Issue Permit");
+	if  (recordAppType == "Commercial")	addStdCondition("Building Permit","Fire CAD Drawings Required to Issue Permit");
 }
 catch (err){
 	logDebug("A JavaScript Error occured: " + err.message);
