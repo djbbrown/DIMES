50b4cd86c56563410c96ded7dbb0df42c7cbd671
suzannam <31020270+suzannam@users.noreply.github.com>
2017-09-25 11:34:54 -0700

    Issue SP # : 18
    re-formatting and testing

--- SCRIPTS/MESA EVENT SCRIPTS/ENG_UTL_Email.js
+++ SCRIPTS/MESA EVENT SCRIPTS/ENG_UTL_Email.js
@@ -16,14 +16,10 @@
 try {  
 
     var url = lookup("Agency_URL","ACA");
-    var wfStatusChngResult;
-    
-    var wfStatusChngResult = getWorkflowTaskAppStatus(wfTask,wfStatus);
 
     //Workflow Required and for ALL task types
     if ((wfStatus == "Revisions Required"))
     { 
-        if(wfStatusChngResult !=null) {
             var vEParams = aa.util.newHashtable();         
             var emailAddress = lookup("EMAIL_RECIPIENTS", "ENG_UTL_NCU_EMAIL");
 
@@ -67,160 +63,152 @@ try {
                         //sendNotification(efrom, ToEmail, "City of Mesa: Revisions Required", "ENG_UTL_WORKFLOW_REVISIONREQUIRED", vEParams, null, capId);
                     }
                     }        
-                }
             }
-        }
-        else if ((wfTask == "Permit Issuance" || wfStatus == "Issued"))
-        {
-            
-                if(wfStatusChngResult !=null) {
-            
-                    var vEParams = aa.util.newHashtable();
-                    var emailAddress = lookup("EMAIL_RECIPIENTS", "ENG_UTL_NCU_EMAIL");
+    }
+    else if ((wfTask == "Permit Issuance" || wfStatus == "Issued"))
+    {
+           
+            var vEParams = aa.util.newHashtable();
+            var emailAddress = lookup("EMAIL_RECIPIENTS", "ENG_UTL_NCU_EMAIL");
 
-                    //retrieve template information
-                    var tmpl = aa.communication.getNotificationTemplate("ENG_UTL_WORKFLOW_PERMIT_ISSUED").getOutput();
-                    var ebody = tmpl.getEmailTemplateModel().getContentText();
-                    var esub = tmpl.getEmailTemplateModel().getTitle();
-                    var efrom = tmpl.getEmailTemplateModel().getFrom();
+            //retrieve template information
+            var tmpl = aa.communication.getNotificationTemplate("ENG_UTL_WORKFLOW_PERMIT_ISSUED").getOutput();
+            var ebody = tmpl.getEmailTemplateModel().getContentText();
+            var esub = tmpl.getEmailTemplateModel().getTitle();
+            var efrom = tmpl.getEmailTemplateModel().getFrom();
             
-                    //Get the contact info
-                    var tInfo = getContactArray();
-                    var rowCount = tInfo.length;
+            //Get the contact info
+            var tInfo = getContactArray();
+            var rowCount = tInfo.length;
                     
-                    //Get Email of Applicant
-                    for (var x=0;x<=(rowCount-1);x++)
-                    {
-                        var TypeContact = tInfo[x]["contactType"];
-                        if( TypeContact == "Applicant" || TypeContact == "Contact")  {
+            //Get Email of Applicant
+            for (var x=0;x<=(rowCount-1);x++)
+            {
+                var TypeContact = tInfo[x]["contactType"];
+                if( TypeContact == "Applicant" || TypeContact == "Contact")  {
                             
-                            ToEmail = tInfo[x]["email"];
+                    ToEmail = tInfo[x]["email"];
             
-                            //Add Params
-                            addParameter(vEParams,"$$RECORDID$$",capIDString);
-                            addParameter(vEParams,"$$WORKFLOWSTATUS$$",wfStatus);
-                            addParameter(vEParams, "$$URL$$", url);                          
+                    //Add Params
+                    addParameter(vEParams,"$$RECORDID$$",capIDString);
+                    addParameter(vEParams,"$$WORKFLOWSTATUS$$",wfStatus);
+                    addParameter(vEParams, "$$URL$$", url);                          
                     
-                            if (wfComment != null){
-                                var comments = 'Comments: '+ wfComment;
-                                addParameter(vEParams,"$$WORKFLOWCOMMENT$$",comments);
-                            }
+                    if (wfComment != null){
+                        var comments = 'Comments: '+ wfComment;
+                        addParameter(vEParams,"$$WORKFLOWCOMMENT$$",comments);
+                     }
                     
-                            addParameter(vEParams, "$$EMAILCONTACT$$", emailAddress);
+                    addParameter(vEParams, "$$EMAILCONTACT$$", emailAddress);
                     
-                            logDebug('parameters: '+ vEParams);
+                    logDebug('parameters: '+ vEParams);
                             
-                            //Send email
-                            if(ToEmail){
-                                logDebug("Sending an email to the following contact: (ENG_UTL_WORKFLOW_PERMIT_ISSUED): " + ToEmail + "  Type of Contact: "+TypeContact);
+                    //Send email
+                    if(ToEmail){
+                        logDebug("Sending an email to the following contact: (ENG_UTL_WORKFLOW_PERMIT_ISSUED): " + ToEmail + "  Type of Contact: "+TypeContact);
                                 //sendNotification(efrom, ToEmail, "City of Mesa: Revisions Required", "ENG_UTL_WORKFLOW_PERMIT_ISSUED", vEParams, null, capId);
-                            }
-                            }        
-                        }
-                }
+                    }
+                }        
+            }
         }
         else if ((wfTask == "Plans Coordination" || wfStatus == "Ready to Issue"))
         {
                 
-                    if(wfStatusChngResult !=null) {
-                        var vEParams = aa.util.newHashtable();
-                        
-                        var emailAddress = lookup("EMAIL_RECIPIENTS", "ENG_UTL_NCU_EMAIL");
                   
-                        //retrieve template information
-                        var tmpl = aa.communication.getNotificationTemplate("ENG_UTL_WORKFLOW_READY_TO_ISSUE").getOutput();
-                        var ebody = tmpl.getEmailTemplateModel().getContentText();
-                        var esub = tmpl.getEmailTemplateModel().getTitle();
-                        var efrom = tmpl.getEmailTemplateModel().getFrom();
+                var vEParams = aa.util.newHashtable();
+                        
+                var emailAddress = lookup("EMAIL_RECIPIENTS", "ENG_UTL_NCU_EMAIL");
+                  
+                //retrieve template information
+                var tmpl = aa.communication.getNotificationTemplate("ENG_UTL_WORKFLOW_READY_TO_ISSUE").getOutput();
+                var ebody = tmpl.getEmailTemplateModel().getContentText();
+                var esub = tmpl.getEmailTemplateModel().getTitle();
+                var efrom = tmpl.getEmailTemplateModel().getFrom();
                 
-                        //Get the contact info
-                        var tInfo = getContactArray();
-                        var rowCount = tInfo.length;
-                        var x = 0;
-                        //Get Email of Applicant
-                        for (var x=0;x<=(rowCount-1);x++)
-                        {
-                            var TypeContact = tInfo[x]["contactType"];
-                            if( TypeContact == "Applicant" || TypeContact == "Contact")  {
+                //Get the contact info
+                var tInfo = getContactArray();
+                var rowCount = tInfo.length;
+                var x = 0;
+                //Get Email of Applicant
+                for (var x=0;x<=(rowCount-1);x++)
+                {
+                    var TypeContact = tInfo[x]["contactType"];
+                    if( TypeContact == "Applicant" || TypeContact == "Contact")  {
                                 
-                                ToEmail = tInfo[x]["email"];
+                        ToEmail = tInfo[x]["email"];
                             
-                                //Add Params
-                                addParameter(vEParams,"$$RECORDID$$",capIDString);
-                                addParameter(vEParams,"$$WORKFLOWSTATUS$$",wfStatus);
-                                addParameter(vEParams, "$$URL$$", url);                          
+                        //Add Params
+                        addParameter(vEParams,"$$RECORDID$$",capIDString);
+                        addParameter(vEParams,"$$WORKFLOWSTATUS$$",wfStatus);
+                        addParameter(vEParams, "$$URL$$", url);                          
                         
-                                if (wfComment != null){
-                                    var comments = 'Comments: '+ wfComment;
-                                    addParameter(vEParams,"$$WORKFLOWCOMMENT$$",comments);
-                                }
-                        
-                                addParameter(vEParams, "$$EMAILCONTACT$$", emailAddress);
-                        
-                                logDebug('parameters: '+ vEParams);
-                                
-                                //Send email
-                                if(ToEmail){
-                                    logDebug("Sending an email to the following contact: (ENG_UTL_WORKFLOW_READY_TO_ISSUE): " + ToEmail + "  Type of Contact: "+TypeContact);                                    
-                                    //sendNotification(efrom, ToEmail, "City of Mesa: Revisions Required", "ENG_UTL_WORKFLOW_READY_TO_ISSUE", vEParams, null, capId);
-                                }
-                                }        
+                        if (wfComment != null){
+                                var comments = 'Comments: '+ wfComment;
+                                addParameter(vEParams,"$$WORKFLOWCOMMENT$$",comments);
                         }
-                    }
-
+                        
+                        addParameter(vEParams, "$$EMAILCONTACT$$", emailAddress);
+                        
+                        logDebug('parameters: '+ vEParams);
+                                
+                        //Send email
+                        if(ToEmail){
+                            logDebug("Sending an email to the following contact: (ENG_UTL_WORKFLOW_READY_TO_ISSUE): " + ToEmail + "  Type of Contact: "+TypeContact);                                    
+                                    //sendNotification(efrom, ToEmail, "City of Mesa: Revisions Required", "ENG_UTL_WORKFLOW_READY_TO_ISSUE", vEParams, null, capId);
+                        }
+                    }        
+                }
         }
         else if ((wfTask == "Plans Distributions" || wfStatus == "Revisions Received"))
         {
                 
-                    if(wfStatusChngResult !=null) {
-                        var vEParams = aa.util.newHashtable();
+                var vEParams = aa.util.newHashtable();
                         
-                        var emailAddress = lookup("EMAIL_RECIPIENTS", "ENG_UTL_NCU_EMAIL");
+                var emailAddress = lookup("EMAIL_RECIPIENTS", "ENG_UTL_NCU_EMAIL");
     
                 
-                        //retrieve template information
-                        var tmpl = aa.communication.getNotificationTemplate("ENG_UTL_WORKFLOW_REVISION_RECEIVED").getOutput();
-                        var ebody = tmpl.getEmailTemplateModel().getContentText();
-                        var esub = tmpl.getEmailTemplateModel().getTitle();
-                        var efrom = tmpl.getEmailTemplateModel().getFrom();
+                //retrieve template information
+                var tmpl = aa.communication.getNotificationTemplate("ENG_UTL_WORKFLOW_REVISION_RECEIVED").getOutput();
+                var ebody = tmpl.getEmailTemplateModel().getContentText();
+                var esub = tmpl.getEmailTemplateModel().getTitle();
+                var efrom = tmpl.getEmailTemplateModel().getFrom();
                 
-                        //Get the contact info
-                        var tInfo = getContactArray();
-                        var rowCount = tInfo.length;
-                        var x = 0;
-                        //Get Email of Applicant
-                        for (var x=0;x<=(rowCount-1);x++)
-                        {
-                            var TypeContact = tInfo[x]["contactType"];
-                            if( TypeContact == "Applicant" || TypeContact == "Contact")  {
+                //Get the contact info
+                var tInfo = getContactArray();
+                var rowCount = tInfo.length;
+                var x = 0;
+                //Get Email of Applicant
+                 for (var x=0;x<=(rowCount-1);x++)
+                    {
+                        var TypeContact = tInfo[x]["contactType"];
+                        if( TypeContact == "Applicant" || TypeContact == "Contact")  {
                                 
-                                ToEmail = tInfo[x]["email"];
+                            ToEmail = tInfo[x]["email"];
                 
-                                logDebug("ToEmail = " + ToEmail);
-                                logDebug("efrom = " + efrom);
-                                logDebug("TypeContact = " + TypeContact);
+                            logDebug("ToEmail = " + ToEmail);
+                            logDebug("efrom = " + efrom);
+                            logDebug("TypeContact = " + TypeContact);
                                 
-                                //Add Params
-                                addParameter(vEParams,"$$RECORDID$$",capIDString);
-                                addParameter(vEParams,"$$WORKFLOWSTATUS$$",wfStatus);
-                                addParameter(vEParams, "$$URL$$", url);                          
+                            //Add Params
+                            addParameter(vEParams,"$$RECORDID$$",capIDString);
+                            addParameter(vEParams,"$$WORKFLOWSTATUS$$",wfStatus);
+                            addParameter(vEParams, "$$URL$$", url);                          
                         
-                                if (wfComment != null){
-                                    var comments = 'Comments: '+ wfComment;
-                                    addParameter(vEParams,"$$WORKFLOWCOMMENT$$",comments);
-                                }
+                        if (wfComment != null){
+                                var comments = 'Comments: '+ wfComment;
+                                addParameter(vEParams,"$$WORKFLOWCOMMENT$$",comments);
+                        }
                         
-                                addParameter(vEParams, "$$EMAILCONTACT$$", emailAddress);
+                        addParameter(vEParams, "$$EMAILCONTACT$$", emailAddress);
                         
-                                logDebug('parameters: '+ vEParams);
+                        logDebug('parameters: '+ vEParams);
                                 
-                                //Send email
-                                if(ToEmail){
-                                    logDebug("Sending an email to the following contact: (ENG_UTL_WORKFLOW_REVISION_RECEIVED): " + ToEmail + "  Type of Contact: "+TypeContact);                                                                        
+                        //Send email
+                        if(ToEmail){
+                            logDebug("Sending an email to the following contact: (ENG_UTL_WORKFLOW_REVISION_RECEIVED): " + ToEmail + "  Type of Contact: "+TypeContact);                                                                        
                                    // sendNotification(efrom, ToEmail, "City of Mesa: Revisions Required", "ENG_UTL_WORKFLOW_REVISION_RECEIVED", vEParams, null, capId);
-                                }
-                                }        
-                            }
+                        }
+                        }        
                     }
         }
     }
