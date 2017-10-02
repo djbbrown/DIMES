286224bd04e4b91a2bb86d6496fca50893a11b44
Steve Allred <steve.allred@mesaaz.gov>
2017-10-02 15:31:06 -0700

    Per HB.

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_DocumentReceived.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_DocumentReceived.js
@@ -7,27 +7,31 @@ if (!matches(appTypeArray[1],"Fire","Police Department") && capStatus == "Revisi
 //logDebug("wfTaskStatusCheck = " + wfTaskStatusCheck);
 
 if ((documentModelArray.size() > 0) && (wfTaskStatusCheck) && (publicUser)) 	{
-		// Update workflow task for document to be reviewed
-
-		updateTask("Plans Distribution", "Revisions Received", "Updated by DUA event", "Updated by DUA event");
-		//potentially send email	
-  		
-	
+	for(var index = 0; index < documentModelArray.size(); index++)	{
+		//Exclude Fire CAD Ordinance documents
+		if (documentModelArray.get(index).getDocCategory() != "Fire CAD Ordinance")		{ 
+			// Update workflow task for document to be reviewed
+			updateTask("Plans Distribution", "Revisions Received", "Updated by DUA event", "Updated by DUA event");
+			//potentially send email	
+		}
+	}
 }
-//End Script Permits DUA
 
 var wfTaskStatusCheckAppSubmit = false;
-if (!matches(appTypeArray[1],"Fire","Police Department") && capStatus == "Incomplete" && isTaskActive("Application Submittal")) {
+if (!matches(appTypeArray[1],"Fire","Police Department") && (capStatus == "Incomplete" || capStatus == "Incomplete Submittal") && isTaskActive("Application Submittal")) {
 	var wfTaskStatusCheckAppSubmit = true;
 }
 //logDebug("wfTaskStatusCheckAppSubmit = " + wfTaskStatusCheckAppSubmit);
 
 if ((documentModelArray.size() > 0) && (wfTaskStatusCheckAppSubmit) && (publicUser)) 	{
-	// Update workflow task for document to be reviewed
-
-	updateTask("Application Submittal", "Note", "Document Received - Updated by DUA event", "Document Received - Updated by DUA event");
-	updateAppStatus("Submitted","Document Received");
-	//potentially send email
-
+	for(var index = 0; index < documentModelArray.size(); index++)	{
+		//Exclude Fire CAD Ordinance documents
+		if (documentModelArray.get(index).getDocCategory() != "Fire CAD Ordinance")		{ 
+			// Update workflow task for document to be reviewed
+			updateTask("Application Submittal", "Note", "Document Received - Updated by DUA event", "Document Received - Updated by DUA event");
+			updateAppStatus("Submitted","Document Received");
+			//potentially send email
+		}
+	}
 }
 //End Script Permits DUA
