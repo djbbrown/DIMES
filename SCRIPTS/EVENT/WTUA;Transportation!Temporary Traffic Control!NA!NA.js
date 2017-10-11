a8b00efb8fc53fced33ab62529731dcc651a1e44
mlucward <mong.ward@mesaaz.gov>
2017-10-11 12:45:17 -0700

    SP #68

--- SCRIPTS/EVENT/WTUA;Transportation!Temporary Traffic Control!NA!NA.js
+++ SCRIPTS/EVENT/WTUA;Transportation!Temporary Traffic Control!NA!NA.js
@@ -1,3 +1,4 @@
 //include("TRA_TTC_Approved");  Update workflow on modification so can move this event script call to Transportation!Temporary Traffic Control!~!~  
 include ("TRA_TTC_SetPermitIssuedDate");
 include ("TRA_TTC_PermitIssuedEmail");
+include ("TRA_TTC_SetRecordPermitExpirationDate");
