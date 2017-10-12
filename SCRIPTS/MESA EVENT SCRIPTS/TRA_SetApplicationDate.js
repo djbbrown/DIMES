1ebc72194fde5527290cb3590f99fcb118dd1d4b
mlucward <mong.ward@mesaaz.gov>
2017-10-12 16:21:26 -0700

    SP Punch List Item #318 - Revise Scripts 280 & 286 to not count Fridays and Weekends.

--- SCRIPTS/MESA EVENT SCRIPTS/TRA_SetApplicationDate.js
+++ SCRIPTS/MESA EVENT SCRIPTS/TRA_SetApplicationDate.js
@@ -23,7 +23,30 @@ try
 
   if (currentTime >= 11)
   {
-    today.setDate(today.getDate() + 1);
+    //Sunday thru Wednesday - Next day
+	if ((today.getDay(mesaWorkingDays) == 0) || (today.getDay(mesaWorkingDays) == 1) || (today.getDay(mesaWorkingDays) == 2) || (today.getDay(mesaWorkingDays) == 3)) 
+	{
+	today.setDate(today.getDate(mesaWorkingDays) + 1);
+	}
+	
+	//Thursday add 4 days to get to Monday
+	else if (today.getDay(mesaWorkingDays) == 4) 
+	{
+		today.setDate(today.getDate(mesaWorkingDays) + 4);
+	}
+	
+	//Friday add 3 days to get to Monday
+	else if (today.getDay(mesaWorkingDays) == 5) 
+	{
+		today.setDate(today.getDate(mesaWorkingDays) + 3);
+	}
+	// Saturday add 2 days to get to Monday
+	if (today.getDay(mesaWorkingDays) == 6) 
+	{
+		today.setDate(today.getDate(mesaWorkingDays) + 2);
+	} 
+	
+
   }
 
   editAppSpecific("Application Date",jsDateToASIDate(today),capId)
--- SCRIPTS/MESA EVENT SCRIPTS/TRA_SetPlanReviewExpirationDate.js
+++ SCRIPTS/MESA EVENT SCRIPTS/TRA_SetPlanReviewExpirationDate.js
@@ -26,12 +26,32 @@ try
   if (theDate == null)
   {
     var today = new Date();
-    var currentTime = new Date().getHours();
+	var currentTime = new Date().getHours();
 
-    if (currentTime >= 11)
-    {
-      today.setDate(today.getDate() + 1);
-    }
+		if (currentTime >= 11)
+		{
+			//Sunday thru Wednesday - next day
+			if ((today.getDay(mesaWorkingDays) == 0) || (today.getDay(mesaWorkingDays) == 1) || (today.getDay(mesaWorkingDays) == 2) || (today.getDay(mesaWorkingDays) == 3)) 
+			{
+			today.setDate(today.getDate(mesaWorkingDays) + 1);
+			}
+	
+			//Thursday add 4 days to get to monday
+			else if (today.getDay(mesaWorkingDays) == 4) 
+			{
+				today.setDate(today.getDate(mesaWorkingDays) + 4);
+			}
+	
+			//Friday add 3 days to get to monday
+			else if (today.getDay(mesaWorkingDays) == 5) 
+			{
+				today.setDate(today.getDate(mesaWorkingDays) + 3);
+			}
+			// Saturday, add 2 days to get to monday
+			if (today.getDay(mesaWorkingDays) == 6) 
+			{
+				today.setDate(today.getDate(mesaWorkingDays) + 2);
+			} 
 
     theDate = today;
   }
