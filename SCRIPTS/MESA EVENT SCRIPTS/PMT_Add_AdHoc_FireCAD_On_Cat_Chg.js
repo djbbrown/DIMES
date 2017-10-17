9ae073d96ca596b628aa5d0c7e2166b276dcdeed
sallred <steve.allred@mesaaz.gov>
2017-10-17 11:14:12 -0700

    SP 22

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_Add_AdHoc_FireCAD_On_Cat_Chg.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_Add_AdHoc_FireCAD_On_Cat_Chg.js
@@ -20,13 +20,14 @@ if (docArray)
 	//logDebug("docArray length = " + String(docArray.length));
 	for (var i = 0; i < docArray.length; i++) 
 	{
+		logDebug("Document:" + docArray[i]);
 		if (docArray[i] == "Fire CAD Ordinance") 
 		{
 			docCount += 1;
 		}
 	}
 }
-//logDebug("docCount:" + docCount);
+logDebug("docCount:" + docCount);
 
 //count the Fire CAD Review getTasks
 var arrReviews = ["Fire CAD Review"];
@@ -39,16 +40,17 @@ for(task in tasks)
 		ahtCount += 1;
 	}
 }
-//logDebug("ahtCount:" + ahtCount);
+logDebug("ahtCount:" + ahtCount);
 
 //add adhoc task(s) if there are not enough to match the documents
 if (docCount > ahtCount)	
 {  
 	//Create Ad Hoc Task
+	var thisUser = lookup("PMT_FIRE_CAD_DOC_REVIEWER","Reviewer");
 	for(var index = 0; index < (docCount - ahtCount); index++)
 	{
+		logDebug("index:" + index);
 		var thisCap = capId;
-		var thisUser = lookup("PMT_FIRE_CAD_DOC_REVIEWER","Reviewer");
 		var adt = dateFormatted(sysDate.getMonth(),sysDate.getDayOfMonth(),sysDate.getYear(),"MM/DD/YYYY");  //Assigned Date: today 
 		var ddt = mesaWorkingDays(aa.util.now(), 4);  //Due Date: today + 3 days based on 4-day work-week
 		var taskObj = aa.workflow.getTasks(thisCap).getOutput()[0].getTaskItem();
