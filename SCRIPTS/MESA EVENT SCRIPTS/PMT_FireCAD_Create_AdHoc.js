3cd4451018fb5c1c0e8ba2b9c7acf88f87220153
suzannam <31020270+suzannam@users.noreply.github.com>
2017-10-19 13:35:11 -0700

    Issue SP #: 93
    Reverting the change for testing

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_FireCAD_Create_AdHoc.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_FireCAD_Create_AdHoc.js
@@ -9,54 +9,54 @@
 //  1.0      |03/28/17  |Steve Allred     |Initial Release 
 /*===================================================================*/
 //Start Script
-try{
-	if (documentModelArray.size() > 0) 	{
-		for(var index = 0; index < documentModelArray.size(); index++){
+/*===================================================================*/
+// Script Number: 405
+// Script Name: PMT_FireCAD_Create_AdHoc.js
+// Script Description: Create Fire CAD Ad Hoc Task when Fire CAD document has been uploaded.   
+// Script Run Event: DUA
+// Script Parents: DUA;Permits/Commercial/NA/NA
+//
+// Version   |Date      |Engineer         |Details
+//  1.0      |03/28/17  |Steve Allred     |Initial Release 
+/*===================================================================*/
+//Start Script
+
+if (documentModelArray.size() > 0) 	{
+	for(var index = 0; index < documentModelArray.size(); index++){
 		
 		// Create a Fire CD Activity
 
-			if (documentModelArray.get(index).getDocCategory()=="Fire CAD Ordinance")  {
+		if (documentModelArray.get(index).getDocCategory()=="Fire CAD Ordinance") {
 			//Create Ad Hoc Task
-				var thisCap = capId;
-				var thisUser = lookup("PMT_FIRE_CAD_DOC_REVIEWER","Reviewer");
-				var adt = dateFormatted(sysDate.getMonth(),sysDate.getDayOfMonth(),sysDate.getYear(),"MM/DD/YYYY");  //Assigned Date: today 
-				var ddt = mesaWorkingDays(aa.util.now(), 4);  //Due Date: today + 3 days based on 4-day work-week
-				//var tasks = aa.workflow.getTasks(thisCap).getOutput()[0];
-
-				//if (tasks)
-				{
-					//taskObj = tasks.getTaskItem();
-
-					var userObj = aa.person.getUser(thisUser);
-				    var taskObj = aa.workflow.getTasks(thisCap).getOutput()[0].getTaskItem();
+			var thisCap = capId;
+			var thisUser = lookup("PMT_FIRE_CAD_DOC_REVIEWER","Reviewer");
+			var adt = dateFormatted(sysDate.getMonth(),sysDate.getDayOfMonth(),sysDate.getYear(),"MM/DD/YYYY");  //Assigned Date: today 
+			var ddt = mesaWorkingDays(aa.util.now(), 4);  //Due Date: today + 3 days based on 4-day work-week
+			var taskObj = aa.workflow.getTasks(thisCap).getOutput()[0].getTaskItem();
+			var userObj = aa.person.getUser(thisUser);
 			
 /*			logDebug("thisCap:" + thisCap);
 			logDebug("thisUser:" + thisUser);
 			logDebug("adt:" + new Date(adt));
 			logDebug("ddt:" + new Date(ddt));
 */			
-					taskObj.setProcessCode("WFADHOC_PROCESS");
-					taskObj.setTaskDescription("Fire CAD Review");
-					taskObj.setDispositionNote("Fire CAD Document Uploaded");
-					taskObj.setProcessID(0);
-					taskObj.setAssignmentDate(new Date(adt));
-					taskObj.setDueDate(new Date(ddt));
-					taskObj.setAssignedUser(userObj.getOutput());
+			taskObj.setProcessCode("WFADHOC_PROCESS");
+			taskObj.setTaskDescription("Fire CAD Review");
+			taskObj.setDispositionNote("Fire CAD Document Uploaded");
+			taskObj.setProcessID(0);
+			taskObj.setAssignmentDate(new Date(adt));
+			taskObj.setDueDate(new Date(ddt));
+			taskObj.setAssignedUser(userObj.getOutput());
 
-					wf = aa.proxyInvoker.newInstance("com.accela.aa.workflow.workflow.WorkflowBusiness").getOutput();
-					wf.createAdHocTaskItem(taskObj);
-			}
-			//else{
-			//	logDebug("Could not capture tasks for the given capid. Issue could be TMP record")
-			//}
+			wf = aa.proxyInvoker.newInstance("com.accela.aa.workflow.workflow.WorkflowBusiness").getOutput();
+			wf.createAdHocTaskItem(taskObj);
+	
 		}	
   		
-		}
 	}
 }
-catch (err){
-	logDebug("A JavaScript Error occurred:  in PMT_FireCAD_Create_AdHoc: " + err.message);
-}
+
+//End Script 
 
 
 //End Script 
