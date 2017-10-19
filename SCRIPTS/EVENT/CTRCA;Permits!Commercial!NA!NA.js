d4ddec943dc9e933733f2b2d8761ac557282fd38
suzannam <31020270+suzannam@users.noreply.github.com>
2017-10-19 15:24:46 -0700

    Issue SP #: 93
    remove not required to be added.

--- SCRIPTS/EVENT/CTRCA;Permits!Commercial!NA!NA.js
+++ SCRIPTS/EVENT/CTRCA;Permits!Commercial!NA!NA.js
@@ -12,5 +12,4 @@ if (publicUser) {
 	include("PMT_CodeEnforcementNumber");
 	include("PMT_Email_Hazardmat_Received");
 	include("PMT_MasterPlan_DetailDesc");
-	include("PMT_FireCAD_Create_AdHoc");
 }
