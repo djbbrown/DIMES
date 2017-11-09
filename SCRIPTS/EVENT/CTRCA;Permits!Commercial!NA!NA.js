c7b464017d9bd353a787f51c920c40c3a49bdaaa
gurneykevin <kgurney@accela.com>
2017-11-09 15:33:14 -0800

    SP Issue 422 Remove publicUser If statement since CTRCA event is executed when internal user finishes permit application that was started in ACA.

--- SCRIPTS/EVENT/CTRCA;Permits!Commercial!NA!NA.js
+++ SCRIPTS/EVENT/CTRCA;Permits!Commercial!NA!NA.js
@@ -1,15 +1,14 @@
 //adding publicUser criteria check to execute scripts based on AA or ACA
-if (publicUser) {
-	include("PMT_FloodControlPermit");
-	include("PMT_StapleyTag");
-	include("PMT_Parcel_51");
-	include("PMT_CM_Gas_Electric");
-	include("PMT_CMSW_Gas");
-	include("PMT_Light_Rail");
-	include("PMT_Communications_Towers");
-	// added by Vance Smith (Mesa)
-	include("PMT_AutopopulateASIFieldsFromGISAttributes");
-	include("PMT_CodeEnforcementNumber");
-	include("PMT_Email_Hazardmat_Received");
-	include("PMT_MasterPlan_DetailDesc");
-}
+//removing if statement on publicUser since if record is finished in AA it runs the CTRCA event with the internal user
+include("PMT_FloodControlPermit");
+include("PMT_StapleyTag");
+include("PMT_Parcel_51");
+include("PMT_CM_Gas_Electric");
+include("PMT_CMSW_Gas");
+include("PMT_Light_Rail");
+include("PMT_Communications_Towers");
+// added by Vance Smith (Mesa)
+include("PMT_AutopopulateASIFieldsFromGISAttributes");
+include("PMT_CodeEnforcementNumber");
+include("PMT_Email_Hazardmat_Received");
+include("PMT_MasterPlan_DetailDesc");
