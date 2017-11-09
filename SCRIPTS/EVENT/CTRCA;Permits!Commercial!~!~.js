ab2d2cb5c0be4e61bc8cfc84e0a76b0c4e748422
gurneykevin <kgurney@accela.com>
2017-11-09 15:44:23 -0800

    SF Issue 422
    Add a call to execute the CommercialASAFees script if the ACA record is completed by an internal user in Accela Automation.

--- SCRIPTS/EVENT/CTRCA;Permits!Commercial!~!~.js
+++ SCRIPTS/EVENT/CTRCA;Permits!Commercial!~!~.js
@@ -3,3 +3,8 @@ if (publicUser) {
 	include("PMT_SignalButteTag");
 	include("PMT_Condition_Email");
 }
+
+//adding this call for executing fees if ACA record application is completed by internal user.
+if (!publicUser) {
+	include("PMT_CommercialASAFees");
+}
