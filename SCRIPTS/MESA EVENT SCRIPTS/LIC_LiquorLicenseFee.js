090a90ef0c3dbb3fadba39e125161e576acac9a6
Michael VanWie <MVanWie@mesaaz.gov>
2017-12-05 11:12:10 -0700

    #11 - Added Proration of Liquor annual fee

--- SCRIPTS/MESA EVENT SCRIPTS/LIC_LiquorLicenseFee.js
+++ SCRIPTS/MESA EVENT SCRIPTS/LIC_LiquorLicenseFee.js
@@ -4,52 +4,107 @@
 // Script Developer: Raminder Gill
 // Script Agency: Accela
 // Script Description: 
-// 		On a Licenses/Liquor/Liquor/Application, assess and invoice the annual liquor license fee 
-// 		when the workflow task “Issue License” is set to a status of “Ready to Issue”. 
-// 		On a Licenses/Liquor/Liquor/Renewal, assess and invoice the annual liquor license fee 
-// 		when the workflow task “Renewal Intake” is set to a status of “Submitted”, set by Mesa.
+// 		1) On a Licenses/Liquor/Liquor/Application, when the workflow task “City Council Preparation” is becomes active
+//			determine which Annual fee to add and invoice based on which quarter the Council Agenda Date falls in. Also add
+//          a an Issuance fee
+//
+// 		2) On a Licenses/Liquor/Liquor/Renewal, assess and invoice the annual liquor license fee 
+// 		   when the workflow task “Renewal Intake” is set to a status of “Submitted”, set by Mesa.
 // Script Run Event: WTUA
 // Script Parents:
 // WTUA: Licenses/Liquor/Liquor/Application 
 // WTUA: Licenses/Liquor/Liquor/Renewal 
 // 
+// Version   |Date       |Engineer          |Details
+//  1.0      | --/--/--  | Raminder Gill    | Initial Release
+//  1.1      | 12/05/17  | Michael VanWie   | Added proration of Annual fee based on quarter
 /*==================================================================*/
 
-var valSeries = getAppSpecific("Series Type");
+try {
+	var valSeries = getAppSpecific("Series Type");
 
-if (appTypeArray[3] == "Application"){
-	if (isTaskActive("City Council Preparation")
-		&& (!feeExists("L020") || !feeExists("L030") || !feeExists("L040"))) { 
-		//Issuance Fee
-		if (matches(valSeries, "1", "2", "3", "4", "6", "7", "8", "9", "10", "11", "12", "13", "14") && !feeExists("L020")) {
-			addFee("L020","LIC_LIQUOR", "FINAL",  1, "Y");
-		}
-		//Annual Fee Series 1-4,8 and 13
-		if ((valSeries== "1" ||  valSeries== "2" || valSeries== "3" || valSeries== "4" || valSeries== "8" || valSeries== "11" || valSeries== "12" || valSeries== "13") && !feeExists("L030")) 
+	if (appTypeArray[3] == "Application"){
+		var agendaDate = '' + getAppSpecific("Council Agenda Date");
+		
+		if(agendaDate != '')
 		{
-			addFee("L030","LIC_LIQUOR", "FINAL",  1, "Y");
+			var agendaQuarter = getQuarter(parseDate(agendaDate));
+		
+			if (isTaskActive("City Council Preparation")
+				&& (!feeExists("L020") 
+				|| !feeExists("L030") || !feeExists("L031") || !feeExists("L032") || !feeExists("L033") 
+				|| !feeExists("L040") || !feeExists("L041") || !feeExists("L042") || !feeExists("L043"))) { 
+				
+				//Issuance Fee
+				if (matches(valSeries, "1", "2", "3", "4", "6", "7", "8", "9", "10", "11", "12", "13", "14") && !feeExists("L020")) {
+					addFee("L020","LIC_LIQUOR", "FINAL",  1, "Y");
+				}
+
+				//Annual Fee Series 1-4,8 and 13
+				//Added Prorated values - Michael VanWie 12/04/2017
+				if ((valSeries== "1" ||  valSeries== "2" || valSeries== "3" || valSeries== "4" || valSeries== "8" || valSeries== "11" || valSeries== "12" || valSeries== "13") 
+				&& (!feeExists("L030") && !feeExists("L031") && !feeExists("L032") && !feeExists("L033"))) 
+				{
+					if(agendaQuarter == 1)
+						addFee("L030","LIC_LIQUOR", "FINAL",  1, "Y");
+					if(agendaQuarter == 2)
+						addFee("L031","LIC_LIQUOR", "FINAL",  1, "Y");
+					if(agendaQuarter == 3)
+						addFee("L032","LIC_LIQUOR", "FINAL",  1, "Y");
+					if(agendaQuarter == 4)
+						addFee("L033","LIC_LIQUOR", "FINAL",  1, "Y");
+				}
+
+				// Annual Fee Series 6,7,8,10,14
+				if ((valSeries== "6" ||  valSeries== "7" || valSeries== "9" || valSeries== "10" || valSeries== "14") 
+				&& (!feeExists("L040") && !feeExists("L041") && !feeExists("L042") && !feeExists("L043") ))
+				{
+					if(agendaQuarter == 1)
+						addFee("L040","LIC_LIQUOR", "FINAL",  1, "Y");
+					if(agendaQuarter == 2)
+						addFee("L041","LIC_LIQUOR", "FINAL",  1, "Y");
+					if(agendaQuarter == 3)
+						addFee("L042","LIC_LIQUOR", "FINAL",  1, "Y");
+					if(agendaQuarter == 4)
+						addFee("L043","LIC_LIQUOR", "FINAL",  1, "Y");
+				}
+			}
 		}
-		// Annual Fee Series 6,7,8,10,14
-		if ((valSeries== "6" ||  valSeries== "7" || valSeries== "9" || valSeries== "10" || valSeries== "14") && !feeExists("L040")) 
+		else
 		{
-			addFee("L040","LIC_LIQUOR", "FINAL",  1, "Y");
+		    logDebug("No annual fee was applied to record because a Council Agenda Date is required.");
+		}
+	}
+	if (wfTask == "Renewal Intake" && wfStatus == "Submitted"){
+		if (appTypeArray[3] == "Renewal") {
+		
+			//Annual Fee Series 1-4,8 and 13
+			if ((valSeries== "1" ||  valSeries== "2" || valSeries== "3" || valSeries== "4" || valSeries== "8" || valSeries== "11" || valSeries== "12" || valSeries== "13") && !feeExists("L010")) 
+			{
+				addFee("L010","LIC_LIQ_RNWL", "FINAL",  1, "Y");
+			}
+			// Annual Fee Series 6,7,8,10,14
+			if ((valSeries== "6" ||  valSeries== "7" || valSeries== "9" || valSeries== "10" || valSeries== "14") && !feeExists("L020")) 
+			{
+				addFee("L020","LIC_LIQ_RNWL", "FINAL",  1, "Y");
+			}
 		}
 	}
 }
-if (wfTask == "Renewal Intake" && wfStatus == "Submitted"){
-	if (appTypeArray[3] == "Renewal") {
-	
-		//Annual Fee Series 1-4,8 and 13
-		if ((valSeries== "1" ||  valSeries== "2" || valSeries== "3" || valSeries== "4" || valSeries== "8" || valSeries== "11" || valSeries== "12" || valSeries== "13") && !feeExists("L010")) 
-		{
-			addFee("L010","LIC_LIQ_RNWL", "FINAL",  1, "Y");
-		}
-		// Annual Fee Series 6,7,8,10,14
-		if ((valSeries== "6" ||  valSeries== "7" || valSeries== "9" || valSeries== "10" || valSeries== "14") && !feeExists("L020")) 
-		{
-			addFee("L020","LIC_LIQ_RNWL", "FINAL",  1, "Y");
-		}
-	}
+catch(err)
+{
+	logDebug("A JavaScript Error occured: " + err.message);
+}
+
+function parseDate(str) {
+    var mdy = str.split('/');
+    return new Date(mdy[2], mdy[0]-1, mdy[1]);
+}
+
+function getQuarter(d) {
+  d = d || new Date(); // If no date supplied, use today
+  var q = [1,2,3,4];
+  return q[Math.floor(d.getMonth() / 3)];
 }
 
 
