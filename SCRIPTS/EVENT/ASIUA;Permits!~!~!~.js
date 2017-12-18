ed41f571ee546fec80ffe0863bd8d78ccfb4e35e
gurneykevin <kgurney@accela.com>
2017-12-18 13:28:04 -0800

    SP 403 Reactivating call to PMT_PenaltyDate_ForAsiua for testing.

--- SCRIPTS/EVENT/ASIUA;Permits!~!~!~.js
+++ SCRIPTS/EVENT/ASIUA;Permits!~!~!~.js
@@ -1,6 +1,8 @@
 include("PMT_ClearanceTo");
-// added by John Cheney (Mesa) 9/19/2016
-include("PMT_CopyPenaltyDateToDueDate");
 
 // added by Brian ODell (Mesa) 10/24/2016
 include("PMT_PenaltyDate_ForAsiua");  
+
+// added by John Cheney (Mesa) 9/19/2016
+include("PMT_CopyPenaltyDateToDueDate");
+
