f19597b2cc261b0b5653eacecd13b97217da79cb
suzannam <31020270+suzannam@users.noreply.github.com>
2018-01-09 09:21:25 -0700

    Project SP: 126
    clean up

--- SCRIPTS/EVENT/ASA;Engineering!Utilities!Non-City!Small Wireless Facility.js
+++ SCRIPTS/EVENT/ASA;Engineering!Utilities!Non-City!Small Wireless Facility.js
@@ -1,4 +1,4 @@
 /*===================================================================
 // Script Name: ASA;Engineering!Utilities!Non City!Small Wireless Facility.js
 ==================================================================*/
-include("ENG_UTL_SWF_Email");
+include("ENG_UTL_SWF_EMail");
