6e4e6e3e7d79d66da8eb1d301d7521be8e5e4d96
suzannam <31020270+suzannam@users.noreply.github.com>
2018-01-24 16:38:48 -0700

    Project SP: 126
    add batch process to send email notification for 15 days SWF notice

---
+++ SCRIPTS/BATCH/ENG_SendEmail_SWF_On15DaysLateNotice.js
@@ -0,0 +1,86 @@
+/*------------------------------------------------------------------------------------------------------/
+| Author: SuzannaM
+| Script Name: ENG_SendEmail_SWF_On15DaysLateNotice.js
+| Date: January 24th, 2018
+| Purpose: Sends an email notification for Small Wireless Facility Records that have not been address  
+| within 15 days of submittal.
+| Records that have not been Issued, Incomplete, Closed or Voided
+| 
+----------------------------------------------------------------------------------------------------*/
+
+/*------------------------------------------------------------------------------------------------------/
+| Initialize the include scripts.
+--------------------------------------------------------------------------------------------------------*/
+eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
+eval(getScriptText("INCLUDES_BATCH"));
+eval(getScriptText("INCLUDES_CUSTOM"));
+
+function getScriptText(vScriptName) {
+	vScriptName = vScriptName.toUpperCase();
+	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
+	var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+	return emseScript.getScriptText() + "";
+}
+
+/*------------------------------------------------------------------------------------------------------/
+| Start Main Processing and sending of email
+--------------------------------------------------------------------------------------------------------*/
+try {
+    logDebug("Starting the Batch Process : ENG_SendEmail_SWF_On15DaysLateNoticeTo");
+    
+    capId = "";
+    var showDebug = true;
+    var emailText = "";
+    var initialContext = aa.proxyInvoker.newInstance("javax.naming.InitialContext", null).getOutput();
+    var ds = initialContext.lookup("java:/AA");
+    var conn = ds.getConnection();
+    var selectString = "SELECT B1_ALT_ID, B1_APPL_STATUS,B1_FILE_DD from b1permit where b1_per_group like 'Engineering' and b1_per_type = 'Utilities' \
+            and B1_ALT_ID not like '%TMP%' \
+            and B1_PER_SUB_TYPE = 'Non-City' and B1_PER_CATEGORY = 'Small Wireless Facility' \
+            and SERV_PROV_CODE = 'MESA'\
+            and B1_APPL_STATUS not like 'Issued' \
+            and B1_APPL_STATUS not like 'Incomplete Submittal'\
+            and B1_APPL_STATUS not like 'Closed'\
+            and B1_APPL_STATUS not like 'Void' and TO_DATE(B1_FILE_DD,'DD-MON-YYYY') <= TO_DATE(SYSDATE-15, 'DD-MON-YYYY')";
+
+    var SQLStatement = conn.prepareStatement(selectString);
+    var rSet = SQLStatement.executeQuery();
+    var emailString  = "";
+
+    while (rSet.next()) {
+        var permitNumber =rSet.getString("B1_ALT_ID");
+        var permitStatus =rSet.getString("B1_APPL_STATUS");
+        var permitFileDate = rSet.getString("B1_FILE_DD");
+        emailString = emailString + "ID: "+permitNumber + " Status : "+permitStatus+ " File Date: "+ permitFileDate+ " ** ";
+
+        }
+
+    var params = aa.util.newHashtable();
+    emailTo = lookup("EMAIL_RECIPIENTS","ENG_UTL_SWF_EMAIL");
+        
+    //If records are found send email high importance
+    if (emailString != null && emailString != ""){
+        params.put("$$LISTPMTS$$", emailString );
+        logDebug("Emailing: To: "+emailTo+" Following Records" +emailString);
+        sendNotification(emailTo,emailTo,"","ENG_UTL_SWF_15DAYS_NOTICE",params,null);
+    }
+    //If no records found send email low importance.
+    else {
+        logDebug("Emailing: To: "+emailTo+" No record found" +emailString);
+        sendNotification(emailTo,emailTo,"","ENG_UTL_SWF_15DAYS_OKAY",params,null);
+    }
+    
+    logDebug("Completed the Batch Process : ENG_SendEmail_SWF_On15DaysLateNoticeTo");
+    
+}
+catch (e) {
+    aa.print("Exception getting data from ENG_SendEmail_SWF_On15DaysLateNotice: " + e.message);
+  
+}
+finally {
+    initialContext.close();
+    SQLStatement.close();
+    rSet.close();    
+    conn.close();
+}
+
