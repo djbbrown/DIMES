7113c06d1dfc53cd66d0d06694b04044e1c4b74b
suzannam <31020270+suzannam@users.noreply.github.com>
2018-02-05 15:25:23 -0700

    Issue SP: 136
    send invoice info for engineering with email notification

--- SCRIPTS/MESA EVENT SCRIPTS/ENG_UTL_Email_Invoice.js
+++ SCRIPTS/MESA EVENT SCRIPTS/ENG_UTL_Email_Invoice.js
@@ -11,58 +11,81 @@
 //
 /*==================================================================*/
 try {  
+    
+        if (feesInvoicedTotal > 0 && balanceDue  > 0){
+    
+            //Send Invoice as PDF
+            var reportName = "AA_Invoice";  
+            var recParams = aa.util.newHashtable();
+            reportArray = [];
 
-    if (feesInvoicedTotal > 0 && balanceDue  > 0){
-        var url = lookup("Agency_URL","ACA");
-
-         //Workflow Required and for ALL task types
-        var vEParams = aa.util.newHashtable();         
-        var emailAddress = lookup("EMAIL_RECIPIENTS", "ENG_UTL_NCU_EMAIL");
-
-        //retrieve template information
-        var tmpl = aa.communication.getNotificationTemplate("ENG_UTL_WORKFLOW_INVOICE_AFTER").getOutput();
-        var ebody = tmpl.getEmailTemplateModel().getContentText();
-        var esub = tmpl.getEmailTemplateModel().getTitle();
-        var efrom = tmpl.getEmailTemplateModel().getFrom();
-
-        //Get the contact info
-        var tInfo = getContactArray();
-        var rowCount = tInfo.length;
-
-        var UtilityNo = AInfo["Utility Provider Project No."];                
+            //Retrieving all current invoices for the PMT.
+            var invoices = aa.finance.getInvoiceByCapID(capId, null).getOutput();
             
-        //Get Email of Applicant
-        for (var x=0;x<=(rowCount-1);x++)
-        {
-            var TypeContact = tInfo[x]["contactType"];
-            if( TypeContact == "Applicant" || TypeContact == "Contact")  {
-                
-                var ToEmail = tInfo[x]["email"];
-                   
-                //Add Params
-                addParameter(vEParams,"$$RECORDID$$",capIDString);
-                addParameter(vEParams, "$$EMAILCONTACT$$", emailAddress);
-                addParameter(vEParams, "$$URL$$", url); 
-                                 
-                if (UtilityNo != null){
-                    var utlNo = 'Utility Provider Project No: '+UtilityNo;
-                    addParameter(vEParams,"$$UTILITYPROVIDERNO$$",utlNo);
-                }                
-                logDebug('parameters: '+ vEParams);
-                    
-                //Send email
-                if(ToEmail){
-                    logDebug("Sending an email to the following contact: (ENG_UTL_WORKFLOW_INVOICE_AFTER): " + ToEmail + "  Type of Contact: "+TypeContact);
-                    sendNotification(efrom, ToEmail, "City of Mesa: Revisions Required", "ENG_UTL_WORKFLOW_INVOICE_AFTER", vEParams, null, capId);
+            for(i in invoices) {
+                if (invoices[i].invNbr != null){
+                    var recParams = aa.util.newHashtable();
+    
+                    addParameter(recParams ,"capId",capId.getCustomID());
+                    addParameter(recParams ,"invoicenbr",invoices[i].invNbr.toString());
+    
+                    logDebug("Attaching Invoice PDF File for : ");
+                    logDebug("Invoice Number: "+invoices[i].invNbr);
+                    logDebug("Invoice Number: "+capId.getCustomID());
+    
+                   var rpt = generateReport(capId, reportName, "Engineering", recParams);
+                   reportArray.push(rpt);     
                 }
-            }        
+            }
+     
+            var url = lookup("Agency_URL","ACA");
+    
+             //Workflow Required and for ALL task types
+            var vEParams = aa.util.newHashtable();         
+            var emailAddress = lookup("EMAIL_RECIPIENTS", "ENG_UTL_NCU_EMAIL");
+    
+            //retrieve template information
+            var tmpl = aa.communication.getNotificationTemplate("ENG_UTL_WORKFLOW_INVOICE_AFTER").getOutput();
+            var ebody = tmpl.getEmailTemplateModel().getContentText();
+            var esub = tmpl.getEmailTemplateModel().getTitle();
+            var efrom = tmpl.getEmailTemplateModel().getFrom();
+    
+            //Get the contact info
+            var tInfo = getContactArray();
+            var rowCount = tInfo.length;
+    
+            var UtilityNo = AInfo["Utility Provider Project No."];                
+                
+            //Get Email of Applicant
+            for (var x=0;x<=(rowCount-1);x++)
+            {
+                var TypeContact = tInfo[x]["contactType"];
+                if( TypeContact == "Applicant" || TypeContact == "Contact")  {
+                    
+                    var ToEmail = tInfo[x]["email"];
+                       
+                    //Add Params
+                    addParameter(vEParams,"$$RECORDID$$",capIDString);
+                    addParameter(vEParams, "$$EMAILCONTACT$$", emailAddress);
+                    addParameter(vEParams, "$$URL$$", url); 
+                                     
+                    if (UtilityNo != null){
+                        var utlNo = 'Utility Provider Project No: '+UtilityNo;
+                        addParameter(vEParams,"$$UTILITYPROVIDERNO$$",utlNo);
+                    }                
+                    logDebug('parameters: '+ vEParams);
+                        
+                    //Send email
+                    if(ToEmail){
+                        logDebug("Sending an email to the following contact: (ENG_UTL_WORKFLOW_INVOICE_AFTER): " + ToEmail + "  Type of Contact: "+TypeContact);
+                        sendNotification(efrom, ToEmail, "City of Mesa: Revisions Required", "ENG_UTL_WORKFLOW_INVOICE_AFTER", vEParams, reportArray,capId);
+                        
+                    }
+                }        
+            }
+            }
         }
+        catch (err)
+        {
+        logDebug("A JavaScript Error occured: " + err.message);
         }
-    }
-    catch (err)
-    {
-    logDebug("A JavaScript Error occured: " + err.message);
-    }
-
-
-  
