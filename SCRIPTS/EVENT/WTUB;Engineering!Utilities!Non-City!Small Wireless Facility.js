baaa35e1821cd88f4c5898dbb97ef64a124a9c99
suzannam <31020270+suzannam@users.noreply.github.com>
2018-02-07 12:39:13 -0700

    Project SP: 126
    prevent issuance of SWF on inspections and fees still due.

---
+++ SCRIPTS/EVENT/WTUB;Engineering!Utilities!Non-City!Small Wireless Facility.js
@@ -0,0 +1,9 @@
+/*===================================================================
+//
+// Script Name: WTUB;Engineering!Utilities!Non-City!Small Wireless Facility.js
+==================================================================*/
+include("ENG_ZeroFeeBalanceCheck");
+
+//Prevent the inspection task to complete if there are
+//inspection still Scheduled.
+include("PMT_WorkflowTask_Inspections");
