f7c76a821fd8c38161b1c3aa81e81988a7500b00
Michael VanWie <MVanWie@mesaaz.gov>
2018-02-08 14:10:15 -0700

    SPP 249 - Removing auto invoice script

--- SCRIPTS/EVENT/WTUA;Licenses!Liquor!Liquor!Application.js
+++ SCRIPTS/EVENT/WTUA;Licenses!Liquor!Liquor!Application.js
@@ -1,4 +1,4 @@
-include("LIC_LiquorLicenseFee");
+//include("LIC_LiquorLicenseFee"); --removed 2/8/2018 issue #249 on PunchList
 //include("LIC_CreateLicenseRecord"); 
 include("LIC_AdministrativeReviewDueDateSet_2"); 
 include("LIC_AdjustSubstantiveReviewDueDate");
