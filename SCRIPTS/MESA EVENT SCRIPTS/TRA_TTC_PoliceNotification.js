3dcf5619bd630f9cf0505cca333d6fd3611ca9d1
mlucward <mong.ward@mesaaz.gov>
2018-02-12 11:51:13 -0700

    SP 258 No line breaks spaces and ; only

--- SCRIPTS/MESA EVENT SCRIPTS/TRA_TTC_PoliceNotification.js
+++ SCRIPTS/MESA EVENT SCRIPTS/TRA_TTC_PoliceNotification.js
@@ -76,9 +76,9 @@ try {
                 for (x in tInfo) 
                     {
                     //Put one line for each row of data
-                    TrafficRestriction = TrafficRestriction + " " + (tInfo[x]["Street Name"] + "\xa0\xa0\xa0\xa0\xa0 " + tInfo[x]["Direction of Travel"] + "\xa0\xa0\xa0\xa0\xa0 " + tInfo[x]["No. of Lanes Closed in this Direction"] + "\xa0\xa0\xa0\xa0\xa0 " + tInfo[x]["Lane Type"] + "\xa0\xa0\xa0\xa0\xa0 " + tInfo[x]["Notes"] +  "<br />");
+                    TrafficRestriction = TrafficRestriction + " " + (tInfo[x]["Street Name"] + "\xa0\xa0\xa0\xa0\xa0 " + tInfo[x]["Direction of Travel"] + "\xa0\xa0\xa0\xa0\xa0 " + tInfo[x]["No. of Lanes Closed in this Direction"] + "\xa0\xa0\xa0\xa0\xa0 " + tInfo[x]["Lane Type"] + "\xa0\xa0\xa0\xa0\xa0 " + tInfo[x]["Notes"] +  ";" + "\xa0\xa0\xa0\xa0\xa0 " + "\n");
                     }
-					//logDebug("Traffic Restriction: " + TrafficRestriction + "<br />");
+					//logDebug("Traffic Restriction: " + TrafficRestriction + "\n");
             }
             
 			//Get the contact info
