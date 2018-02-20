ce09d74d5d8d66c022bc94b311a67e6bb919adc3
Jack Tu <jtu@accela.com>
2018-02-20 08:44:36 -0500

    Added to log debug for user JTU.

---
+++ INCLUDES_CUSTOM_GLOBALS.js
@@ -0,0 +1,4 @@
+if (matches(currentUserID,"ADMIN","LICADMIN", "LWACHT", "EWYLAM", "KGURNEY","LINSCHEIDM","BODELL","KFORD","SVELOUD","MKNISKERN","SALLRED","MWARD","MVANWIE","VSMITH","SMAJCHR","JTU")) {
+     showDebug = 1;
+     showMessage=true;
+}
