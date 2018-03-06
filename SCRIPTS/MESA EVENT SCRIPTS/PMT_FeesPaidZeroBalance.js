b3d117e7dc442446dc0c32cecd13e0377db692c2
mlucward <mong.ward@mesaaz.gov>
2018-03-06 08:43:37 -0700

    SP 165 Version 1 deployment

--- SCRIPTS/MESA EVENT SCRIPTS/PMT_FeesPaidZeroBalance.js
+++ SCRIPTS/MESA EVENT SCRIPTS/PMT_FeesPaidZeroBalance.js
@@ -7,10 +7,17 @@
 // Script Description: 
 // 		When record status is "Fees Due" and payment is received so that balance on record is = $0.00
 //		Then change the status of record to "Fees Paid"
+//		(Copied from TRA_TTC_FeesPaidZeroBalance.js)
 //
-// Script Run Event: PRA
+// Script Run Event: PaymentRecievedAfter
 // Script Parents:
-//             PRA:Permits!Sign!NA!NA.js
+//             	PRA;Permits!Addenda or Deferred!~!~.js
+//				PRA;Permits!Commercial!~!~.js
+//				PRA;Permits!Demolition!NA!NA.js
+//				PRA;Permits!Document Retrieval!NA!NA.js
+//				PRA;Permits!Master Plan!NA!NA.js
+//				PRA;Permits!Residential!~!~.js
+//				PRA:Permits!Sign!NA!NA.js
 // 
 //==================================================================*/
 /* intellisense references */
