ae92596887e4e214f34d0d0a710d456661232f7c
mlucward <mong.ward@mesaaz.gov>
2018-03-08 12:16:12 -0700

    SP 318 update to update task

--- SCRIPTS/MESA EVENT SCRIPTS/TRA_TTC_Steel_Plate.js
+++ SCRIPTS/MESA EVENT SCRIPTS/TRA_TTC_Steel_Plate.js
@@ -1,13 +1,13 @@
 /*===================================================================
 // Script Number: 282
 // Script Name: TRA_TTC_Steel_Plate.js
-// Script Description: 	If wfTask Inspections is resulted with Steel Plate in ROW, add Condition Steel Plate in ROW to record.
+// Script Description: 	If Final Inspections is resulted with "Steel Plate in ROW", add Condition Steel Plate in ROW and update WF Task Inspections with "Steel Plate in ROW"
 // Script Parents:IRSA;Transportation!~!~!~.js
 // Test Record: TTC16-00024
 // Version   |Date      |Engineer         |Details
 //  1.0      |09/19/16  |Steve Veloudos   |Initial Release
 //  2.0      |11/22/16  |Steve Veloudos   |Adj to fix multipule conditions added
-//	3.0		 |11/22/16  |Mong Ward	      |Adj to fix multiple conditions added - Check if condition exists
+//	3.0		 |03/08/18  |Mong Ward	      |Adj to fix multiple conditions added - Check if condition exists - Update WF
 /*==================================================================*/
 
 try {
@@ -21,6 +21,9 @@ try {
 				showMessage = true;
 				comment("Steel Plate condition Hold - See Holds and Notices");
 			}
+			updateTask("Inspections", "Steel Plate in ROW", "Updated by ISRA event", "Updated by ISRA event");
+			showMessage = true;
+			comment("The Permit is Updated to Steel Plate in ROW.");
 		}
                             
     }
