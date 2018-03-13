3a61aebcd077df97cce54ce877b4bc1d73df8c68
Michael VanWie <MVanWie@mesaaz.gov>
2018-03-13 14:16:34 -0700

    SP#12 - Peddler Update

--- SCRIPTS/MESA EVENT SCRIPTS/LIC_IssueLicenseFeeBalanceCheck.js
+++ SCRIPTS/MESA EVENT SCRIPTS/LIC_IssueLicenseFeeBalanceCheck.js
@@ -7,30 +7,37 @@
 // Script Run Event: WorkflowTaskUpdateBefore
 // Script Parents:
 //            WTUB;Licenses!~!~!~
+// Updates:
+// -----------------------------------------------------------------------------
+// | BY         |    DATE      |  NOTES
+// -----------------------------------------------------------------------------
+// | M VanWie   |  03/13/2018  | - Expanded to check Peddler Renewal and made it easier to include more WTUB checks
+// |            |              | - Added Try / Catch
 /*==================================================================*/
 
 // Under this line create the function that will need to run at script runtime.
 // the function will be called in the event ("WorkflowTaskUpdateBefore") major event.
 
-try{
-	if(
-			(
-						(wfTask == "Issue License" && wfStatus == "Issued")
-						|| (wfTask == "License Issuance" && wfStatus == "Issued")
-						|| (wfTask == "City Clerk" && wfStatus == "Applicant Notified")
-						|| (wfTask == "License Application" && wfStatus == "Received")
-						|| (wfTask == "Application Intake" && wfStatus == "Received")
-						|| (wfTask == "Renewal Submittal" && wfStatus == "Renewed")
-			)
-			&& appTypeArray[3] == 'Application'
-			&& (balanceDue > 0 || feeTotalByStatus("NEW") > 0)
-	){
+try {
+	var appTasks = ["Issue License", "License Issuance", "City Clerk", "License Application", "Application Intake"];
+	var appStatus = ["Issued", "Applicant Notified", "Received"]
+	
+	var rnwTasks = ["Renewal Submittal"];
+	var rnwStatus = ["Renewed"];
+	
+	if( (IsStrInArry(wfTask, appTasks) && IsStrInArry(wfStatus, appStatus) && appTypeArray[3] == "Application")
+		||
+		(IsStrInArry(wfTask, rnwTasks) && IsStrInArry(wfStatus, rnwStatus) && appTypeArray[3] == "Renewal")
+		&&
+		(balanceDue > 0 || feeTotalByStatus("NEW") > 0)
+	  ){
 		showMessage = true;
 		message = "";
 		comment("All invoiced fees must be paid before selecting this status.");
 		cancel = true;
-	}
+	  }
+		
 }
-catch(err) {
+catch(err){
 	logDebug('Error in LIC_IssueLicenseFeeBalanceCheck: ' + err.message + "   ***StackTrace: " + err.stack);
 }
